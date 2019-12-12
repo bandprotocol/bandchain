1321c07bdd9e0fbe28d686174cf80a94fc2a0081
taobun <taobunoi@gmail.com>
2019-12-13 02:00:44 +0700

    Fix follow comment

--- bridges/evm/contracts/BytesLib.sol
+++ bridges/evm/contracts/BytesLib.sol
@@ -1,26 +1,26 @@
 pragma solidity 0.5.14;
 
 /**
- * @dev Helper library using in calculating merkle proof
+ * @dev Helper library for calculating Merkle proof and managing bytes.
  */
 library BytesLib {
   /**
-    * @dev Returns the hash of leaf node of merkle tree
-  */
+   * @dev Returns the hash of a Merkle leaf node
+   */
   function leafHash(bytes memory value) internal pure returns (bytes memory) {
-    return abi.encodePacked(sha256(abi.encodePacked(false, value)));
+    return abi.encodePacked(sha256(abi.encodePacked(uint8(0), value)));
   }
 
   /**
-    * @dev Returns the hash of internal node calculate from children nodes
-  */
+   * @dev Returns the hash of internal node, calculated from child nodes
+   */
   function innerHash(bytes memory left, bytes memory right) internal pure returns (bytes memory) {
-    return abi.encodePacked(sha256(abi.encodePacked(true, left, right)));
+    return abi.encodePacked(sha256(abi.encodePacked(uint8(1), left, right)));
   }
 
   /**
-    * @dev Returns the decoded uint from input bytes without checking valid varint format
-  */
+   * @dev Returns the decoded uint from input bytes without checking valid varint format
+   */
   function decodeVarint(bytes memory _encodeByte) internal pure returns (uint) {
     uint v = 0;
     for (uint i = 0; i < _encodeByte.length; i++) {
@@ -30,13 +30,16 @@ library BytesLib {
   }
 
   /**
-    * @dev Returns data part from input prefix (from tendermint), prefix is represeneted in this form
-    * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    * | L/R | Length |      ...      |   Data   | -> Data is an output of this function
-    * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    * |  1  |    7   |  248-Length  |   Length  | -> Represent size (bit)
-    * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-  */
+   * @dev Returns data part from input prefix (from tendermint), prefix is represented in the following format
+   * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+   * | L/R | Length |     ...     |   Data   | -> Data is the output of this function
+   * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+   * |  1  |    7   | 248-Length  | 8*Length | -> Represent size (bit)
+   * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+   * The first bit not relevant here (used to specify whether a node is left or right child on other contexts).
+   * The next 7 bits encode the size of the data in bytes, using big-endian.
+   * The last size bytes of the given input are the actual data.
+   */
   function getBytes(uint _prefix) internal pure returns(bytes memory) {
     uint prefix = _prefix;
     uint length = (prefix >> 248) & 127;
--- bridges/evm/test/BytesLib.test.js
+++ bridges/evm/test/BytesLib.test.js
@@ -1,14 +1,12 @@
 const BytesLibMock = artifacts.require("BytesLibMock");
 
-const { expect } = require("chai");
-
-describe("BytesLib", () => {
+contract("BytesLib", () => {
   beforeEach(async () => {
     this.bytesLib = await BytesLibMock.new();
   });
 
-  describe("leafHash", () => {
-    it("calculate leaf hash correctly", async () => {
+  context("leafHash", () => {
+    it("should calculate leaf hash correctly", async () => {
       (
         await this.bytesLib.leafHash(
           "0x08d1082cc8d85a0833da8815ff1574675c415760e0aff7fb4e32de6de27faf86"
@@ -21,8 +19,8 @@ describe("BytesLib", () => {
     });
   });
 
-  describe("innerHash", () => {
-    it("calculate inner hash correctly", async () => {
+  context("innerHash", () => {
+    it("should calculate inner hash correctly", async () => {
       (
         await this.bytesLib.innerHash(
           "0x08d1082cc8d85a0833da8815ff1574675c415760e0aff7fb4e32de6de27faf86",
@@ -36,47 +34,50 @@ describe("BytesLib", () => {
     });
   });
 
-  describe("decodeVarint", () => {
-    it("decode 1 byte varint correctly", async () => {
+  context("decodeVarint", () => {
+    it("should decode 1 byte varint correctly", async () => {
       (await this.bytesLib.decodeVarint("0x74")).toString().should.eq("116");
     });
-    it("decode 2 bytes varint correctly", async () => {
+    it("should decode 2 bytes varint correctly", async () => {
       (await this.bytesLib.decodeVarint("0xe374"))
         .toString()
         .should.eq("14947");
     });
-    it("decode >2 bytes varint correctly", async () => {
+    it("should decode >2 bytes varint correctly", async () => {
       (await this.bytesLib.decodeVarint("0xa3f2e574"))
         .toString()
         .should.eq("244939043");
     });
   });
 
-  describe("getBytes", () => {
-    it("calculate (3) bytes from prefix", async () => {
+  context("getBytes", () => {
+    it("should get 3 bytes from prefix", async () => {
       (
         await this.bytesLib.getBytes(
           "1356938545749799165119972480570561420155507632800475359837393562592742450709"
+          // hex format is "0x3000000000000000000000000000000000000000000000000000000009fa615"
         )
       )
         .toString()
         .should.eq("0x9fa615");
     });
 
-    it("calculate (3) bytes from prefix must padding zero", async () => {
+    it("should get 3 bytes from prefix with padding zero", async () => {
       (
         await this.bytesLib.getBytes(
           "1356938545749799165119972480570561420155507632800475359837393562592731987993"
+          // hex format is "0x300000000000000000000000000000000000000000000000000000000000019"
         )
       )
         .toString()
         .should.eq("0x000019");
     });
 
-    it("calculate (3) bytes from prefix must not use exceed bytes", async () => {
+    it("should get only last 3 bytes from prefix", async () => {
       (
         await this.bytesLib.getBytes(
           "1356938545749799165119972480570561420155507632800475359837393811163738369617"
+          // hex format is "0x300000000000000000000000000000000000000000000000000e212f2896e51"
         )
       )
         .toString()
