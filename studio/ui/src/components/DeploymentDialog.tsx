016042599f720049aaf397f3dd9ae0684a92b8f7
Paul Chonpimai <pchonpimai@gmail.com>
2019-12-13 16:10:23 +0700

    Change license

--- studio/ui/src/components/DeploymentDialog.tsx
+++ studio/ui/src/components/DeploymentDialog.tsx
@@ -1,4 +1,4 @@
-/* Copyright 2018 Mozilla Foundation
+/* Copyright 2019 Band Protocol
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
