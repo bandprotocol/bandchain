625cc31592cace17c0322b82490a4a80dbf39858
Kanisorn Thongprapaisaeng <kanisorn.peach@gmail.com>
2019-12-19 15:31:14 +0700

    Combine owasm docker-compose (#77)
    * combine owasm to docker compose
    
    * fix cors block for 1 validator
    
    * fix explorer rpc ip
    
    * update docker.md
    
    * create docker volume

--- chain/docker-config/single-validator/run.sh
+++ chain/docker-config/single-validator/run.sh
@@ -6,4 +6,10 @@ cp /zoracle/docker-config/single-validator/priv_validator_key.json ~/.bandd/conf
 cp /zoracle/docker-config/single-validator/node_key.json ~/.bandd/config/node_key.json
 cp /zoracle/docker-config/single-validator/genesis.json ~/.bandd/config/genesis.json
 
+# add cors in config.toml
+cd ~/.bandd/config/
+sed 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' config.toml > config_tmp.toml
+mv config_tmp.toml config.toml
+cd /zoracle/
+
 bandd start --rpc.laddr tcp://0.0.0.0:26657 & go run cmd/provider/main.go
