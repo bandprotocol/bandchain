19d9fc23e5594d5acd23245d6aee5eccef7c9caa
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-01-03 10:17:47 +0700

    scan: Add WIP of latest txs

--- chain/client/client.go
+++ chain/client/client.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
@@ -58,6 +59,32 @@ func LatestBlocksRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	}
 }
 
+func LatestTxsRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		_, _, limit, err := rest.ParseHTTPArgs(r)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// TODO: (1) Sort result in desc order after tendermint/tendermint:#4253 is released
+		// TODO: (2) Perform binary search on 'tx.height>?' to optimize the performance
+		searchResult, err := utils.QueryTxsByEvents(cliCtx, []string{"tx.height>0"}, page, limit)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		rest.PostProcessResponseBare(w, cliCtx, searchResult)
+	}
+}
+
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/d3n/blocks/latest", LatestBlocksRequestHandlerFn(cliCtx)).Methods("GET")
+	r.HandleFunc("/d3n/txs/latest", LatestTxsRequestHandlerFn(cliCtx)).Methods("GET")
 }
