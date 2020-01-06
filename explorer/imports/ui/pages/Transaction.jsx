674745aca30c7f203ba57695a9be278198319e44
Kanisorn Thongprapaisaeng <kanisorn.peach@gmail.com>
2020-01-06 15:03:43 +0700

    tx page ui (#105)
    * tx page
    
    * add report, create request script
    
    * update request

--- explorer/imports/ui/pages/Transaction.jsx
+++ explorer/imports/ui/pages/Transaction.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Flex } from 'rebass'
+import TxDetail from '/imports/ui/components/TxDetail'
+import MsgDetail from '/imports/ui/components/msg/MsgDetail'
 
-export default props => (
-  <Flex justifyContent="center" alignItems="center" width="100%">
-    Transaction Details {props.match.params.txHash}
-  </Flex>
-)
+export default props => {
+  const txHash = props.match.params.txHash
+  return (
+    <Flex flexDirection="column" width="100%" py="20px">
+      <TxDetail txHash={txHash} />
+      <MsgDetail txHash={txHash} />
+    </Flex>
+  )
+}
