260906a495c84cad335f1a2bf6a8cdd24b833c9e
Bun Uthaitirat <taobunoi@gmail.com>
2020-01-14 21:04:57 +0700

    Add temporary implementation to make /d3n/txs/latest perform correctly (#224)
    * latest tx desc
    
    * Add comment

--- chain/client/client.go
+++ chain/client/client.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
@@ -74,13 +75,31 @@ func LatestTxsRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 
 		// TODO: (1) Sort result in desc order after tendermint/tendermint:#4253 is released
 		// TODO: (2) Perform binary search on 'tx.height>?' to optimize the performance
-		searchResult, err := utils.QueryTxsByEvents(cliCtx, []string{"tx.height>0"}, page, limit)
+
+		// Temporary implementation to get latest tx sort by descending timestamp
+		// Pull request at bandprotocol/d3n:#224
+		searchResult, err := utils.QueryTxsByEvents(cliCtx, []string{"tx.height>0"}, 1, 1000000)
 		if err != nil {
 			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 
-		rest.PostProcessResponseBare(w, cliCtx, searchResult)
+		noTx := searchResult.TotalCount
+		startIdx := noTx - (page-1)*limit - 1 // Latest tx in page
+		endIdx := noTx - page*limit           // Oldest tx in page
+
+		var result sdk.SearchTxsResult
+		result.PageNumber = page
+		result.TotalCount = noTx
+		result.Limit = limit
+		result.Txs = make([]sdk.TxResponse, 0)
+		for ; startIdx >= endIdx && startIdx >= 0; startIdx-- {
+			result.Txs = append(result.Txs, searchResult.Txs[startIdx])
+		}
+		result.Count = len(result.Txs)
+		result.PageTotal = (noTx + limit - 1) / limit
+
+		rest.PostProcessResponseBare(w, cliCtx, result)
 	}
 }
 
