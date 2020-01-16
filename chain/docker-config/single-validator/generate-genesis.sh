4e11b6d02b853e96323487d8a7783aa8a7ac357b
taobun <taobunoi@gmail.com>
2020-01-16 12:32:21 +0700

    Update single validator script

--- chain/docker-config/single-validator/generate-genesis.sh
+++ chain/docker-config/single-validator/generate-genesis.sh
@@ -21,8 +21,15 @@ expect $DIR/../add-account.exp \
     12345678 \
     "lock nasty suffer dirt dream fine fall deal curtain plate husband sound tower mom crew crawl guard rack snake before fragile course bacon range" \
 
+expect $DIR/../add-account.exp \
+    requester \
+    12345678 \
+    "smile stem oven genius cave resource better lunar nasty moon company ridge brass rather supply used horn three panic put venue analyst leader comic" \
+
 # add accounts to genesis
 bandd add-genesis-account $(bandcli keys show validator -a) 10000000000000uband
+
+bandd add-genesis-account $(bandcli keys show requester -a) 10000000000000uband
 
 # genesis configurations
 bandcli config chain-id bandchain
