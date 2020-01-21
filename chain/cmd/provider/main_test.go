25dd542ab1df1f8cc8c83fd6a0dd2fe8186dc534
prin-r <q@bandprotocol.com>
2020-01-21 12:53:54 +0700

    change from ping to slee

--- chain/cmd/provider/main_test.go
+++ chain/cmd/provider/main_test.go
@@ -1,46 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"testing"
+	"time"
 
-	"github.com/bandprotocol/d3n/chain/x/zoracle"
 	"github.com/stretchr/testify/require"
-	abci "github.com/tendermint/tendermint/abci/types"
-	common "github.com/tendermint/tendermint/libs/common"
 )
 
 func TestExecWithTimeoutSuccess(t *testing.T) {
-	command := Command{Cmd: "ping", Arguments: []string{"-c 1", "-i 1", "8.8.8.8"}}
-	_, err := execWithTimeout(command, 2000)
+	command := Command{Cmd: "sleep", Arguments: []string{"0.1"}}
+	_, err := execWithTimeout(command, 200*time.Millisecond)
 	require.Nil(t, err)
 }
 
 func TestExecWithTimeoutFailTimeout(t *testing.T) {
-	command := Command{Cmd: "ping", Arguments: []string{"-c 2", "-i 1", "8.8.8.8"}}
-	_, err := execWithTimeout(command, 1000)
+	command := Command{Cmd: "sleep", Arguments: []string{"0.2"}}
+	_, err := execWithTimeout(command, 100*time.Millisecond)
 	require.EqualError(t, err, "Command timed out")
-}
-
-func TestHandleRequestShouldNotTimeout(t *testing.T) {
-	commands := []Command{Command{Cmd: "ping", Arguments: []string{"-c 1", "-i 1", "8.8.8.8"}}}
-	value, err := json.Marshal(&commands)
-	require.Nil(t, err)
-
-	event := abci.Event{}
-	event.Attributes = []common.KVPair{common.KVPair{Key: ([]byte)(zoracle.AttributeKeyPrepare), Value: ([]byte)(fmt.Sprintf("%x", value))}}
-	_, err = handleRequest(&event)
-	require.EqualError(t, err, "handleRequest send tx fail : ABCIQuery: Post http:: http: no Host in request URL")
-}
-
-func TestHandleRequestShouldTimeout(t *testing.T) {
-	commands := []Command{Command{Cmd: "ping", Arguments: []string{"-c 4", "-i 1", "8.8.8.8"}}}
-	value, err := json.Marshal(&commands)
-	require.Nil(t, err)
-
-	event := abci.Event{}
-	event.Attributes = []common.KVPair{common.KVPair{Key: ([]byte)(zoracle.AttributeKeyPrepare), Value: ([]byte)(fmt.Sprintf("%x", value))}}
-	_, err = handleRequest(&event)
-	require.EqualError(t, err, "handleRequest query err with command: ping [-c 4 -i 1 8.8.8.8], error: Command timed out")
 }
