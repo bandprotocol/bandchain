1cc0c963086de2bf1e42aedb01952e44ffe09bdc
prin-r <q@bandprotocol.com>
2020-01-22 15:49:05 +0700

    function new should receive impl Into<String> instead of String

--- owasm/src/ext/crypto/alphavantage.rs
+++ owasm/src/ext/crypto/alphavantage.rs
@@ -9,14 +9,14 @@ pub struct Price {
 }
 
 impl Price {
-    pub fn new(coin: &Coins, alphavantage_api_key: &String) -> Price {
+    pub fn new(coin: &Coins, alphavantage_api_key: impl Into<String>) -> Price {
         Price {
             symbol: String::from(match coin {
                 Coins::BTC => "BTC",
                 Coins::ETH => "ETH",
                 Coins::BAND => "BAND",
             }),
-            alphavantage_api_key: (*alphavantage_api_key).clone(),
+            alphavantage_api_key: alphavantage_api_key.into(),
         }
     }
 }
@@ -53,7 +53,7 @@ mod tests {
     #[test]
     fn test_as_cmd() {
         assert_eq!(
-            Price::new(&Coins::BTC, &"WVKPOO76169EX950".to_string()).as_cmd(),
+            Price::new(&Coins::BTC, String::from("WVKPOO76169EX950")).as_cmd(),
             ShellCmd::new(
                 "curl",
                 &["https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=WVKPOO76169EX950"]
@@ -64,45 +64,45 @@ mod tests {
     #[test]
     fn test_from_cmd_ok() {
         assert_eq!(
-            Price::new(&Coins::BTC, &"".to_string()).from_cmd_output(r#"{"Realtime Currency Exchange Rate": {"1. From_Currency Code": "BAND","2. From_Currency Name": null,"3. To_Currency Code": "USD","4. To_Currency Name": "United States Dollar","5. Exchange Rate": "100.0","6. Last Refreshed": "2020-01-22 06:07:35","7. Time Zone": "UTC","8. Bid Price": "0.23960000","9. Ask Price": "0.24010000"}}"#.into()),
+            Price::new(&Coins::BTC, String::from("")).from_cmd_output(r#"{"Realtime Currency Exchange Rate": {"1. From_Currency Code": "BAND","2. From_Currency Name": null,"3. To_Currency Code": "USD","4. To_Currency Name": "United States Dollar","5. Exchange Rate": "100.0","6. Last Refreshed": "2020-01-22 06:07:35","7. Time Zone": "UTC","8. Bid Price": "0.23960000","9. Ask Price": "0.24010000"}}"#.into()),
             Some(100.0)
         );
     }
 
     #[test]
     fn test_from_cmd_not_ok() {
-        assert_eq!(Price::new(&Coins::BTC, &"".to_string()).from_cmd_output(r#"{}"#.into()), None);
+        assert_eq!(Price::new(&Coins::BTC, String::from("")).from_cmd_output(r#"{}"#.into()), None);
         assert_eq!(
-            Price::new(&Coins::BTC, &"just normal string".to_string())
+            Price::new(&Coins::BTC, &String::from("just a normal string"))
                 .from_cmd_output(r#"{}"#.into()),
             None
         );
         assert_eq!(
-            Price::new(&Coins::BTC, &"{}".to_string()).from_cmd_output(r#"{}"#.into()),
+            Price::new(&Coins::BTC, String::from("{}")).from_cmd_output(r#"{}"#.into()),
             None
         );
         assert_eq!(
-            Price::new(&Coins::BTC, &r#"{"UnknowField":"100.0"}"#.to_string())
+            Price::new(&Coins::BTC, String::from(r#"{"UnknowField":"100.0"}"#))
                 .from_cmd_output(r#"{}"#.into()),
             None
         );
         assert_eq!(
             Price::new(
                 &Coins::BTC,
-                &r#"{"Realtime Currency Exchange Rate":"just normal string""#.to_string()
+                String::from(r#"{"Realtime Currency Exchange Rate":"just normal string""#)
             )
             .from_cmd_output(r#"{}"#.into()),
             None
         );
         assert_eq!(
-            Price::new(&Coins::BTC, &r#"{"Realtime Currency Exchange Rate":{}}"#.to_string())
+            Price::new(&Coins::BTC, String::from(r#"{"Realtime Currency Exchange Rate":{}}"#))
                 .from_cmd_output(r#"{}"#.into()),
             None
         );
         assert_eq!(
             Price::new(
                 &Coins::BTC,
-                &r#"{"Realtime Currency Exchange Rate":{"UnknowField":"100.0"}}"#.to_string()
+                String::from(r#"{"Realtime Currency Exchange Rate":{"UnknowField":"100.0"}}"#)
             )
             .from_cmd_output(r#"{}"#.into()),
             None
