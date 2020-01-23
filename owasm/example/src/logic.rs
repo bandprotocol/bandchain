335f6729de76946c9b2307f5f63edaff45d9037b
prin-r <q@bandprotocol.com>
2020-01-23 23:57:56 +0700

    add date to logic

--- owasm/example/src/logic.rs
+++ owasm/example/src/logic.rs
@@ -1,4 +1,5 @@
 use owasm::ext::crypto::{binance, coingecko, coins, cryptocompare};
+use owasm::ext::utils::date;
 use owasm::{decl_data, decl_params, decl_result};
 
 decl_params! {
@@ -12,12 +13,14 @@ decl_data! {
         pub coin_gecko: f32 = |params: &Parameter| coingecko::Price::new(&params.symbol),
         pub crypto_compare: f32 = |params: &Parameter| cryptocompare::Price::new(&params.symbol),
         pub binance: f32 = |params: &Parameter| binance::Price::new(&params.symbol),
+        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub price_in_usd: u64,
+        pub time_stamp: u64,
     }
 }
 
@@ -29,11 +32,14 @@ impl Data {
 
 pub fn execute(data: Vec<Data>) -> Result {
     let mut total = 0.0;
+    let mut time_stamp_acc: u64 = 0;
     for each in &data {
         total += each.avg_px();
+        time_stamp_acc += each.time_stamp;
     }
     let average = total / (data.len() as f32);
-    Result { price_in_usd: (average * 100.0) as u64 }
+    let avg_time_stamp = time_stamp_acc / (data.len() as u64);
+    Result { price_in_usd: (average * 100.0) as u64, time_stamp: avg_time_stamp }
 }
 
 #[cfg(test)]
@@ -43,11 +49,13 @@ mod tests {
     #[test]
     fn test_execute() {
         // Average is 120.00
-        let data1 = Data { coin_gecko: 100.0, crypto_compare: 150.0, binance: 110.0 };
+        let data1 =
+            Data { coin_gecko: 100.0, crypto_compare: 150.0, binance: 110.0, time_stamp: 10 };
         // Average is 220.00
-        let data2 = Data { coin_gecko: 200.0, crypto_compare: 250.0, binance: 210.0 };
+        let data2 =
+            Data { coin_gecko: 200.0, crypto_compare: 250.0, binance: 210.0, time_stamp: 12 };
         // Average among the two data points is 170.00
-        assert_eq!(execute(vec![data1, data2]), Result { price_in_usd: 17000 });
+        assert_eq!(execute(vec![data1, data2]), Result { price_in_usd: 17000, time_stamp: 11 });
     }
 
     #[test]
