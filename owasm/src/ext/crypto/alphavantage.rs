8c1470967d73330e75fd7eb45a07320381da8ee2
prin-r <q@bandprotocol.com>
2020-01-23 20:22:45 +0700

    fix wrong test

--- owasm/src/ext/crypto/alphavantage.rs
+++ owasm/src/ext/crypto/alphavantage.rs
@@ -28,7 +28,7 @@ impl Oracle for Price {
 
     fn from_cmd_output(&self, output: String) -> Option<f32> {
         let parsed = json::parse(&output).ok()?;
-        Some(parsed["Global Quote"]["05. price"].as_str()?.parse::<f32>().ok()?)
+        parsed["Global Quote"]["05. price"].as_str()?.parse::<f32>().ok()
     }
 }
 
@@ -50,37 +50,13 @@ mod tests {
     #[test]
     fn test_from_cmd_ok() {
         assert_eq!(
-            Price::new("GOOG", "").from_cmd_output(r#"{"Global Quote":{"01. symbol":"GOOG","02. open":"1491.0000","03. high":"1503.2100","04. low":"1484.9300","05. price":"100.0","06. volume":"1593218","07. latest trading day":"2020-01-22","08. previous close":"1484.4000","09. change":"1.5500","10. change percent":"0.1044%"}}"#.into()),
+            Price::new("GOOG", "WVKPOO76169EX950").from_cmd_output(r#"{"Global Quote":{"01. symbol":"GOOG","02. open":"1491.0000","03. high":"1503.2100","04. low":"1484.9300","05. price":"100.0","06. volume":"1593218","07. latest trading day":"2020-01-22","08. previous close":"1484.4000","09. change":"1.5500","10. change percent":"0.1044%"}}"#.into()),
             Some(100.0)
         );
     }
 
     #[test]
     fn test_from_cmd_not_ok() {
-        assert_eq!(Price::new("GOOG", "").from_cmd_output(r#"{}"#.into()), None);
-        assert_eq!(
-            Price::new("GOOG", "just a normal string").from_cmd_output(r#"{}"#.into()),
-            None
-        );
-        assert_eq!(Price::new("GOOG", "{}").from_cmd_output(r#"{}"#.into()), None);
-        assert_eq!(
-            Price::new("GOOG", r#"{"UnknowField":"100.0"}"#).from_cmd_output(r#"{}"#.into()),
-            None
-        );
-        assert_eq!(
-            Price::new("GOOG", r#"{"Realtime Currency Exchange Rate":"just normal string""#)
-                .from_cmd_output(r#"{}"#.into()),
-            None
-        );
-        assert_eq!(
-            Price::new("GOOG", r#"{"Realtime Currency Exchange Rate":{}}"#)
-                .from_cmd_output(r#"{}"#.into()),
-            None
-        );
-        assert_eq!(
-            Price::new("GOOG", r#"{"Realtime Currency Exchange Rate":{"UnknowField":"100.0"}}"#)
-                .from_cmd_output(r#"{}"#.into()),
-            None
-        );
+        assert_eq!(Price::new("GOOG", "WVKPOO76169EX950").from_cmd_output(r#"{}"#.into()), None);
     }
 }
