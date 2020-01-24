2d30d81e79ecb10cbdf567e66023551b9c47f2ef
prin-r <q@bandprotocol.com>
2020-01-24 14:06:21 +0700

    fix calculation make more sense

--- owasm/example/src/logic.rs
+++ owasm/example/src/logic.rs
@@ -24,25 +24,33 @@ decl_result! {
     pub struct Result {
         pub price_in_usd: u64,
         pub time_stamp: u64,
+        pub price_from_alphavantage_in_usd: u64,
     }
 }
 
 impl Data {
     pub fn avg_px(&self) -> f32 {
-        (self.coin_gecko + self.crypto_compare + self.binance + self.alphavantage) / 4.0
+        (self.coin_gecko + self.crypto_compare + self.binance) / 3.0
     }
 }
 
 pub fn execute(data: Vec<Data>) -> Result {
     let mut total = 0.0;
     let mut time_stamp_acc: u64 = 0;
+    let mut total_price_from_alphavantage = 0.0;
     for each in &data {
         total += each.avg_px();
         time_stamp_acc += each.time_stamp;
+        total_price_from_alphavantage += each.alphavantage;
     }
     let average = total / (data.len() as f32);
+    let average_price_from_alphavantage = total_price_from_alphavantage / (data.len() as f32);
     let avg_time_stamp = time_stamp_acc / (data.len() as u64);
-    Result { price_in_usd: (average * 100.0) as u64, time_stamp: avg_time_stamp }
+    Result {
+        price_in_usd: (average * 100.0) as u64,
+        price_from_alphavantage_in_usd: (average_price_from_alphavantage * 100.0) as u64,
+        time_stamp: avg_time_stamp,
+    }
 }
 
 #[cfg(test)]
@@ -68,7 +76,10 @@ mod tests {
             time_stamp: 12,
         };
         // Average among the two data points is 170.00
-        assert_eq!(execute(vec![data1, data2]), Result { price_in_usd: 17000, time_stamp: 11 });
+        assert_eq!(
+            execute(vec![data1, data2]),
+            Result { price_in_usd: 17000, price_from_alphavantage_in_usd: 17000, time_stamp: 11 }
+        );
     }
 
     #[test]
@@ -79,18 +90,6 @@ mod tests {
             alphavantage_api_key: String::from("WVKPOO76169EX950"),
         })
         .unwrap();
-        println!("Current GOOG price (times 100) is {:?}", execute(vec![data]));
-    }
-
-    #[test]
-    fn test_end_to_end_from_local_env() {
-        // Run with local environment
-        let data = Data::build_from_local_env(&Parameter {
-            symbol: coins::Coins::ETH,
-            alphavantage_symbol: String::from("ETH"),
-            alphavantage_api_key: String::from("WVKPOO76169EX950"),
-        })
-        .unwrap();
-        println!("Current ETH price (times 100) is {:?}", execute(vec![data]));
+        println!("Current ETH and GOOG price (times 100) is {:?}", execute(vec![data]));
     }
 }
