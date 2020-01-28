c0c1fe1c91866e2563e6751ca8c2f1733508b09e
prin-r <q@bandprotocol.com>
2020-01-28 13:38:54 +0700

    add new end point

--- chain/x/zoracle/client/rest/rest.go
+++ chain/x/zoracle/client/rest/rest.go
@@ -18,5 +18,5 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) 
 	r.HandleFunc(fmt.Sprintf("/%s/request/{%s}", storeName, requestID), getRequestHandler(cliCtx, storeName)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/%s/script/{%s}", storeName, codeHash), getScriptInfoHandler(cliCtx, storeName)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/%s/scripts", storeName), getScriptsHandler(cliCtx, storeName)).Methods("GET")
-	r.HandleFunc(fmt.Sprintf("/%s/{%s}/serialize-params/{%s}", storeName, codeHash, params), getSerializeParams(cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/serialize-params/{%s}/{%s}", storeName, codeHash, params), getSerializeParams(cliCtx, storeName)).Methods("GET")
 }
