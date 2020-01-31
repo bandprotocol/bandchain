7e3cd50c6f7a49b1045dbc375c7e1bd3e5294533
prin-r <q@bandprotocol.com>
2020-01-31 15:54:33 +0700

    separate bridge interface to its own file outside of example

---
+++ bridges/evm/contracts/IBridge.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.14;
+pragma experimental ABIEncoderV2;
+
+interface IBridge {
+    /// Helper struct to help the function caller to decode oracle data.
+    struct VerifyOracleDataResult {
+        bytes data;
+        bytes32 codeHash;
+        bytes params;
+    }
+
+    function relayAndVerify(bytes calldata _data)
+        external
+        returns (VerifyOracleDataResult memory result);
+}
