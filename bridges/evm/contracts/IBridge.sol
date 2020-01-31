07e239ef9e12f7d3394141bf4c818d44078416d3
prin-r <q@bandprotocol.com>
2020-01-31 16:00:25 +0700

    fix spacing for IBridge

--- bridges/evm/contracts/IBridge.sol
+++ bridges/evm/contracts/IBridge.sol
@@ -2,14 +2,14 @@ pragma solidity 0.5.14;
 pragma experimental ABIEncoderV2;
 
 interface IBridge {
-    /// Helper struct to help the function caller to decode oracle data.
-    struct VerifyOracleDataResult {
-        bytes data;
-        bytes32 codeHash;
-        bytes params;
-    }
+	/// Helper struct to help the function caller to decode oracle data.
+	struct VerifyOracleDataResult {
+		bytes data;
+		bytes32 codeHash;
+		bytes params;
+	}
 
-    function relayAndVerify(bytes calldata _data)
-        external
-        returns (VerifyOracleDataResult memory result);
+	function relayAndVerify(bytes calldata _data)
+		external
+		returns (VerifyOracleDataResult memory result);
 }
