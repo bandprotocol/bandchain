a83c909d49659e7ab04c2942100bcf6b1e80e839
prin-r <q@bandprotocol.com>
2020-01-31 15:55:52 +0700

    remove local bridge interface and use IBridge instead

--- bridges/evm/contracts/example/LuckyNumber.sol
+++ bridges/evm/contracts/example/LuckyNumber.sol
@@ -2,17 +2,7 @@ pragma solidity 0.5.14;
 pragma experimental ABIEncoderV2;
 
 import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-
-interface Bridge {
-  struct VerifyOracleDataResult {
-    bytes data;
-    bytes32 codeHash;
-    bytes params;
-  }
-  function relayAndVerify(bytes calldata data)
-    external
-    returns (VerifyOracleDataResult memory result);
-}
+import { IBridge } from "../IBridge.sol";
 
 contract LuckyNumber is Ownable {
   bytes32 public codeHash;
@@ -20,12 +10,13 @@ contract LuckyNumber is Ownable {
 
   uint64 target;
 
-  Bridge bridge = Bridge(0x3e1F8745E4088443350121075828F119075ef641);
+  IBridge bridge;
 
-  constructor(bytes32 _codeHash, uint64 _maxNumber, uint64 _target)
+  constructor(IBridge _bridge, bytes32 _codeHash, uint64 _maxNumber, uint64 _target)
     public
     payable
   {
+    bridge = _bridge;
     codeHash = _codeHash;
     maxNumber = _maxNumber;
     target = _target;
@@ -41,7 +32,7 @@ contract LuckyNumber is Ownable {
   }
 
   function guess(bytes memory _reportPrice) public {
-    Bridge.VerifyOracleDataResult memory result = bridge.relayAndVerify(
+    IBridge.VerifyOracleDataResult memory result = bridge.relayAndVerify(
       _reportPrice
     );
 
