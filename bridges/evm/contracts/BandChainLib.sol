6ce4b5ab98e69fa4241d5ad6f1c93e8fb714d2d4
prin-r <q@bandprotocol.com>
2020-02-04 21:48:40 +0700

    use uint64 instead of bytes8

--- bridges/evm/contracts/BandChainLib.sol
+++ bridges/evm/contracts/BandChainLib.sol
@@ -4,15 +4,15 @@ library BandChainLib {
   function toUint64List(bytes memory _data) internal pure returns(uint64[] memory) {
     uint64[] memory result = new uint64[](_data.length / 8);
     require(result.length * 8 == _data.length, 'DATA_LENGTH_IS_INVALID');
-    
+
     for (uint256 i = 0; i < result.length; i++) {
-      bytes8 tmp;
+      uint64 tmp;
       assembly {
-        tmp := mload(add(_data, add(0x20, mul(i,0x08))))
+        tmp := mload(add(_data, add(0x08, mul(i, 0x08))))
       }
-      result[i] = uint64(tmp);
+      result[i] = tmp;
     }
-    
+
     return result;
   }
 }
