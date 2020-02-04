7eb7fe72fecd92e116934621f4b6a688063f23b9
prin-r <q@bandprotocol.com>
2020-02-04 21:56:48 +0700

    change single code to double for consistency

--- bridges/evm/contracts/BandChainLib.sol
+++ bridges/evm/contracts/BandChainLib.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.14;
 library BandChainLib {
   function toUint64List(bytes memory _data) internal pure returns(uint64[] memory) {
     uint64[] memory result = new uint64[](_data.length / 8);
-    require(result.length * 8 == _data.length, 'DATA_LENGTH_IS_INVALID');
+    require(result.length * 8 == _data.length, "DATA_LENGTH_IS_INVALID");
 
     for (uint256 i = 0; i < result.length; i++) {
       uint64 tmp;
