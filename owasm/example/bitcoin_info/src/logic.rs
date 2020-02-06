6d55341ce1ddc98ac122bafda39acb9030f24b5c
prin-r <q@bandprotocol.com>
2020-02-06 14:05:39 +0700

    fix logic to be compatible with [u64;4]

--- owasm/example/bitcoin_info/src/logic.rs
+++ owasm/example/bitcoin_info/src/logic.rs
@@ -9,14 +9,14 @@ decl_params! {
 
 decl_data! {
     pub struct Data {
-        pub block_hash: String = |params: &Parameter| block_hash::Info::new(params.block_height),
+        pub block_hash: [u64;4] = |params: &Parameter| block_hash::Info::new(params.block_height),
         pub block_count: u64 = |_: &Parameter| block_count::Info::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
-        pub block_hash: String,
+        pub block_hash: [u64;4],
         pub confirmation: u64,
     }
 }
@@ -29,7 +29,7 @@ pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
                 count = count + 1;
                 if count > data.len() / 2 {
                     return Result {
-                        block_hash: a.block_hash.to_string(),
+                        block_hash: a.block_hash,
                         confirmation: a.block_count - _params.block_height,
                     };
                 }
@@ -37,7 +37,7 @@ pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
         }
     }
 
-    Result { block_hash: "".into(), confirmation: 0 }
+    Result { block_hash: [0; 4], confirmation: 0 }
 }
 
 #[cfg(test)]
@@ -48,29 +48,21 @@ mod tests {
     fn test_execute_when_every_report_the_same_things() {
         let params = Parameter { block_height: 616047 };
         let data1 = Data {
-            block_hash: String::from(
-                "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         let data2 = Data {
-            block_hash: String::from(
-                "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         let data3 = Data {
-            block_hash: String::from(
-                "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         assert_eq!(
             execute(params, vec![data1, data2, data3]),
             Result {
-                block_hash: String::from(
-                    "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b"
-                ),
+                block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
                 confirmation: 616058 - 616047
             }
         );
@@ -80,29 +72,21 @@ mod tests {
     fn test_execute_when_the_minority_report_differently() {
         let params = Parameter { block_height: 616047 };
         let data1 = Data {
-            block_hash: String::from(
-                "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         let data2 = Data {
-            block_hash: String::from(
-                "0000000000000000000000000000000000000000000000000000000000000000",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         let data3 = Data {
-            block_hash: String::from(
-                "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         assert_eq!(
             execute(params, vec![data1, data2, data3]),
             Result {
-                block_hash: String::from(
-                    "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b"
-                ),
+                block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
                 confirmation: 616058 - 616047
             }
         );
@@ -112,20 +96,36 @@ mod tests {
     fn test_execute_when_cant_find_majority() {
         let params = Parameter { block_height: 616047 };
         let data1 = Data {
-            block_hash: String::from(
-                "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b",
-            ),
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         let data2 = Data {
-            block_hash: String::from(
-                "0000000000000000000000000000000000000000000000000000000000000000",
-            ),
+            block_hash: [0, 9999999999999999, 16236236487057196107, 2784772046813676667],
             block_count: 616058,
         };
         assert_eq!(
             execute(params, vec![data1, data2]),
-            Result { block_hash: String::from(""), confirmation: 0 }
+            Result { block_hash: [0; 4], confirmation: 0 }
+        );
+    }
+
+    #[test]
+    fn test_encode_output() {
+        let output = Result {
+            block_hash: [0, 3502949345222752, 16236236487057196107, 2784772046813676667],
+            confirmation: 616058 - 616047,
+        };
+        let output_hex = bincode::config()
+            .big_endian()
+            .serialize(&output)
+            .unwrap()
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<Vec<String>>()
+            .join("");
+        assert_eq!(
+            output_hex,
+            "0000000000000000000c71e9f3636060e152b30fcc47c44b26a57f6c16c4447b000000000000000b"
         );
     }
 
