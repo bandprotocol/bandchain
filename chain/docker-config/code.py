a3d866315d9230008f6b467ad351bcec4b021575
taobun <taobunoi@gmail.com>
2020-02-07 22:40:37 +0700

    Update comment and format script

--- chain/docker-config/code.py
+++ chain/docker-config/code.py
@@ -10,13 +10,24 @@ scripts = []
 for script in os.listdir(EXAMPLE_DIR):
     if script.startswith("."):
         continue
-    subprocess.call([CURRENT_PATH+"/build_wasm.sh %s"% EXAMPLE_DIR+script],shell=True)
-    a = subprocess.check_output(["xxd", "-p", "-c100000000000",EXAMPLE_DIR+script+"/pkg/"+script+"_bg.wasm"])
-    scripts.append({
-        "code": a.decode()[:-1],
-        "name": script,
-        "owner": "band13zmknvkq2sj920spz90g4r9zjan8g584x8qalj",
-    })
+    subprocess.call(
+        [CURRENT_PATH + "/build_wasm.sh %s" % EXAMPLE_DIR + script], shell=True
+    )
+    a = subprocess.check_output(
+        [
+            "xxd",
+            "-p",
+            "-c100000000000",
+            EXAMPLE_DIR + script + "/pkg/" + script + "_bg.wasm",
+        ]
+    )
+    scripts.append(
+        {
+            "code": a.decode()[:-1],
+            "name": script,
+            "owner": "band13zmknvkq2sj920spz90g4r9zjan8g584x8qalj",
+        }
+    )
 
-with open(CURRENT_PATH+"/temp.json","w") as f:
-    json.dump(scripts,f)
+with open(CURRENT_PATH + "/temp.json", "w") as f:
+    json.dump(scripts, f)
