95e888513325054efd03df3dde90610e59114762
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-02-09 23:13:47 +0700

    chain: Update owasm execution environment interface

--- chain/owasm/types.go
+++ chain/owasm/types.go
@@ -6,28 +6,28 @@ package owasm
 type ExecutionEnvironment interface {
 	// GetCurrentRequestID returns the unique identifier that can is the reference
 	// to the current data request.
-	GetCurrentRequestID() (int64, error)
+	GetCurrentRequestID() int64
 
 	// GetRequestedValidatorCount returns the number of validators that the current
 	// data request specifies for the oracle query.
-	GetRequestedValidatorCount() (int32, error)
+	GetRequestedValidatorCount() int64
 
 	// GetReceivedValidatorCount returns the number of validators among the
-	// requested ones that replied with raw data reports. Only available during
-	// the *aggregation* phase.
-	GetReceivedValidatorCount() (int32, error)
-
-	// GetValidatorPublic returns the 33-byte compressed public key of the
-	// block validator at the specified index.
-	GetValidatorPubKey(validatorIndex int32) ([]byte, error)
+	// requested ones that replied with raw data reports. Return zero during the
+	// *preparation* phase.
+	GetReceivedValidatorCount() int64
 
 	// GetPrepareBlockTime returns the time at which the *preparation* phase of
 	// this data request was being run.
-	GetPrepareBlockTime() (int64, error)
+	GetPrepareBlockTime() int64
 
 	// GetAggregateBlockTime returns the time at which the *aggregation* phase of
-	// this data request was being run. Only available during the *aggregation* phase.
-	GetAggregateBlockTime() (int64, error)
+	// this data request was being run. Return zero during the *preparation* phase.
+	GetAggregateBlockTime() int64
+
+	// GetValidatorPublic returns the 33-byte compressed public key of the
+	// block validator at the specified index.
+	GetValidatorPubKey(validatorIndex int64) ([]byte, error)
 
 	// RequestExternalData performs a request to the specified data source
 	// with and assigns the request with the external data ID. The function must
@@ -43,6 +43,6 @@ type ExecutionEnvironment interface {
 	// The function must only be called during the *aggregation* phase.
 	GetExternalData(
 		externalDataID int64,
-		validatorIndex int32,
+		validatorIndex int64,
 	) ([]byte, error)
 }
