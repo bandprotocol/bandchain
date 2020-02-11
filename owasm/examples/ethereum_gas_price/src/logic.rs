049f313c7b774d539db0cb35fb4c41568baf311e
prin-r <q@bandprotocol.com>
2020-02-11 19:53:57 +0700

    add timestap to example ethereum_gas_price

--- owasm/examples/ethereum_gas_price/src/logic.rs
+++ owasm/examples/ethereum_gas_price/src/logic.rs
@@ -1,4 +1,5 @@
 use owasm::ext::ethgasstation::gas_price;
+use owasm::ext::utils::date;
 use owasm::{decl_data, decl_params, decl_result};
 
 decl_params! {
@@ -10,22 +11,27 @@ decl_params! {
 decl_data! {
     pub struct Data {
         pub gas_price: f32 = |params: &Parameter| gas_price::Price::new(&params.gas_option),
+        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub gas_price_in_gwei: u64,
+        pub time_stamp: u64,
     }
 }
 
 pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
     let mut total_gas_price = 0.0;
+    let mut time_stamp_acc: u64 = 0;
     for each in &data {
         total_gas_price += each.gas_price;
+        time_stamp_acc += each.time_stamp;
     }
     let average_gas_price = total_gas_price / (data.len() as f32);
-    Result { gas_price_in_gwei: (average_gas_price * 100.0) as u64 }
+    let avg_time_stamp = time_stamp_acc / (data.len() as u64);
+    Result { gas_price_in_gwei: (average_gas_price * 100.0) as u64, time_stamp: avg_time_stamp }
 }
 
 #[cfg(test)]
@@ -35,9 +41,12 @@ mod tests {
     #[test]
     fn test_execute() {
         let params = Parameter { gas_option: String::from("average") };
-        let data1 = Data { gas_price: 13.0 };
-        let data2 = Data { gas_price: 7.0 };
-        assert_eq!(execute(params, vec![data1, data2]), Result { gas_price_in_gwei: 1000 });
+        let data1 = Data { gas_price: 13.0, time_stamp: 10 };
+        let data2 = Data { gas_price: 7.0, time_stamp: 12 };
+        assert_eq!(
+            execute(params, vec![data1, data2]),
+            Result { gas_price_in_gwei: 1000, time_stamp: 11 }
+        );
     }
 
     #[test]
