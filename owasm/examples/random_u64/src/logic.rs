ca9837f2454ded3f261b73f4424b3f8ee1afeab4
prin-r <q@bandprotocol.com>
2020-02-11 20:30:28 +0700

    fix typo time_stamp -> timestamp

--- owasm/examples/random_u64/src/logic.rs
+++ owasm/examples/random_u64/src/logic.rs
@@ -13,14 +13,14 @@ decl_params! {
 decl_data! {
     pub struct Data {
         pub random_bytes8: Vec<u8> = |_: &Parameter| qrng_anu::RandomBytes::new(8),
-        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
+        pub timestamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub random_u64: u64,
-        pub time_stamp: u64,
+        pub timestamp: u64,
     }
 }
 
@@ -36,7 +36,7 @@ pub fn execute(params: Parameter, data: Vec<Data>) -> Result {
     Result {
         random_u64: data.iter().fold(0, |accumulator, each| accumulator ^ each.rng_to_u64())
             % params.max_range,
-        time_stamp: data.iter().fold(0, |accumulator, each| accumulator + each.time_stamp)
+        timestamp: data.iter().fold(0, |accumulator, each| accumulator + each.timestamp)
             / (data.len() as u64),
     }
 }
@@ -48,16 +48,16 @@ mod tests {
     #[test]
     fn test_execute() {
         let params = Parameter { max_range: 31 };
-        let data1 = Data { random_bytes8: vec![1, 2, 3, 4, 5, 6, 7, 8], time_stamp: 10 };
-        let data2 = Data { random_bytes8: vec![8, 7, 6, 5, 4, 3, 2, 1], time_stamp: 12 };
-        assert_eq!(execute(params, vec![data1, data2]), Result { random_u64: 18, time_stamp: 11 });
+        let data1 = Data { random_bytes8: vec![1, 2, 3, 4, 5, 6, 7, 8], timestamp: 10 };
+        let data2 = Data { random_bytes8: vec![8, 7, 6, 5, 4, 3, 2, 1], timestamp: 12 };
+        assert_eq!(execute(params, vec![data1, data2]), Result { random_u64: 18, timestamp: 11 });
     }
 
     #[test]
     fn test_call_get_random_u64() {
         let params = Parameter { max_range: 31 };
         let data = Data::build_from_local_env(&params)
-            .unwrap_or(Data { random_bytes8: vec![0; 8], time_stamp: 0 });
+            .unwrap_or(Data { random_bytes8: vec![0; 8], timestamp: 0 });
         println!("Current random number is {:?}", execute(params, vec![data]));
     }
 }
