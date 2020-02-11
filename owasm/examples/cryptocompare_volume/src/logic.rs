13d0e6fd25e03a4cf11cd61a85020d58f2cbc9f3
prin-r <q@bandprotocol.com>
2020-02-11 19:53:20 +0700

    add timestap to example cryptocompare vol

--- owasm/examples/cryptocompare_volume/src/logic.rs
+++ owasm/examples/cryptocompare_volume/src/logic.rs
@@ -1,4 +1,5 @@
-use owasm::ext::crypto::{cryptocompare, coins};
+use owasm::ext::crypto::{coins, cryptocompare};
+use owasm::ext::utils::date;
 use owasm::{decl_data, decl_params, decl_result};
 
 decl_params! {
@@ -10,22 +11,27 @@ decl_params! {
 decl_data! {
     pub struct Data {
         pub crypto_compare_vol24h: f64 = |params: &Parameter| cryptocompare::Volume24h::new(&params.crypto_symbol),
+        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub vol24h_in_usd: u64,
+        pub time_stamp: u64,
     }
 }
 
 pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
     let mut total_vol: f64 = 0.0;
+    let mut time_stamp_acc: u64 = 0;
     for each in &data {
         total_vol += each.crypto_compare_vol24h;
+        time_stamp_acc += each.time_stamp;
     }
     let average_vol = total_vol / (data.len() as f64);
-    Result { vol24h_in_usd: (average_vol * 100.0) as u64 }
+    let avg_time_stamp = time_stamp_acc / (data.len() as u64);
+    Result { vol24h_in_usd: (average_vol * 100.0) as u64, time_stamp: avg_time_stamp }
 }
 
 #[cfg(test)]
@@ -35,9 +41,12 @@ mod tests {
     #[test]
     fn test_execute() {
         let params = Parameter { crypto_symbol: coins::Coins::ETH };
-        let data1 = Data { crypto_compare_vol24h: 250.0 };
-        let data2 = Data { crypto_compare_vol24h: 600.0 };
-        assert_eq!(execute(params, vec![data1, data2]), Result { vol24h_in_usd: 42500 });
+        let data1 = Data { crypto_compare_vol24h: 250.0, time_stamp: 10 };
+        let data2 = Data { crypto_compare_vol24h: 600.0, time_stamp: 12 };
+        assert_eq!(
+            execute(params, vec![data1, data2]),
+            Result { vol24h_in_usd: 42500, time_stamp: 11 }
+        );
     }
 
     #[test]
