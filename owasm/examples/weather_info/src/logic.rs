ef2751b27545c66dffbcd18b4044094ad3d1e393
prin-r <q@bandprotocol.com>
2020-02-11 19:54:32 +0700

    add timestap to example weather

--- owasm/examples/weather_info/src/logic.rs
+++ owasm/examples/weather_info/src/logic.rs
@@ -1,3 +1,4 @@
+use owasm::ext::utils::date;
 use owasm::ext::weather::openweathermap;
 use owasm::{decl_data, decl_params, decl_result};
 
@@ -12,22 +13,27 @@ decl_params! {
 decl_data! {
     pub struct Data {
         pub weather_value: f64 = |params: &Parameter| openweathermap::WeatherInfo::new(&params.city, &params.key, &params.sub_key),
+        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub weather_value: u64,
+        pub time_stamp: u64,
     }
 }
 
 pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
     let mut total_weather_value = 0.0;
+    let mut time_stamp_acc: u64 = 0;
     for each in &data {
         total_weather_value += each.weather_value;
+        time_stamp_acc += each.time_stamp;
     }
     let average_weather_value = total_weather_value / (data.len() as f64);
-    Result { weather_value: (average_weather_value * 100.0) as u64 }
+    let avg_time_stamp = time_stamp_acc / (data.len() as u64);
+    Result { weather_value: (average_weather_value * 100.0) as u64, time_stamp: avg_time_stamp }
 }
 
 #[cfg(test)]
@@ -41,9 +47,12 @@ mod tests {
             key: String::from("main"),
             sub_key: String::from("temp"),
         };
-        let data1 = Data { weather_value: 100.0 };
-        let data2 = Data { weather_value: 200.0 };
-        assert_eq!(execute(params, vec![data1, data2]), Result { weather_value: 15000 });
+        let data1 = Data { weather_value: 100.0, time_stamp: 10 };
+        let data2 = Data { weather_value: 200.0, time_stamp: 12 };
+        assert_eq!(
+            execute(params, vec![data1, data2]),
+            Result { weather_value: 15000, time_stamp: 11 }
+        );
     }
 
     #[test]
