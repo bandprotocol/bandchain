bdae4871cc175d8cbf826c68d5d928573a6f5986
prin-r <q@bandprotocol.com>
2020-02-11 20:29:53 +0700

    fix typo time_stamp -> timestamp

--- owasm/examples/cryptocompare_price/src/logic.rs
+++ owasm/examples/cryptocompare_price/src/logic.rs
@@ -11,30 +11,27 @@ decl_params! {
 decl_data! {
     pub struct Data {
         pub crypto_compare_price: f32 = |params: &Parameter| cryptocompare::Price::new(&params.crypto_symbol),
-        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
+        pub timestamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub crypto_price_in_usd: u64,
-        pub time_stamp: u64,
+        pub timestamp: u64,
     }
 }
 
 pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
     let mut total_crypto_price = 0.0;
-    let mut time_stamp_acc: u64 = 0;
+    let mut timestamp_acc: u64 = 0;
     for each in &data {
         total_crypto_price += each.crypto_compare_price;
-        time_stamp_acc += each.time_stamp;
+        timestamp_acc += each.timestamp;
     }
     let average_crypto_price = total_crypto_price / (data.len() as f32);
-    let avg_time_stamp = time_stamp_acc / (data.len() as u64);
-    Result {
-        crypto_price_in_usd: (average_crypto_price * 100.0) as u64,
-        time_stamp: avg_time_stamp,
-    }
+    let avg_timestamp = timestamp_acc / (data.len() as u64);
+    Result { crypto_price_in_usd: (average_crypto_price * 100.0) as u64, timestamp: avg_timestamp }
 }
 
 #[cfg(test)]
@@ -44,11 +41,11 @@ mod tests {
     #[test]
     fn test_execute() {
         let params = Parameter { crypto_symbol: coins::Coins::ETH };
-        let data1 = Data { crypto_compare_price: 100.0, time_stamp: 10 };
-        let data2 = Data { crypto_compare_price: 200.0, time_stamp: 12 };
+        let data1 = Data { crypto_compare_price: 100.0, timestamp: 10 };
+        let data2 = Data { crypto_compare_price: 200.0, timestamp: 12 };
         assert_eq!(
             execute(params, vec![data1, data2]),
-            Result { crypto_price_in_usd: 15000, time_stamp: 11 }
+            Result { crypto_price_in_usd: 15000, timestamp: 11 }
         );
     }
 
