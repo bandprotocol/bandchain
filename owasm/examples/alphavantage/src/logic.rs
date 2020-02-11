f107cd0050f91b4a9b1f2c79737cdc740bd9f4ba
prin-r <q@bandprotocol.com>
2020-02-11 20:29:27 +0700

    fix typo time_stamp -> timestamp

--- owasm/examples/alphavantage/src/logic.rs
+++ owasm/examples/alphavantage/src/logic.rs
@@ -11,27 +11,27 @@ decl_params! {
 decl_data! {
     pub struct Data {
         pub alphavantage_price: f32 = |params: &Parameter| alphavantage::Price::new(&params.stock_symbol, "WVKPOO76169EX950"),
-        pub time_stamp: u64 = |_: &Parameter| date::Date::new(),
+        pub timestamp: u64 = |_: &Parameter| date::Date::new(),
     }
 }
 
 decl_result! {
     pub struct Result {
         pub stock_price_in_usd: u64,
-        pub time_stamp: u64,
+        pub timestamp: u64,
     }
 }
 
 pub fn execute(_params: Parameter, data: Vec<Data>) -> Result {
     let mut total_stock_price = 0.0;
-    let mut time_stamp_acc: u64 = 0;
+    let mut timestamp_acc: u64 = 0;
     for each in &data {
         total_stock_price += each.alphavantage_price;
-        time_stamp_acc += each.time_stamp;
+        timestamp_acc += each.timestamp;
     }
     let average_stock_price = total_stock_price / (data.len() as f32);
-    let avg_time_stamp = time_stamp_acc / (data.len() as u64);
-    Result { stock_price_in_usd: (average_stock_price * 100.0) as u64, time_stamp: avg_time_stamp }
+    let avg_timestamp = timestamp_acc / (data.len() as u64);
+    Result { stock_price_in_usd: (average_stock_price * 100.0) as u64, timestamp: avg_timestamp }
 }
 
 #[cfg(test)]
@@ -41,11 +41,11 @@ mod tests {
     #[test]
     fn test_execute() {
         let params = Parameter { stock_symbol: String::from("FB") };
-        let data1 = Data { alphavantage_price: 100.0, time_stamp: 10 };
-        let data2 = Data { alphavantage_price: 200.0, time_stamp: 12 };
+        let data1 = Data { alphavantage_price: 100.0, timestamp: 10 };
+        let data2 = Data { alphavantage_price: 200.0, timestamp: 12 };
         assert_eq!(
             execute(params, vec![data1, data2]),
-            Result { stock_price_in_usd: 15000, time_stamp: 11 }
+            Result { stock_price_in_usd: 15000, timestamp: 11 }
         );
     }
 
