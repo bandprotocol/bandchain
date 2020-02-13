40551a0b97eee9e23151c085f55d4008930f68a5
prin-r <q@bandprotocol.com>
2020-02-13 21:54:08 +0700

    fill in fucntions InitGenesis and ExportGenesis

--- chain/x/zoracle/genesis.go
+++ chain/x/zoracle/genesis.go
@@ -6,7 +6,7 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-// GenesisState is the bank state that must be provided at genesis.
+// GenesisState is the zoracle state that must be provided at genesis.
 type GenesisState struct {
 	// Scripts []types.StoredCode `json:"scripts"`
 	Params types.Params `json:"params" yaml:"params"` // inflation params
@@ -41,28 +41,18 @@ func DefaultGenesisState() GenesisState {
 	}
 }
 
-func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) []abci.ValidatorUpdate {
-	// for _, storeCode := range data.Scripts
-	// 	codeHash := storeCode.GetCodeHash()
-	// 	if keeper.CheckCodeHashExists(ctx, codeHash) {
-	// 		continue
-	// 	}
-	// 	keeper.SetCode(ctx, storeCode.Code, storeCode.Name, storeCode.Owner)
-	// }
+func InitGenesis(ctx sdk.Context, k Keeper, data GenesisState) []abci.ValidatorUpdate {
+	k.SetMaxDataSourceExecutableSize(ctx, data.Params.MaxDataSourceExecutableSize)
+	k.SetMaxOracleScriptCodeSize(ctx, data.Params.MaxOracleScriptCodeSize)
+	k.SetMaxCalldataSize(ctx, data.Params.MaxCalldataSize)
+	k.SetMaxDataSourceCountPerRequest(ctx, data.Params.MaxDataSourceCountPerRequest)
+	k.SetMaxRawDataReportSize(ctx, data.Params.MaxRawDataReportSize)
 
 	return []abci.ValidatorUpdate{}
 }
 
 func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
-	// var records []Whois
-	// iterator := k.GetNamesIterator(ctx)
-	// for ; iterator.Valid(); iterator.Next() {
-
-	// 	name := string(iterator.Key())
-	// 	whois := k.GetWhois(ctx, name)
-	// 	records = append(records, whois)
-
-	// }
-	// return GenesisState{WhoisRecords: records}
-	return GenesisState{}
+	return GenesisState{
+		Params: k.GetParams(ctx),
+	}
 }
