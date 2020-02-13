0d04fab4d80f862849543683d09c902fa4943656
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-02-13 10:38:10 +0700

    Use require.Nil instead of panicing on error

--- chain/owasm/execute_test.go
+++ chain/owasm/execute_test.go
@@ -10,9 +10,7 @@ import (
 
 func TestExecuteCanCallEnv(t *testing.T) {
 	code, err := ioutil.ReadFile("./res/main.wasm")
-	if err != nil {
-		panic(err)
-	}
+	require.Nil(t, err)
 	result, gasUsed, err := Execute(&mockExecutionEnvironment{}, code, "execute", []byte{}, 10000)
 	if err != nil {
 		panic(err)
@@ -23,33 +21,25 @@ func TestExecuteCanCallEnv(t *testing.T) {
 
 func TestExecuteOutOfGas(t *testing.T) {
 	code, err := ioutil.ReadFile("./res/main.wasm")
-	if err != nil {
-		panic(err)
-	}
+	require.Nil(t, err)
 	_, _, err = Execute(&mockExecutionEnvironment{}, code, "execute", []byte{}, 10)
 	require.EqualError(t, err, "gas limit exceeded")
 }
 
 func TestExecuteEndToEnd(t *testing.T) {
 	code, err := ioutil.ReadFile("./res/silly.wasm")
-	if err != nil {
-		panic(err)
-	}
+	require.Nil(t, err)
 	env := &mockExecutionEnvironment{
 		externalDataResults: [][][]byte{nil, {[]byte("RETURN_DATA")}},
 	}
 
 	// It should log "RequestExternalData: DataSourceID = 1, ExternalDataID = 1"
 	_, _, err = Execute(env, code, "prepare", []byte{}, 10000)
-	if err != nil {
-		panic(err)
-	}
+	require.Nil(t, err)
 
 	// It should return "RETURN_DATA" as the code return data from externalID = 1, validatorIndex = 0
 	result, gasUsed, err := Execute(env, code, "execute", []byte{}, 10000)
-	if err != nil {
-		panic(err)
-	}
+	require.Nil(t, err)
 	require.Equal(t, []byte("RETURN_DATA"), result)
 	require.Equal(t, int64(1061), gasUsed)
 }
