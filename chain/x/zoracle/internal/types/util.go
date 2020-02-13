dbee9f67653194286c4ce414a33321c373e4d7d9
kanisorn <kanisorn.peach@gmail.com>
2020-02-13 14:07:12 +0700

    int64ToByte utils

--- chain/x/zoracle/internal/types/util.go
+++ chain/x/zoracle/internal/types/util.go
@@ -14,3 +14,13 @@ func uint64ToBytes(num uint64) []byte {
 	}
 	return buf.Bytes()
 }
+
+func int64ToBytes(num int64) []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, num)
+	// Encode int64 must not have error
+	if err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
