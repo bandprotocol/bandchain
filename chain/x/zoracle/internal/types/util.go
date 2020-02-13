c035ba640bb4ac684ac0060246ae98f0633b3366
taobun <taobunoi@gmail.com>
2020-02-13 15:31:45 +0700

    Fix int64ToBytes

--- chain/x/zoracle/internal/types/util.go
+++ chain/x/zoracle/internal/types/util.go
@@ -1,19 +1,8 @@
 package types
 
 import (
-	"bytes"
 	"encoding/binary"
 )
-
-func int64ToBytes(num int64) []byte {
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, num)
-	// Encode int64 must not have error
-	if err != nil {
-		panic(err)
-	}
-	return buf.Bytes()
-}
 
 func int64ToBytes(num int64) []byte {
 	result := make([]byte, 8)
