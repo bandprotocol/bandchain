5aaf8bb31b4a040a0a6a7c999d6acbc828553ba3
kanisorn <kanisorn.peach@gmail.com>
2020-02-13 15:09:58 +0700

    use PutUint64 instead of Write in util

--- chain/x/zoracle/internal/types/util.go
+++ chain/x/zoracle/internal/types/util.go
@@ -16,11 +16,7 @@ func uint64ToBytes(num uint64) []byte {
 }
 
 func int64ToBytes(num int64) []byte {
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, num)
-	// Encode int64 must not have error
-	if err != nil {
-		panic(err)
-	}
-	return buf.Bytes()
+	result := make([]byte, 8)
+	binary.BigEndian.PutUint64(result, uint64(num))
+	return result
 }
