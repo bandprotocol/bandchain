c1f5b749f12912fd51f9e1e5583ee8d5c47af38b
prin-r <q@bandprotocol.com>
2020-02-14 17:16:24 +0700

    add script for testing

---
+++ chain/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd owasm/res
+
+for f in *; do
+    if [ -d "$f" ]; then
+        wasm-pack build $f
+        cp ./$f/target/wasm32-unknown-unknown/release/*.wasm ./
+    fi
+done
+
+cd ../../
+go test ./...
