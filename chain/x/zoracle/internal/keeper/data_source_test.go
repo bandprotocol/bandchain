e35e750f8daaf69819a8f516b64869d52ce41d7f
prin-r <q@bandprotocol.com>
2020-02-18 15:26:56 +0700

    add test when Add/Edit TooLongDataSource

--- chain/x/zoracle/internal/keeper/data_source_test.go
+++ chain/x/zoracle/internal/keeper/data_source_test.go
@@ -11,6 +11,8 @@ func mockDataSource(ctx sdk.Context, keeper Keeper) sdk.Error {
 	owner := sdk.AccAddress([]byte("owner"))
 	name := "data_source"
 	fee := sdk.NewCoins(sdk.NewInt64Coin("uband", 10))
+
+	// This executable size is 10 bytes.
 	executable := []byte("executable")
 	return keeper.AddDataSource(ctx, owner, name, fee, executable)
 }
@@ -21,8 +23,11 @@ func TestGetterSetterDataSource(t *testing.T) {
 	_, err := keeper.GetDataSource(ctx, 1)
 	require.NotNil(t, err)
 
+	// Set MaxDataSourceExecutableSize to 20
+	keeper.SetMaxDataSourceExecutableSize(ctx, 20)
 	err = mockDataSource(ctx, keeper)
 	require.Nil(t, err)
+
 	actualDataSource, err := keeper.GetDataSource(ctx, 1)
 	require.Nil(t, err)
 	require.Equal(t, sdk.AccAddress([]byte("owner")), actualDataSource.Owner)
@@ -31,9 +36,29 @@ func TestGetterSetterDataSource(t *testing.T) {
 	require.Equal(t, []byte("executable"), actualDataSource.Executable)
 }
 
+func TestAddTooLongDataSource(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+
+	_, err := keeper.GetDataSource(ctx, 1)
+	require.NotNil(t, err)
+
+	// Set MaxDataSourceExecutableSize to 20
+	keeper.SetMaxDataSourceExecutableSize(ctx, 20)
+
+	owner := sdk.AccAddress([]byte("owner"))
+	name := "data_source"
+	fee := sdk.NewCoins(sdk.NewInt64Coin("uband", 10))
+	tooLongExecutable := []byte("The number of bytes of this data source is 80 which is obviously longer than 20.")
+
+	err = keeper.AddDataSource(ctx, owner, name, fee, tooLongExecutable)
+	require.NotNil(t, err)
+}
+
 func TestEditDataSource(t *testing.T) {
 	ctx, keeper := CreateTestInput(t, false)
 
+	// Set MaxDataSourceExecutableSize to 20
+	keeper.SetMaxDataSourceExecutableSize(ctx, 20)
 	err := mockDataSource(ctx, keeper)
 	require.Nil(t, err)
 
@@ -51,4 +76,21 @@ func TestEditDataSource(t *testing.T) {
 	require.Equal(t, newName, actualDataSource.Name)
 	require.Equal(t, newFee, actualDataSource.Fee)
 	require.Equal(t, newExecutable, actualDataSource.Executable)
+}
+
+func TestEditTooLongDataSource(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+
+	// Set MaxDataSourceExecutableSize to 20
+	keeper.SetMaxDataSourceExecutableSize(ctx, 20)
+	err := mockDataSource(ctx, keeper)
+	require.Nil(t, err)
+
+	newOwner := sdk.AccAddress([]byte("owner2"))
+	newName := "data_source_2"
+	newFee := sdk.NewCoins(sdk.NewInt64Coin("uband", 1))
+	newTooLongExecutable := []byte("The number of bytes of this data source is 80 which is obviously longer than 20.")
+
+	err = keeper.EditDataSource(ctx, 1, newOwner, newName, newFee, newTooLongExecutable)
+	require.NotNil(t, err)
 }
