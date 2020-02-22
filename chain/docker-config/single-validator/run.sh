de76e3b0b8927b75e5104a994059ccaee2505627
kanisorn <kanisorn.peach@gmail.com>
2020-02-23 02:53:35 +0700

    add validator state

--- chain/docker-config/single-validator/run.sh
+++ chain/docker-config/single-validator/run.sh
@@ -2,6 +2,7 @@
 
 bandd init validator --chain-id=bandchain
 
+cp /zoracle/docker-config/single-validator/priv_validator_state.json ~/.bandd/config/priv_validator_state.json
 cp /zoracle/docker-config/single-validator/priv_validator_key.json ~/.bandd/config/priv_validator_key.json
 cp /zoracle/docker-config/single-validator/node_key.json ~/.bandd/config/node_key.json
 cp /zoracle/docker-config/single-validator/genesis.json ~/.bandd/config/genesis.json
