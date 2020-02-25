abac0ef391490bacaed384ce6bea50bbb6b7e008
kanisorn <kanisorn.peach@gmail.com>
2020-02-25 15:26:52 +0700

    implement AddResult function and test case in result keeper

--- chain/x/zoracle/internal/keeper/result_test.go
+++ chain/x/zoracle/internal/keeper/result_test.go
@@ -17,3 +17,29 @@ func TestGetterSetterResult(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, []byte("result"), actualResult)
 }
+
+func TestAddResultSuccess(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+
+	_, err := keeper.GetResult(ctx, 1, 1, []byte("calldata"))
+	require.NotNil(t, err)
+
+	keeper.SetMaxResultSize(ctx, int64(1024))
+	err = keeper.AddResult(ctx, 1, 1, []byte("calldata"), []byte("result"))
+	require.Nil(t, err)
+
+	actualResult, err := keeper.GetResult(ctx, 1, 1, []byte("calldata"))
+	require.Nil(t, err)
+	require.Equal(t, []byte("result"), actualResult)
+}
+
+func TestAddResultFailWithExceedResultSize(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+
+	_, err := keeper.GetResult(ctx, 1, 1, []byte("calldata"))
+	require.NotNil(t, err)
+
+	keeper.SetMaxResultSize(ctx, int64(1))
+	err = keeper.AddResult(ctx, 1, 1, []byte("calldata"), []byte("result"))
+	require.NotNil(t, err)
+}
