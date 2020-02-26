6440d14d7462bb4640a56258f984bf762ca76660
kanisorn <kanisorn.peach@gmail.com>
2020-02-26 22:12:00 +0700

    consoume gas before run owasm.Execute

--- chain/x/zoracle/handler.go
+++ chain/x/zoracle/handler.go
@@ -151,13 +151,14 @@ func handleMsgRequestData(ctx sdk.Context, keeper Keeper, msg MsgRequestData) sd
 	if err != nil {
 		return err.Result()
 	}
+
+	ctx.GasMeter().ConsumeGas(msg.PrepareGas, "PrepareRequest")
+
 	_, _, errOwasm := owasm.Execute(&env, script.Code, "prepare", msg.Calldata, msg.PrepareGas)
 	if errOwasm != nil {
 		// TODO: error
 		return sdk.ErrUnknownRequest(errOwasm.Error()).Result()
 	}
-
-	ctx.GasMeter().ConsumeGas(msg.PrepareGas, "PrepareRequest")
 
 	err = keeper.ValidateDataSourceCount(ctx, id)
 	if err != nil {
