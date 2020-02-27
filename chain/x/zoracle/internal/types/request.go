ae1cc646f3def17c98d49c7a0f0345568a1c2d91
prin-r <q@bandprotocol.com>
2020-02-27 14:42:28 +0700

    add function NewResult

--- chain/x/zoracle/internal/types/request.go
+++ chain/x/zoracle/internal/types/request.go
@@ -18,15 +18,6 @@ type Request struct {
 	IsResolved               bool             `json:"isResolved"`
 }
 
-// Result is a data structure that stores the detail of a result of a specific request.
-type Result struct {
-	RequestTime              int64 `json:"requestTime"`
-	AggregationTime          int64 `json:"aggregationTime"`
-	RequestedValidatorsCount int64 `json:"requestedValidatorsCount"`
-	SufficientValidatorCount int64 `json:"sufficientValidatorCount"`
-	ReportedValidatorsCount  int64 `json:"reportedValidatorsCount"`
-}
-
 // NewRequest creates a new Request instance.
 func NewRequest(
 	oracleScriptID int64,
@@ -47,6 +38,35 @@ func NewRequest(
 		RequestTime:              requestTime,
 		ExpirationHeight:         expirationHeight,
 		ExecuteGas:               executeGas,
+	}
+}
+
+// Result is a data structure that stores the detail of a result of a specific request.
+type Result struct {
+	RequestTime              int64  `json:"requestTime"`
+	AggregationTime          int64  `json:"aggregationTime"`
+	RequestedValidatorsCount int64  `json:"requestedValidatorsCount"`
+	SufficientValidatorCount int64  `json:"sufficientValidatorCount"`
+	ReportedValidatorsCount  int64  `json:"reportedValidatorsCount"`
+	Data                     []byte `json:"data"`
+}
+
+// NewResult creates a new Result instance.
+func NewResult(
+	requestTime int64,
+	aggregationTime int64,
+	requestedValidatorsCount int64,
+	sufficientValidatorCount int64,
+	reportedValidatorsCount int64,
+	data []byte,
+) Result {
+	return Result{
+		RequestTime:              requestTime,
+		AggregationTime:          aggregationTime,
+		RequestedValidatorsCount: requestedValidatorsCount,
+		SufficientValidatorCount: sufficientValidatorCount,
+		ReportedValidatorsCount:  reportedValidatorsCount,
+		Data:                     data,
 	}
 }
 
