746c5551a535fcf660445c96d2ed1c31270c1ad8
prin-r <q@bandprotocol.com>
2020-02-27 14:46:38 +0700

    remove result out of request

--- chain/x/zoracle/internal/types/request.go
+++ chain/x/zoracle/internal/types/request.go
@@ -41,35 +41,6 @@ func NewRequest(
 	}
 }
 
-// Result is a data structure that stores the detail of a result of a specific request.
-type Result struct {
-	RequestTime              int64  `json:"requestTime"`
-	AggregationTime          int64  `json:"aggregationTime"`
-	RequestedValidatorsCount int64  `json:"requestedValidatorsCount"`
-	SufficientValidatorCount int64  `json:"sufficientValidatorCount"`
-	ReportedValidatorsCount  int64  `json:"reportedValidatorsCount"`
-	Data                     []byte `json:"data"`
-}
-
-// NewResult creates a new Result instance.
-func NewResult(
-	requestTime int64,
-	aggregationTime int64,
-	requestedValidatorsCount int64,
-	sufficientValidatorCount int64,
-	reportedValidatorsCount int64,
-	data []byte,
-) Result {
-	return Result{
-		RequestTime:              requestTime,
-		AggregationTime:          aggregationTime,
-		RequestedValidatorsCount: requestedValidatorsCount,
-		SufficientValidatorCount: sufficientValidatorCount,
-		ReportedValidatorsCount:  reportedValidatorsCount,
-		Data:                     data,
-	}
-}
-
 // RawDataRequest is a data structure that store what datasource and calldata will be used in request.
 type RawDataRequest struct {
 	DataSourceID int64  `json:"dataSourceID"`
