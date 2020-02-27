9da2254d04ceacbb9b4285cd9343e11fed1dde23
prin-r <q@bandprotocol.com>
2020-02-27 21:56:59 +0700

    add encode

--- chain/x/zoracle/internal/types/result.go
+++ chain/x/zoracle/internal/types/result.go
@@ -1,6 +1,8 @@
 package types
 
-// Result is a data structure that stores the detail of a result of a specific request.
+import "encoding/binary"
+
+// Result is a data structure that stores the detail of the result of a specific request.
 type Result struct {
 	RequestTime              int64  `json:"requestTime"`
 	AggregationTime          int64  `json:"aggregationTime"`
@@ -27,4 +29,32 @@ func NewResult(
 		ReportedValidatorsCount:  reportedValidatorsCount,
 		Data:                     data,
 	}
+}
+
+// DecodeResult is a helper function for decoding bytes to Result.
+func DecodeResult(b []byte) Result {
+	if len(b) < 40 {
+		return Result{}
+	}
+	return Result{
+		RequestTime:              int64(binary.BigEndian.Uint64(b[0:8])),
+		AggregationTime:          int64(binary.BigEndian.Uint64(b[8:16])),
+		RequestedValidatorsCount: int64(binary.BigEndian.Uint64(b[16:24])),
+		SufficientValidatorCount: int64(binary.BigEndian.Uint64(b[24:32])),
+		ReportedValidatorsCount:  int64(binary.BigEndian.Uint64(b[32:40])),
+		Data:                     b[40:],
+	}
+}
+
+// Bytes is a helper function for encoding Result to bytes.
+func (result Result) Bytes() []byte {
+	bs := make([]byte, 40)
+
+	binary.BigEndian.PutUint64(bs[0:8], uint64(result.RequestTime))
+	binary.BigEndian.PutUint64(bs[8:16], uint64(result.AggregationTime))
+	binary.BigEndian.PutUint64(bs[16:24], uint64(result.RequestedValidatorsCount))
+	binary.BigEndian.PutUint64(bs[24:32], uint64(result.SufficientValidatorCount))
+	binary.BigEndian.PutUint64(bs[32:40], uint64(result.ReportedValidatorsCount))
+
+	return append(bs, result.Data...)
 }
