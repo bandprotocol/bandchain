09fd0bef7a432fbb3c27fa79cfed15929cdf8245
prin-r <q@bandprotocol.com>
2020-02-28 19:28:49 +0700

    add wrapped result strcut

--- bridges/evm/contracts/BandChainLib.sol
+++ bridges/evm/contracts/BandChainLib.sol
@@ -9,19 +9,6 @@ library BandChainLib {
         uint64[] memory result = new uint64[](_data.length / 8);
         require(result.length * 8 == _data.length, "DATA_LENGTH_IS_INVALID");
 
-    struct Result {
-      uint64 requestTime;
-			uint64 aggregationTime;
-			uint64 requestedValidatorsCount;
-			uint64 sufficientValidatorCount;
-			uint64 reportedValidatorsCount;
-			bytes data;
-    }
-
-  function toUint64List(bytes memory _data) internal pure returns(uint64[] memory) {
-    uint64[] memory result = new uint64[](_data.length / 8);
-    require(result.length * 8 == _data.length, "DATA_LENGTH_IS_INVALID");
-
         for (uint256 i = 0; i < result.length; i++) {
             uint64 tmp;
             assembly {
