ae8c4ca1fa9844f5e10a887c7c4f882d3cfa9c24
prin-r <q@bandprotocol.com>
2020-02-28 22:34:08 +0700

    add new function for getting validators with ethereum address

---
+++ chain/client/validators.go
@@ -0,0 +1,70 @@
+package rpc
+
+import (
+	"crypto/elliptic"
+	"fmt"
+	"net/http"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	secptm "github.com/tendermint/tendermint/crypto/secp256k1"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+
+	// unnamed import of statik for swagger UI support
+	_ "github.com/bandprotocol/d3n/chain/client/lcd/statik"
+)
+
+type ValidatorMinimal struct {
+	Address     string `json:"address"`
+	VotingPower int64  `json:"voting_power"`
+}
+
+type ValidatorsOnEthereum struct {
+	BlockHeight int64              `json:"block_height"`
+	Validators  []ValidatorMinimal `json:"validators"`
+}
+
+func GetValidators(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		node, err := cliCtx.GetNode()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		validators, err := node.Validators(nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		validatorsOnETH := ValidatorsOnEthereum{}
+		validatorsOnETH.BlockHeight = validators.BlockHeight
+		validatorsOnETH.Validators = []ValidatorMinimal{}
+
+		for _, validator := range validators.Validators {
+			pubKeyBytes, ok := validator.PubKey.(secptm.PubKeySecp256k1)
+			if !ok {
+				rest.WriteErrorResponse(w, http.StatusInternalServerError, "fail to cast pubkey")
+				return
+			}
+			if pubkey1, err := crypto.DecompressPubkey(pubKeyBytes[:]); err != nil {
+				rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			} else {
+				pubkey := elliptic.Marshal(secp256k1.S256(), pubkey1.X, pubkey1.Y)
+				validatorsOnETH.Validators = append(
+					validatorsOnETH.Validators,
+					ValidatorMinimal{
+						Address:     fmt.Sprintf("0x%x", crypto.Keccak256(pubkey[1:])[12:]),
+						VotingPower: validator.VotingPower,
+					},
+				)
+			}
+		}
+
+		rest.PostProcessResponseBare(w, cliCtx, validatorsOnETH)
+	}
+}
