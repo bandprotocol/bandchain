dec8e009ee6cfa396afde1b71a977dce4388c362
prin-r <q@bandprotocol.com>
2020-02-29 18:16:39 +0700

    remove WrappedResult and move its children in to VerifyOracleDataResult

--- bridges/evm/contracts/IBridge.sol
+++ bridges/evm/contracts/IBridge.sol
@@ -19,6 +19,18 @@ interface IBridge {
         bytes params;
     }
 
+    /// Helper struct to help the function caller to decode oracle data.
+    struct VerifyOracleDataResult {
+        uint64 oracleScriptId;
+        uint64 requestTime;
+        uint64 aggregationTime;
+        uint64 requestedValidatorsCount;
+        uint64 sufficientValidatorCount;
+        uint64 reportedValidatorsCount;
+        bytes params;
+        bytes data;
+    }
+
     /// Performs oracle state relay and oracle data verification in one go. The caller submits
     /// the encoded proof and receives back the decoded data, ready to be validated and used.
     /// @param _data The encoded data for oracle state relay and data verification.
