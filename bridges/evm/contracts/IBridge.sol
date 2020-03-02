15ce4dfca1b096a1561b5d7c92f0a3f0ba9d045d
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-03-02 14:47:44 +0700

    bridge/evm: Reformat solidity code

--- bridges/evm/contracts/IBridge.sol
+++ bridges/evm/contracts/IBridge.sol
@@ -2,17 +2,17 @@ pragma solidity 0.5.14;
 pragma experimental ABIEncoderV2;
 
 interface IBridge {
-  /// Helper struct to help the function caller to decode oracle data.
-  struct VerifyOracleDataResult {
-    bytes data;
-    uint64 oracleScriptId;
-    bytes params;
-  }
+    /// Helper struct to help the function caller to decode oracle data.
+    struct VerifyOracleDataResult {
+        bytes data;
+        uint64 oracleScriptId;
+        bytes params;
+    }
 
-  /// Performs oracle state relay and oracle data verification in one go. The caller submits
-  /// the encoded proof and receives back the decoded data, ready to be validated and used.
-  /// @param _data The encoded data for oracle state relay and data verification.
-  function relayAndVerify(bytes calldata _data)
-    external
-    returns (VerifyOracleDataResult memory result);
+    /// Performs oracle state relay and oracle data verification in one go. The caller submits
+    /// the encoded proof and receives back the decoded data, ready to be validated and used.
+    /// @param _data The encoded data for oracle state relay and data verification.
+    function relayAndVerify(bytes calldata _data)
+        external
+        returns (VerifyOracleDataResult memory result);
 }
