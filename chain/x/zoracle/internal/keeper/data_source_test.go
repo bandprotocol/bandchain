fcf8e54f3aa4a406c077b832b35a107e63305c1a
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-03-06 20:21:15 +0700

    rearrage for consistency code

--- chain/x/zoracle/internal/keeper/data_source_test.go
+++ chain/x/zoracle/internal/keeper/data_source_test.go
@@ -54,6 +54,43 @@ func TestAddTooLongDataSource(t *testing.T) {
 	require.NotNil(t, err)
 }
 
+func TestAddTooLongDataSourceName(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+
+	_, err := keeper.GetDataSource(ctx, 1)
+	require.NotNil(t, err)
+
+	// Set MaxNameLength to 5
+	keeper.SetMaxNameLength(ctx, 5)
+
+	owner := sdk.AccAddress([]byte("owner"))
+	tooLongName := "data_source"
+	description := "description"
+	fee := sdk.NewCoins(sdk.NewInt64Coin("uband", 10))
+	executable := []byte("executable")
+
+	err = keeper.AddDataSource(ctx, owner, tooLongName, description, fee, executable)
+	require.NotNil(t, err)
+}
+
+func TestAddTooLongDataSourceDescription(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+
+	_, err := keeper.GetDataSource(ctx, 1)
+	require.NotNil(t, err)
+
+	// Set MaxDescriptionLength to 5
+	keeper.SetMaxDescriptionLength(ctx, 5)
+
+	owner := sdk.AccAddress([]byte("owner"))
+	name := "data_source"
+	tooLongDescription := "description"
+	fee := sdk.NewCoins(sdk.NewInt64Coin("uband", 10))
+	executable := []byte("executable")
+
+	err = keeper.AddDataSource(ctx, owner, name, tooLongDescription, fee, executable)
+	require.NotNil(t, err)
+}
 func TestEditDataSource(t *testing.T) {
 	ctx, keeper := CreateTestInput(t, false)
 
@@ -93,44 +130,6 @@ func TestEditTooLongDataSource(t *testing.T) {
 	newTooLongExecutable := []byte("The number of bytes of this data source is 80 which is obviously longer than 20.")
 
 	err = keeper.EditDataSource(ctx, 1, newOwner, newName, newDescription, newFee, newTooLongExecutable)
-	require.NotNil(t, err)
-}
-
-func TestAddTooLongDataSourceName(t *testing.T) {
-	ctx, keeper := CreateTestInput(t, false)
-
-	_, err := keeper.GetDataSource(ctx, 1)
-	require.NotNil(t, err)
-
-	// Set MaxNameLength to 5
-	keeper.SetMaxNameLength(ctx, 5)
-
-	owner := sdk.AccAddress([]byte("owner"))
-	tooLongName := "data_source"
-	description := "description"
-	fee := sdk.NewCoins(sdk.NewInt64Coin("uband", 10))
-	executable := []byte("executable")
-
-	err = keeper.AddDataSource(ctx, owner, tooLongName, description, fee, executable)
-	require.NotNil(t, err)
-}
-
-func TestAddTooLongDataSourceDescription(t *testing.T) {
-	ctx, keeper := CreateTestInput(t, false)
-
-	_, err := keeper.GetDataSource(ctx, 1)
-	require.NotNil(t, err)
-
-	// Set MaxDescriptionLength to 5
-	keeper.SetMaxDescriptionLength(ctx, 5)
-
-	owner := sdk.AccAddress([]byte("owner"))
-	name := "data_source"
-	tooLongDescription := "description"
-	fee := sdk.NewCoins(sdk.NewInt64Coin("uband", 10))
-	executable := []byte("executable")
-
-	err = keeper.AddDataSource(ctx, owner, name, tooLongDescription, fee, executable)
 	require.NotNil(t, err)
 }
 
