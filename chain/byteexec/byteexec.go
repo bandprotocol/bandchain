f9f195e546ba219aa50c3abb8a8152151e1c0842
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-03-10 20:26:57 +0700

    if error reture empty string

--- chain/byteexec/byteexec.go
+++ chain/byteexec/byteexec.go
@@ -38,7 +38,7 @@ func writeFile(executable []byte) (string, string, error) {
 func RunOnLocal(executable []byte, timeOut time.Duration, arg string) ([]byte, error) {
 	args, err := shellwords.Parse(arg)
 	if err != nil {
-		return []byte{0}, err
+		return []byte{}, err
 	}
 
 	dir, filename, err := writeFile(executable)
@@ -59,7 +59,7 @@ func RunOnLocal(executable []byte, timeOut time.Duration, arg string) ([]byte, e
 func RunOnDocker(executable []byte, timeOut time.Duration, arg string) ([]byte, error) {
 	args, err := shellwords.Parse(arg)
 	if err != nil {
-		return []byte{0}, err
+		return []byte{}, err
 	}
 
 	dir, filename, err := writeFile(executable)
