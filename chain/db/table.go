c6e39cc499e0f91e38de1b50fede698d10fd16f3
taobun <bun@bandprotocol.com>
2020-03-19 09:26:16 +0000

    Define table for validator

--- chain/db/table.go
+++ chain/db/table.go
@@ -13,3 +13,17 @@ type Event struct {
 	gorm.Model
 	Name string
 }
+
+type ValidatorStatus struct {
+	gorm.Model
+	ValidatorAddress string
+	ElectedCount     uint
+	VotedCount       uint
+	MissedCount      uint
+}
+
+type ValidatorVote struct {
+	ValidatorAddress string `gorm:"primary_key"`
+	Block            int64  `gorm:"primary_key;auto_increment:false"`
+	Voted            bool
+}
