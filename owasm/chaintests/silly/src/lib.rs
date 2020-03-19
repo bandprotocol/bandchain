ff708a53838442f08948846fd5c125634ee0e9b7
kanisorn <kanisorn.peach@gmail.com>
2020-03-19 07:15:59 +0700

    add prepare from more data source

--- owasm/chaintests/silly/src/lib.rs
+++ owasm/chaintests/silly/src/lib.rs
@@ -3,6 +3,7 @@ use owasm::oei;
 #[no_mangle]
 pub fn prepare() {
     oei::request_external_data(1, 1, "band-protocol".as_bytes());
+    oei::request_external_data(2, 2, "band-chain".as_bytes());
 }
 
 #[no_mangle]
