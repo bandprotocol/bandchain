ba0682f63d3193daf19456400d3990e1270ebdcb
prin-r <q@bandprotocol.com>
2020-03-26 15:28:45 +0700

    add handleMsgDelegate

--- chain/db/Delegation.go
+++ chain/db/Delegation.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
 )
 
 func (b *BandDB) AddDelegation(
@@ -14,4 +15,20 @@ func (b *BandDB) AddDelegation(
 		OperatorAddress:  operatorAddress.String(),
 		Amount:           Coin.Amount.ToDec().String(),
 	}).Error
+}
+
+func (b *BandDB) handleMsgDelegate(msg staking.MsgDelegate) error {
+	delegation := Delegation{DelegatorAddress: msg.DelegatorAddress.String(), OperatorAddress: msg.ValidatorAddress.String()}
+	err := b.tx.First(&delegation).Error
+	if err != nil {
+		return b.AddDelegation(msg.DelegatorAddress, msg.ValidatorAddress, msg.Amount)
+	}
+	accumulationAmount, err := sdk.NewDecFromStr(delegation.Amount)
+	if err != nil {
+		return err
+	}
+
+	delegation.Amount = accumulationAmount.Add(msg.Amount.Amount.ToDec()).String()
+
+	return b.tx.Save(&delegation).Error
 }
