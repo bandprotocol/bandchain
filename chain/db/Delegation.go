ef781e4d26ad0755f3005989694eafeef94cd64d
prin-r <q@bandprotocol.com>
2020-03-27 12:14:13 +0700

    remove code inside each functions of Delegation

--- chain/db/Delegation.go
+++ chain/db/Delegation.go
@@ -10,25 +10,11 @@ func (b *BandDB) AddDelegation(
 	operatorAddress sdk.ValAddress,
 	Coin sdk.Coin,
 ) error {
-	return b.tx.Create(&Delegation{
-		DelegatorAddress: delegatorAddress.String(),
-		OperatorAddress:  operatorAddress.String(),
-		Amount:           Coin.Amount.ToDec().String(),
-	}).Error
+	// TODO
+	return nil
 }
 
 func (b *BandDB) handleMsgDelegate(msg staking.MsgDelegate) error {
-	delegation := Delegation{DelegatorAddress: msg.DelegatorAddress.String(), OperatorAddress: msg.ValidatorAddress.String()}
-	err := b.tx.First(&delegation).Error
-	if err != nil {
-		return b.AddDelegation(msg.DelegatorAddress, msg.ValidatorAddress, msg.Amount)
-	}
-	accumulationAmount, err := sdk.NewDecFromStr(delegation.Amount)
-	if err != nil {
-		return err
-	}
-
-	delegation.Amount = accumulationAmount.Add(msg.Amount.Amount.ToDec()).String()
-
-	return b.tx.Save(&delegation).Error
+	// TODO
+	return nil
 }
