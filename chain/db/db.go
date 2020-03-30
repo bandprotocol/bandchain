b07be5f92b5d0f463f7bc9299a6f77d4c1484d87
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-03-30 21:20:47 +0700

    remove duplicate foreign key

--- chain/db/db.go
+++ chain/db/db.go
@@ -176,13 +176,6 @@ func NewDB(dialect, path string, metadata map[string]string) (*BandDB, error) {
 		"RESTRICT",
 	)
 
-	db.Model(&RelatedDataSources{}).AddForeignKey(
-		"oracle_script_id",
-		"oracle_scripts(id)",
-		"RESTRICT",
-		"RESTRICT",
-	)
-
 	for key, value := range metadata {
 		err := db.Where(Metadata{Key: key}).
 			Assign(Metadata{Value: value}).
