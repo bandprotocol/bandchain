c2e583b54e9f710453b1dc458361e30bdfd6d12e
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-03-30 12:07:45 +0700

    Use map to check for external ID uniqueness

--- chain/x/zoracle/internal/types/msgs.go
+++ chain/x/zoracle/internal/types/msgs.go
@@ -135,12 +135,12 @@ func (msg MsgReportData) ValidateBasic() sdk.Error {
 	if msg.DataSet == nil || len(msg.DataSet) == 0 {
 		return ErrInvalidBasicMsg("MsgReportData: Data set must not be empty.")
 	}
-	for idx1, rawReport1 := range msg.DataSet {
-		for _, rawReport2 := range msg.DataSet[idx1+1:] {
-			if rawReport1.ExternalDataID == rawReport2.ExternalDataID {
-				return ErrInvalidBasicMsg("MsgReportData: External IDs in dataset must be unique.")
-			}
+	uniqueMap := make(map[ExternalID]bool)
+	for _, rawReport := range msg.DataSet {
+		if _, found := uniqueMap[rawReport.ExternalDataID]; found {
+			return ErrInvalidBasicMsg("MsgReportData: External IDs in dataset must be unique.")
 		}
+		uniqueMap[rawReport.ExternalDataID] = true
 	}
 	if msg.Validator.Empty() {
 		return ErrInvalidBasicMsg("MsgReportData: Validator address must not be empty.")
