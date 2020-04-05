ed9fc14b423bebae9b4782a7d13e170483fd8911
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-04-05 18:31:11 +0700

    Better error messages

--- chain/x/zoracle/internal/types/error.go
+++ chain/x/zoracle/internal/types/error.go
@@ -5,11 +5,11 @@ import (
 )
 
 var (
-	ErrInvalidBasicMsg        = sdkerrors.Register(ModuleName, 1, "")
-	ErrBadDataValue           = sdkerrors.Register(ModuleName, 2, "")
-	ErrUnauthorizedPermission = sdkerrors.Register(ModuleName, 3, "")
-	ErrItemDuplication        = sdkerrors.Register(ModuleName, 4, "")
-	ErrItemNotFound           = sdkerrors.Register(ModuleName, 5, "")
-	ErrInvalidState           = sdkerrors.Register(ModuleName, 6, "")
-	ErrBadWasmExecution       = sdkerrors.Register(ModuleName, 7, "")
+	ErrInvalidBasicMsg        = sdkerrors.Register(ModuleName, 1, "InvalidBasicMsg")
+	ErrBadDataValue           = sdkerrors.Register(ModuleName, 2, "BadDataValue")
+	ErrUnauthorizedPermission = sdkerrors.Register(ModuleName, 3, "UnauthorizedPermission")
+	ErrItemDuplication        = sdkerrors.Register(ModuleName, 4, "ItemDuplication")
+	ErrItemNotFound           = sdkerrors.Register(ModuleName, 5, "ItemNotFound")
+	ErrInvalidState           = sdkerrors.Register(ModuleName, 6, "InvalidState")
+	ErrBadWasmExecution       = sdkerrors.Register(ModuleName, 7, "BadWasmExecution")
 )
