e2eca1fdecb9a12780f84bb22c470a02d3e2ac60
kanisorn <kanisorn.peach@gmail.com>
2020-04-09 20:19:14 +0700

    add validator's details and website

--- chain/docker-config/generate-genesis.sh
+++ chain/docker-config/generate-genesis.sh
@@ -59,6 +59,8 @@ bandd gentx \
     --node-id 11392b605378063b1c505c0ab123f04bd710d7d7 \
     --pubkey bandvalconspub1addwnpepq06h7wvh5n5pmrejr6t3pyn7ytpwd5c0kmv0wjdfujs847em8dusjl96sxg \
     --name validator1 \
+    --details "Alice's Adventures in Wonderland (commonly shortened to Alice in Wonderland) is an 1865 novel written by English author Charles Lutwidge Dodgson under the pseudonym Lewis Carroll." \
+    --website "https://www.alice.org/" \
     --ip 172.18.0.11 \
     --keyring-backend test
 
@@ -70,6 +72,8 @@ bandd gentx \
     --node-id 0851086afcd835d5a6fb0ffbf96fcdf74fec742e \
     --pubkey bandvalconspub1addwnpepqfey4c5ul6m5juz36z0dlk8gyg6jcnyrvxm4werkgkmcerx8fn5g2gj9q6w \
     --name validator2 \
+    --details "Fish is best known for his appearances with Ring of Honor (ROH) from 2013 to 2017, where he wrestled as one-half of the tag team reDRagon and held the ROH World Tag Team Championship three times and the ROH World Television Championship once." \
+    --website "https://www.wwe.com/superstars/bobby-fish" \
     --ip 172.18.0.12 \
     --keyring-backend test
 
@@ -81,6 +85,8 @@ bandd gentx \
     --node-id 7b58b086dd915a79836eb8bfa956aeb9488d13b0 \
     --pubkey bandvalconspub1addwnpepqwj5l74gfj8j77v8st0gh932s3uyu2yys7n50qf6pptjgwnqu2arxkkn82m \
     --name validator3 \
+    --details "Carol Susan Jane Danvers is a fictional superhero appearing in American comic books published by Marvel Comics. Created by writer Roy Thomas and artist Gene Colan." \
+    --website "https://www.marvel.com/characters/captain-marvel-carol-danvers" \
     --ip 172.18.0.13 \
     --keyring-backend test
 
@@ -92,6 +98,8 @@ bandd gentx \
     --node-id 63808bd64f2ec19acb2a494c8ce8467c595f6fba \
     --pubkey bandvalconspub1addwnpepq0grwz83v8g4s06fusnq5s4jkzxnhgvx67qr5g7v8tx39ur5m8tk7rg2nxj \
     --name validator4 \
+   --details "Evil is an American supernatural drama television series created by Robert King and Michelle King that premiered on September 26, 2019, on CBS. The series is produced by CBS Television Studios and King Size Productions." \
+    --website "https://www.imdb.com/title/tt9055008/" \
     --ip 172.18.0.14 \
     --keyring-backend test
 
