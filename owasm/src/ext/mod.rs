e0cade8c736ae0fce0c8c1464ff2bd822fe810fb
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-04-12 17:47:48 +0700

    implement load majority function and test

--- owasm/src/ext/mod.rs
+++ owasm/src/ext/mod.rs
@@ -41,12 +41,106 @@ where
     sum / num::cast(vals.len()).unwrap()
 }
 
-pub fn load_majority<T>(external_id: i64) -> T
+pub fn load_majority<T>(external_id: i64) -> Option<T>
 where
-    T: std::str::FromStr,
+    T: std::str::FromStr + std::cmp::PartialEq + std::marker::Copy,
 {
-    // TODO
-    oei::get_external_data(external_id, 0).unwrap().parse::<T>().ok().unwrap()
+    let mut vec: Vec<T> = Vec::new();
+    for idx in 0..oei::get_requested_validator_count() {
+        let external_data = oei::get_external_data(external_id, idx);
+        match external_data.and_then(|x| x.parse::<T>().ok()) {
+            Some(v) => {
+                vec.push(v);
+            }
+            None => (),
+        }
+    }
+
+    if vec.len() == 0 {
+        None
+    } else {
+        majority(vec)
+    }
+}
+
+fn majority<T>(vec: Vec<T>) -> Option<T>
+where
+    T: std::str::FromStr + std::cmp::PartialEq + std::marker::Copy,
+{
+    let len = vec.len();
+    let mut candidate = vec[0];
+    let mut count = 0;
+    for (idx, &x) in vec.iter().enumerate() {
+        if idx == 0 {
+            candidate = x;
+            count = 1;
+            continue;
+        }
+
+        if candidate == x {
+            count = count + 1;
+        } else {
+            count = count - 1;
+        }
+        if count == 0 {
+            candidate = x;
+            count = 1;
+        }
+    }
+
+    count = 0;
+    for x in vec {
+        if x == candidate {
+            count = count + 1;
+        }
+    }
+
+    if 2 * count > len {
+        Some(candidate)
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_majority_int() {
+        let vals = vec![1, 2, 3, 1, 3, 1, 1];
+        assert_eq!(majority(vals), Some(1));
+    }
+
+    #[test]
+    fn test_majority_int_result_none() {
+        let vals = vec![1, 2, 3, 1, 3, 1, 1, 3];
+        assert_eq!(majority(vals), None);
+    }
+
+    #[test]
+    fn test_majority_float() {
+        let vals = vec![0.3, 1.0, 0.3, 0.4, 1.0, 1.0, 1.0];
+        assert_eq!(majority(vals), Some(1.0));
+    }
+
+    #[test]
+    fn test_majority_float_result_none() {
+        let vals = vec![0.3, 1.0, 0.3, 0.4, 4.0, 1.0, 99.99, 1.0, 1.0];
+        assert_eq!(majority(vals), None);
+    }
+
+    #[test]
+    fn test_majority_string() {
+        let vals = vec!['a', 'b', 'a', 'b', 'b'];
+        assert_eq!(majority(vals), Some('b'));
+    }
+
+    #[test]
+    fn test_majority_string_result_none() {
+        let vals = vec!['a', 'b', 'a', 'b', 'c', 'b'];
+        assert_eq!(majority(vals), None);
+    }
 }
 
 pub fn load_median<T>(external_id: i64) -> Option<T>
