557c612033f59cac1200afe1f9ac81ce83a8d430
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-04-13 11:10:42 +0700

    fix cmd command to flag

--- chain/cmd/bandevmbot/main.go
+++ chain/cmd/bandevmbot/main.go
@@ -31,8 +31,11 @@ type ValidatorWithPower struct {
 }
 
 func getValidators(nodeURI string) []ValidatorWithPower {
-	node := rpcclient.NewHTTP(nodeURI, "/websocket")
-	validators, err := node.Validators(nil)
+	node, err := rpcclient.NewHTTP(nodeURI, "/websocket")
+	if err != nil {
+		panic(err)
+	}
+	validators, err := node.Validators(nil, 0, 0)
 	if err != nil {
 		panic(err)
 	}
@@ -58,7 +61,7 @@ func getValidators(nodeURI string) []ValidatorWithPower {
 	return vals
 }
 
-func update_validator(rpcURI string, address string, node string, privateKey string, gasPrice uint64) {
+func updateValidators(rpcURI string, address string, node string, privateKey string, gasPrice uint64) {
 	vals := getValidators(node)
 	backgroundCtx := context.Background()
 	contractAddress := common.HexToAddress(address)
@@ -106,35 +109,75 @@ func update_validator(rpcURI string, address string, node string, privateKey str
 
 }
 
+const (
+	flagRPCUri          = "rpc-uri"
+	flagContractAddress = "contract-address"
+	flagNodeUri         = "node-uri"
+	flagPrivKey         = "priv-key"
+	flagGasPrice        = "gas-price"
+	flagPollInterval    = "poll-interval"
+)
+
 func main() {
 
 	cmd := &cobra.Command{
-		Use:   "update-validator [rpc-uri] [contract-address] [node-uri] [priv-key] [gas-price] [poll-interval] ",
+		Use:   "(--rpc-uri [rpc-uri]) (--contract-address [contract-address]) (--node-uri [node-uri]) (--priv-key [priv-key]) (--gas-price [gas-price]) (--poll-interval [poll-interval])",
 		Short: "Periodically update validator set to the destination EVM blockchain",
-		Args:  cobra.ExactArgs(6),
+		Args:  cobra.ExactArgs(0),
 		Long: strings.TrimSpace(
 			fmt.Sprintf(`Periodically update validator set to the destination EVM blockchain
 Example:
-$ update-validator https://kovan.infura.io/v3/d3301689638b40dabad8395bf00d3945 0x0d8152D22a05A3Cf2cE1c5bEfCc2F8658f75a59d http://d3n-debug.bandprotocol.com:26657 AA0C65C16D4B8511C58122966F94192F6963D0EB7896435430BCDFF56E9F13B9 24
+$ --rpc-uri https://kovan.infura.io/v3/d3301689638b40dabad8395bf00d3945 --contract-address 0x0d8152D22a05A3Cf2cE1c5bEfCc2F8658f75a59d --node-uri http://d3n-debug.bandprotocol.com:26657 --priv-key AA0C65C16D4B8511C58122966F94192F6963D0EB7896435430BCDFF56E9F13B9 --gas-price 1000000 --poll-interval 24
 `),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			gasPrice, err := strconv.ParseInt(args[4], 10, 64)
+
+			rpcURI, err := cmd.Flags().GetString(flagRPCUri)
 			if err != nil {
 				return err
 			}
-			interval, err := strconv.ParseInt(args[5], 10, 64)
+			contractAddress, err := cmd.Flags().GetString(flagContractAddress)
+			if err != nil {
+				return err
+			}
+			nodeURI, err := cmd.Flags().GetString(flagNodeUri)
+			if err != nil {
+				return err
+			}
+			privateKey, err := cmd.Flags().GetString(flagPrivKey)
+			if err != nil {
+				return err
+			}
+			rawGasPrice, err := cmd.Flags().GetString(flagGasPrice)
+			if err != nil {
+				return err
+			}
+			gasPrice, err := strconv.ParseInt(rawGasPrice, 10, 64)
+			if err != nil {
+				return err
+			}
+			rawInterval, err := cmd.Flags().GetString(flagPollInterval)
+			if err != nil {
+				return err
+			}
+			interval, err := strconv.ParseInt(rawInterval, 10, 64)
 			if err != nil {
 				return err
 			}
 
 			for {
-				update_validator(args[0], args[1], args[2], args[3], uint64(gasPrice))
+				updateValidators(rpcURI, contractAddress, nodeURI, privateKey, uint64(gasPrice))
 				fmt.Println("finish round")
 				time.Sleep(time.Duration(interval) * time.Hour)
 			}
 		},
 	}
+	cmd.Flags().String(flagRPCUri, "", "RPC URI")
+	cmd.Flags().String(flagContractAddress, "", "Address of contract")
+	cmd.Flags().String(flagNodeUri, "", "Node URI")
+	cmd.Flags().String(flagPrivKey, "", "Private key")
+	cmd.Flags().String(flagGasPrice, "", "Gas Price")
+	cmd.Flags().String(flagPollInterval, "", "Interval of update validatos (Hours)")
 
 	err := cmd.Execute()
 	if err != nil {
