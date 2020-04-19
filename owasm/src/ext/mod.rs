25169972b2e2c382626e11b2599d723fae01a1e3
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-04-19 16:01:28 +0700

    use load input in load majority

--- owasm/src/ext/mod.rs
+++ owasm/src/ext/mod.rs
@@ -45,16 +45,7 @@ pub fn load_majority<T>(external_id: i64) -> Option<T>
 where
     T: std::str::FromStr + std::cmp::PartialEq + std::marker::Copy,
 {
-    let mut vec: Vec<T> = Vec::new();
-    for idx in 0..oei::get_requested_validator_count() {
-        let external_data = oei::get_external_data(external_id, idx);
-        match external_data.and_then(|x| x.parse::<T>().ok()) {
-            Some(v) => {
-                vec.push(v);
-            }
-            None => (),
-        }
-    }
+    let vec: Vec<T> = load_input(external_id);
 
     if vec.len() == 0 {
         None
@@ -96,47 +87,6 @@ where
         Some(candidate)
     } else {
         None
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_majority_int() {
-        let vals = vec![1, 2, 3, 1, 3, 1, 1];
-        assert_eq!(majority(vals), Some(1));
-    }
-
-    #[test]
-    fn test_majority_int_result_none() {
-        let vals = vec![1, 2, 3, 1, 3, 1, 1, 3];
-        assert_eq!(majority(vals), None);
-    }
-
-    #[test]
-    fn test_majority_float() {
-        let vals = vec![0.3, 1.0, 0.3, 0.4, 1.0, 1.0, 1.0];
-        assert_eq!(majority(vals), Some(1.0));
-    }
-
-    #[test]
-    fn test_majority_float_result_none() {
-        let vals = vec![0.3, 1.0, 0.3, 0.4, 4.0, 1.0, 99.99, 1.0, 1.0];
-        assert_eq!(majority(vals), None);
-    }
-
-    #[test]
-    fn test_majority_char() {
-        let vals = vec!['a', 'b', 'a', 'b', 'b'];
-        assert_eq!(majority(vals), Some('b'));
-    }
-
-    #[test]
-    fn test_majority_char_result_none() {
-        let vals = vec!['a', 'b', 'a', 'b', 'c', 'b'];
-        assert_eq!(majority(vals), None);
     }
 }
 
@@ -206,5 +156,41 @@ mod tests {
 
         let mut vals = vec![13.0, 36.0, 45.0, 33.0];
         assert_eq!(median(&mut vals), 34.5);
+    }
+
+    #[test]
+    fn test_majority_int() {
+        let vals = vec![1, 2, 3, 1, 3, 1, 1];
+        assert_eq!(majority(vals), Some(1));
+    }
+
+    #[test]
+    fn test_majority_int_result_none() {
+        let vals = vec![1, 2, 3, 1, 3, 1, 1, 3];
+        assert_eq!(majority(vals), None);
+    }
+
+    #[test]
+    fn test_majority_float() {
+        let vals = vec![0.3, 1.0, 0.3, 0.4, 1.0, 1.0, 1.0];
+        assert_eq!(majority(vals), Some(1.0));
+    }
+
+    #[test]
+    fn test_majority_float_result_none() {
+        let vals = vec![0.3, 1.0, 0.3, 0.4, 4.0, 1.0, 99.99, 1.0, 1.0];
+        assert_eq!(majority(vals), None);
+    }
+
+    #[test]
+    fn test_majority_char() {
+        let vals = vec!['a', 'b', 'a', 'b', 'b'];
+        assert_eq!(majority(vals), Some('b'));
+    }
+
+    #[test]
+    fn test_majority_char_result_none() {
+        let vals = vec!['a', 'b', 'a', 'b', 'c', 'b'];
+        assert_eq!(majority(vals), None);
     }
 }
