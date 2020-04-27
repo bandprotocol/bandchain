b9445df86dac6b64075c773a95e71b52da607702
taobun <bun@bandprotocol.com>
2020-04-27 10:48:33 +0000

    Packets solidity encode and update test

--- bridges/evm/contracts/mocks/PacketsMock.sol
+++ bridges/evm/contracts/mocks/PacketsMock.sol
@@ -13,4 +13,19 @@ contract PacketsMock {
     {
         return Packets.marshalRequestPacket(_packet);
     }
+
+    function marshalResponsePacket(IBridge.ResponsePacket memory _packet)
+        public
+        pure
+        returns (bytes memory)
+    {
+        return Packets.marshalResponsePacket(_packet);
+    }
+
+    function getResultHash(
+        IBridge.RequestPacket memory _req,
+        IBridge.ResponsePacket memory _res
+    ) public pure returns (bytes32) {
+        return Packets.getResultHash(_req, _res);
+    }
 }
