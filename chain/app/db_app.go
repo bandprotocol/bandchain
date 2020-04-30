b4a0e0d70a11210c812f7160255782f85cce6e9c
taobun <bun@bandprotocol.com>
2020-04-30 03:04:55 +0000

    Fix BandApp

--- chain/app/db_app.go
+++ chain/app/db_app.go
@@ -265,7 +265,7 @@ func (app *dbBandApp) EndBlock(req abci.RequestEndBlock) (res abci.ResponseEndBl
 		panic(err)
 	}
 
-	totalSupply := app.BandApp.SupplyKeeper.GetSupply(app.DeliverContext).GetTotal()
+	totalSupply := app.SupplyKeeper.GetSupply(app.DeliverContext).GetTotal()
 	err = app.dbBand.SetTotalSupply(totalSupply)
 	if err != nil {
 		panic(err)
