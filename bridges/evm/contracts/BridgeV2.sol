9954dfef8f3cc8f9f1d992b083135c6e95e7c714
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-05-02 01:35:59 +0700

    fix duplicate key

--- bridges/evm/contracts/BridgeV2.sol
+++ bridges/evm/contracts/BridgeV2.sol
@@ -1,52 +1,49 @@
-pragma solidity 0.5.17;
+pragma solidity 0.5.14;
 pragma experimental ABIEncoderV2;
 
 import {Bridge} from "./Bridge.sol";
-import {IBridgeV2} from "./BridgeV2.sol";
+
+import {IBridgeV2} from "./IBridgeV2.sol";
+import {IBridge} from "./IBridge.sol";
 
 
 contract BridgeV2 is Bridge, IBridgeV2 {
-    struct Request {
-        string clientId;
-        uint64 oracleScriptId;
-        bytes params;
-        int64 askCount;
-        int64 minCount;
-    }
     struct PendingRequest {
-        Request request;
+        IBridge.RequestPacket request;
         address targetAddress;
+        bool isValue;
     }
 
     mapping(bytes32 => PendingRequest) public pendingRequest;
     event RequestOracle(
         string clientId,
         uint256 oracleScriptId,
-        bytes params,
-        int64 askCount,
-        int64 minCount
+        string params,
+        uint64 askCount,
+        uint64 minCount
     );
 
-    function requestOracle(Request calldata request)
+    function requestOracle(IBridge.RequestPacket calldata request)
         external
         returns (bytes32)
     {
         emit RequestOracle(
             request.clientId,
             request.oracleScriptId,
-            request.calldata,
+            request.params,
             request.askCount,
             request.minCount
         );
         bytes32 key = keccak256(abi.encodePacked(msg.sender, request.clientId));
-        Request memory r = Request(
+        if (pendingRequest[key].isValue) revert();
+        IBridge.RequestPacket memory r = IBridge.RequestPacket(
             request.clientId,
             request.oracleScriptId,
             request.params,
             request.askCount,
             request.minCount
         );
-        pendingRequest[key] = PendingRequest(r, msg.sender);
+        pendingRequest[key] = PendingRequest(r, msg.sender, true);
         return key;
     }
 }
