51944884e863181708b1a75a5dcadcb136289cbf
Kanisorn Thongprapaisaeng <kanisorn@bandprotocol.com>
2020-05-14 11:46:43 +0700

    Merge pull request #1597 from bandprotocol/last-250-votes
    Cacher: create view table for get last 250 validator vote

--- chain/db/db.go
+++ chain/db/db.go
@@ -74,6 +74,27 @@ func NewDB(dialect, path string) (*BandDB, error) {
 			FROM delegations JOIN validators ON validator_address = operator_address;
 	`)
 
+	db.Exec(`CREATE VIEW validator_last_250_votes AS
+			SELECT COUNT(*), consensus_address, voted
+			FROM validator_votes 
+			WHERE block_height > (SELECT MAX(height) from blocks) - 250
+			GROUP BY consensus_address, voted;
+	`)
+
+	db.Exec(`CREATE VIEW validator_last_1000_votes AS
+			SELECT COUNT(*), consensus_address, voted
+			FROM validator_votes 
+			WHERE block_height > (SELECT MAX(height) from blocks) - 1000
+			GROUP BY consensus_address, voted;
+	`)
+
+	db.Exec(`CREATE VIEW validator_last_10000_votes AS
+			SELECT COUNT(*), consensus_address, voted
+			FROM validator_votes 
+			WHERE block_height > (SELECT MAX(height) from blocks) - 10000
+			GROUP BY consensus_address, voted;
+	`)
+
 	db.Model(&Block{}).AddForeignKey(
 		"proposer",
 		"validators(consensus_address)",
