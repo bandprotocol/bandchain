152ac0e31704f6e5882256cda3243878b311d6d7
Peri <perii.peko@gmail.com>
2020-05-20 12:24:11 +0700

    Update command description

--- chain/cmd/bandoracled2/run.go
+++ chain/cmd/bandoracled2/run.go
@@ -89,7 +89,7 @@ func runCmd(c *Context) *cobra.Command {
 	cmd.Flags().String(flags.FlagChainID, "", "chain ID of BandChain network")
 	cmd.Flags().String(flags.FlagNode, "tcp://localhost:26657", "RPC url to BandChain node")
 	cmd.Flags().String(flagValidator, "", "validator address")
-	cmd.Flags().String(flags.FlagGasPrices, "", "gas prices")
+	cmd.Flags().String(flags.FlagGasPrices, "", "gas prices for report transaction")
 	viper.BindPFlag(flags.FlagChainID, cmd.Flags().Lookup(flags.FlagChainID))
 	viper.BindPFlag(flags.FlagNode, cmd.Flags().Lookup(flags.FlagNode))
 	viper.BindPFlag(flagValidator, cmd.Flags().Lookup(flagValidator))
