7516324ccaa7089871ad5b1e7aef5f0904686b66
Peri <perii.peko@gmail.com>
2020-05-22 18:11:42 +0700

    Add bandd script

---
+++ chain/scripts/start_bandd.sh
@@ -0,0 +1,38 @@
+DIR=`dirname "$0"`
+
+rm -rf ~/.band*
+dropdb my_db
+createdb my_db
+
+# initial new node
+bandd init validator --chain-id bandchain --oracle band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs
+echo "lock nasty suffer dirt dream fine fall deal curtain plate husband sound tower mom crew crawl guard rack snake before fragile course bacon range" \
+    | bandcli keys add validator --recover --keyring-backend test  --coin-type 494
+echo "smile stem oven genius cave resource better lunar nasty moon company ridge brass rather supply used horn three panic put venue analyst leader comic" \
+    | bandcli keys add requester --recover --keyring-backend test  --coin-type 494
+
+# add accounts to genesis
+bandd add-genesis-account validator 10000000000000uband --keyring-backend test
+bandd add-genesis-account requester 10000000000000uband --keyring-backend test
+
+# genesis configurations
+bandcli config chain-id bandchain
+bandcli config output json
+bandcli config indent true
+bandcli config trust-node true
+
+# register initial validators
+echo "12345678" | bandd gentx \
+    --amount 100000000uband \
+    --node-id 11392b605378063b1c505c0ab123f04bd710d7d7 \
+    --pubkey bandvalconspub1addwnpepq06h7wvh5n5pmrejr6t3pyn7ytpwd5c0kmv0wjdfujs847em8dusjl96sxg \
+    --name validator \
+    --keyring-backend test
+
+# collect genesis transactions
+bandd collect-gentxs
+cp ./docker-config/single-validator/priv_validator_key.json ~/.bandd/config/priv_validator_key.json
+cp ./docker-config/single-validator/node_key.json ~/.bandd/config/node_key.json
+
+# start bandchain
+bandd start
