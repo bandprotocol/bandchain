3d380e38e4179ceaefee403fde6caf73123903f7
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-05-22 15:28:03 +0700

    update function names and tests

--- chain/pkg/obi/decode.go
+++ chain/pkg/obi/decode.go
@@ -15,35 +15,35 @@ func decodeImpl(data []byte, v interface{}) ([]byte, error) {
 	ev := rv.Elem()
 	switch ev.Kind() {
 	case reflect.Uint8:
-		val, rem, err := DecodeU8(data)
+		val, rem, err := DecodeUnsigned8(data)
 		ev.SetUint(uint64(val))
 		return rem, err
 	case reflect.Uint16:
-		val, rem, err := DecodeU16(data)
+		val, rem, err := DecodeUnsigned16(data)
 		ev.SetUint(uint64(val))
 		return rem, err
 	case reflect.Uint32:
-		val, rem, err := DecodeU32(data)
+		val, rem, err := DecodeUnsigned32(data)
 		ev.SetUint(uint64(val))
 		return rem, err
 	case reflect.Uint64:
-		val, rem, err := DecodeU64(data)
+		val, rem, err := DecodeUnsigned64(data)
 		ev.SetUint(uint64(val))
 		return rem, err
 	case reflect.Int8:
-		val, rem, err := DecodeI8(data)
+		val, rem, err := DecodeSigned8(data)
 		ev.SetInt(int64(val))
 		return rem, err
 	case reflect.Int16:
-		val, rem, err := DecodeI16(data)
+		val, rem, err := DecodeSigned16(data)
 		ev.SetInt(int64(val))
 		return rem, err
 	case reflect.Int32:
-		val, rem, err := DecodeI32(data)
+		val, rem, err := DecodeSigned32(data)
 		ev.SetInt(int64(val))
 		return rem, err
 	case reflect.Int64:
-		val, rem, err := DecodeI64(data)
+		val, rem, err := DecodeSigned64(data)
 		ev.SetInt(int64(val))
 		return rem, err
 	case reflect.String:
@@ -51,7 +51,7 @@ func decodeImpl(data []byte, v interface{}) ([]byte, error) {
 		ev.SetString(val)
 		return rem, err
 	case reflect.Slice:
-		length, rem, err := DecodeU32(data)
+		length, rem, err := DecodeUnsigned32(data)
 		if err != nil {
 			return nil, err
 		}
@@ -100,65 +100,65 @@ func MustDecode(data []byte, v interface{}) {
 	}
 }
 
-// DecodeU16 decodes the input bytes into `uint8` and returns the remaining bytes.
-func DecodeU8(data []byte) (uint8, []byte, error) {
+// DecodeUnsigned16 decodes the input bytes into `uint8` and returns the remaining bytes.
+func DecodeUnsigned8(data []byte) (uint8, []byte, error) {
 	if len(data) < 1 {
 		return 0, nil, errors.New("obi: out of range")
 	}
 	return data[0], data[1:], nil
 }
 
-// DecodeU16 decodes the input bytes into `uint16` and returns the remaining bytes.
-func DecodeU16(data []byte) (uint16, []byte, error) {
+// DecodeUnsigned16 decodes the input bytes into `uint16` and returns the remaining bytes.
+func DecodeUnsigned16(data []byte) (uint16, []byte, error) {
 	if len(data) < 2 {
 		return 0, nil, errors.New("obi: out of range")
 	}
 	return binary.LittleEndian.Uint16(data[:2]), data[2:], nil
 }
 
-// DecodeU32 decodes the input bytes into `uint32` and returns the remaining bytes.
-func DecodeU32(data []byte) (uint32, []byte, error) {
+// DecodeUnsigned32 decodes the input bytes into `uint32` and returns the remaining bytes.
+func DecodeUnsigned32(data []byte) (uint32, []byte, error) {
 	if len(data) < 4 {
 		return 0, nil, errors.New("obi: out of range")
 	}
 	return binary.LittleEndian.Uint32(data[:4]), data[4:], nil
 }
 
-// DecodeU64 decodes the input bytes into `uint64` and returns the remaining bytes.
-func DecodeU64(data []byte) (uint64, []byte, error) {
+// DecodeUnsigned64 decodes the input bytes into `uint64` and returns the remaining bytes.
+func DecodeUnsigned64(data []byte) (uint64, []byte, error) {
 	if len(data) < 8 {
 		return 0, nil, errors.New("obi: out of range")
 	}
 	return binary.LittleEndian.Uint64(data[:8]), data[8:], nil
 }
 
-// DecodeI8 decodes the input bytes into `uint64` and returns the remaining bytes.
-func DecodeI8(data []byte) (int8, []byte, error) {
-	unsigned, rem, err := DecodeU8(data)
+// DecodeSigned8 decodes the input bytes into `uint64` and returns the remaining bytes.
+func DecodeSigned8(data []byte) (int8, []byte, error) {
+	unsigned, rem, err := DecodeUnsigned8(data)
 	return int8(unsigned), rem, err
 }
 
-// DecodeI16 decodes the input bytes into `uint64` and returns the remaining bytes.
-func DecodeI16(data []byte) (int16, []byte, error) {
-	unsigned, rem, err := DecodeU16(data)
+// DecodeSigned16 decodes the input bytes into `uint64` and returns the remaining bytes.
+func DecodeSigned16(data []byte) (int16, []byte, error) {
+	unsigned, rem, err := DecodeUnsigned16(data)
 	return int16(unsigned), rem, err
 }
 
-// DecodeI32 decodes the input bytes into `uint64` and returns the remaining bytes.
-func DecodeI32(data []byte) (int32, []byte, error) {
-	unsigned, rem, err := DecodeU32(data)
+// DecodeSigned32 decodes the input bytes into `uint64` and returns the remaining bytes.
+func DecodeSigned32(data []byte) (int32, []byte, error) {
+	unsigned, rem, err := DecodeUnsigned32(data)
 	return int32(unsigned), rem, err
 }
 
-// DecodeI64 decodes the input bytes into `uint64` and returns the remaining bytes.
-func DecodeI64(data []byte) (int64, []byte, error) {
-	unsigned, rem, err := DecodeU64(data)
+// DecodeSigned64 decodes the input bytes into `uint64` and returns the remaining bytes.
+func DecodeSigned64(data []byte) (int64, []byte, error) {
+	unsigned, rem, err := DecodeUnsigned64(data)
 	return int64(unsigned), rem, err
 }
 
 // DecodeBytes decodes the input bytes and returns bytes result and the remaining bytes.
 func DecodeBytes(data []byte) ([]byte, []byte, error) {
-	length, rem, err := DecodeU32(data)
+	length, rem, err := DecodeUnsigned32(data)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -170,7 +170,7 @@ func DecodeBytes(data []byte) ([]byte, []byte, error) {
 
 // DecodeString decodes the input bytes and returns string result and the remaining bytes.
 func DecodeString(data []byte) (string, []byte, error) {
-	length, rem, err := DecodeU32(data)
+	length, rem, err := DecodeUnsigned32(data)
 	if err != nil {
 		return "", nil, err
 	}
