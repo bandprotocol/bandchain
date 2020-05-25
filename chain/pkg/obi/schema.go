77afa3277ed20f4eeab22778de256f4f79f8a304
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-05-25 12:04:50 +0700

    Use strings.Builder

--- chain/pkg/obi/schema.go
+++ chain/pkg/obi/schema.go
@@ -4,65 +4,82 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 )
 
-func getSchemaImpl(t reflect.Type) ([]byte, error) {
+func getSchemaImpl(s *strings.Builder, t reflect.Type) error {
 	switch t.Kind() {
 	case reflect.Uint8:
-		return []byte("u8"), nil
+		s.WriteString("u8")
+		return nil
 	case reflect.Uint16:
-		return []byte("u16"), nil
+		s.WriteString("u16")
+		return nil
 	case reflect.Uint32:
-		return []byte("u32"), nil
+		s.WriteString("u32")
+		return nil
 	case reflect.Uint64:
-		return []byte("u64"), nil
+		s.WriteString("u64")
+		return nil
 	case reflect.Int8:
-		return []byte("i8"), nil
+		s.WriteString("i8")
+		return nil
 	case reflect.Int16:
-		return []byte("i16"), nil
+		s.WriteString("i16")
+		return nil
 	case reflect.Int32:
-		return []byte("i32"), nil
+		s.WriteString("i32")
+		return nil
 	case reflect.Int64:
-		return []byte("i64"), nil
+		s.WriteString("i64")
+		return nil
 	case reflect.String:
-		return []byte("string"), nil
+		s.WriteString("string")
+		return nil
 	case reflect.Slice:
-		inner, err := getSchemaImpl(t.Elem())
+		s.WriteString("[")
+		err := getSchemaImpl(s, t.Elem())
 		if err != nil {
-			return nil, err
+			return err
 		}
-		return append(append([]byte("["), inner...), byte(']')), nil
+		s.WriteString("]")
+		return nil
 	case reflect.Struct:
 		if t.NumField() == 0 {
-			return nil, errors.New("obi: empty struct is not supported")
+			return errors.New("obi: empty struct is not supported")
 		}
-		res := []byte("{")
+		s.WriteString("{")
 		for idx := 0; idx < t.NumField(); idx++ {
 			field := t.Field(idx)
-			inner, err := getSchemaImpl(field.Type)
-			if err != nil {
-				return nil, err
-			}
 			name, ok := field.Tag.Lookup("obi")
 			if !ok {
-				return nil, fmt.Errorf("obi: no obi tag found for field %s of %s", field.Name, t.Name())
+				return fmt.Errorf("obi: no obi tag found for field %s of %s", field.Name, t.Name())
 			}
-			res = append(append(append(append(res, []byte(name)...), byte(':')), inner...), byte(','))
+			if idx != 0 {
+				s.WriteString(",")
+			}
+			s.WriteString(name)
+			s.WriteString(":")
+			err := getSchemaImpl(s, field.Type)
+			if err != nil {
+				return err
+			}
 		}
-		res[len(res)-1] = byte('}')
-		return res, nil
+		s.WriteString("}")
+		return nil
 	default:
-		return nil, fmt.Errorf("obi: unsupported value type: %s", t.Kind())
+		return fmt.Errorf("obi: unsupported value type: %s", t.Kind())
 	}
 }
 
 // GetSchema returns the compact OBI individual schema of the given value.
 func GetSchema(v interface{}) (string, error) {
-	schemaBytes, err := getSchemaImpl(reflect.TypeOf(v))
+	s := &strings.Builder{}
+	err := getSchemaImpl(s, reflect.TypeOf(v))
 	if err != nil {
 		return "", err
 	}
-	return string(schemaBytes), nil
+	return s.String(), nil
 }
 
 // MustGetSchema returns the compact OBI individual schema of the given value. Panics on error.
