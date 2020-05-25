00fee95fb37952a4b450cb39dedbae6a93a5f392
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-05-25 20:23:20 +0700

    obi: Add template for obi.js

---
+++ obi/obi.js/index.js
@@ -0,0 +1,27 @@
+class Obi {
+  /**
+   *
+   * @param {*} schema
+   */
+  constructor(schema) {
+    // TODO
+  }
+
+  /**
+   *
+   * @param {*} value
+   * @param {*} index
+   */
+  encode(value, index) {
+    // TODO
+  }
+
+  /**
+   *
+   * @param {*} data
+   * @param {*} index
+   */
+  decode(data, index) {
+    // TODO
+  }
+}
