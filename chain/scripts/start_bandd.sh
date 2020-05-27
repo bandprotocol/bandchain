e2f8c836a8f23e85c2426b93ea380a3647eb8689
Peri <perii.peko@gmail.com>
2020-05-27 11:16:53 +0700

    Remove debug mode

--- chain/scripts/start_bandd.sh
+++ chain/scripts/start_bandd.sh
@@ -12,17 +12,6 @@ for f in *; do
     fi
 done
 cd ../../chain
-
-# debug mode
-cd ../owasm/chaindebugtests
-for f in *; do
-    if [ -d "$f" ]; then
-        RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --package $f
-        cp ../target/wasm32-unknown-unknown/debug/$f.wasm ../../chain/pkg/owasm/res
-    fi
-done
-
-cd ../../chain
 go test ./...
 
 rm -rf ~/.band*
