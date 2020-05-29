db50d8cb6c4bb7ae0efff732ffa663483c42e2a0
Peri <perii.peko@gmail.com>
2020-05-29 14:21:37 +0700

    Add alias number type test cases

--- chain/pkg/obi/schema_test.go
+++ chain/pkg/obi/schema_test.go
@@ -28,6 +28,26 @@ type AllData struct {
 	NumInt64  int64  `obi:"numInt64"`
 }
 
+type Uint8ID uint8
+type Uint16ID uint16
+type Uint32ID uint32
+type Uint64ID uint64
+type Int8ID int8
+type Int16ID int16
+type Int32ID int32
+type Int64ID int64
+
+type AllDataAlias struct {
+	NumUint8  Uint8ID  `obi:"numUint8"`
+	NumUint16 Uint16ID `obi:"numUint16"`
+	NumUint32 Uint32ID `obi:"numUint32"`
+	NumUint64 Uint64ID `obi:"numUint64"`
+	NumInt8   Int8ID   `obi:"numInt8"`
+	NumInt16  Int16ID  `obi:"numInt16"`
+	NumInt32  Int32ID  `obi:"numInt32"`
+	NumInt64  Int64ID  `obi:"numInt64"`
+}
+
 func TestSchema(t *testing.T) {
 	require.Equal(t, "{symbol:string,px:u64,in:{a:u8,b:u8},arr:[i16]}", MustGetSchema(ExampleData{}))
 }
@@ -46,6 +66,10 @@ func TestUnsupportedTypeFail(t *testing.T) {
 
 func TestSchemaSupportedNumberTypeSuccess(t *testing.T) {
 	require.Equal(t, "{numUint8:u8,numUint16:u16,numUint32:u32,numUint64:u64,numInt8:i8,numInt16:i16,numInt32:i32,numInt64:i64}", MustGetSchema(AllData{}))
+}
+
+func TestSchemaSupportedNumberAliasTypeSuccess(t *testing.T) {
+	require.Equal(t, "{numUint8:u8,numUint16:u16,numUint32:u32,numUint64:u64,numInt8:i8,numInt16:i16,numInt32:i32,numInt64:i64}", MustGetSchema(AllDataAlias{}))
 }
 
 func TestSchemaInvalidSliceFail(t *testing.T) {
