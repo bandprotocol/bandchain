63ff3e2a1cb1bd6fa5e8765c816b3861f5f2e93b
Peri <perii.peko@gmail.com>
2020-06-01 14:23:12 +0700

    Remove chain rest server URI config

--- chain/cmd/bandoracled2/context.go
+++ chain/cmd/bandoracled2/context.go
@@ -7,10 +7,9 @@ import (
 )
 
 type Context struct {
-	client             *rpchttp.HTTP
-	chainRestServerURI string
-	validator          sdk.ValAddress
-	gasPrices          sdk.DecCoins
-	keys               chan keyring.Info
-	executor           executor
+	client    *rpchttp.HTTP
+	validator sdk.ValAddress
+	gasPrices sdk.DecCoins
+	keys      chan keyring.Info
+	executor  executor
 }
