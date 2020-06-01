0e2db5ad5504c7ffb070293ad369f28e71faf7b0
Peri <perii.peko@gmail.com>
2020-06-01 19:42:10 +0700

    Big endian test

--- bridges/evm/test/Borsh.test.js
+++ bridges/evm/test/Borsh.test.js
@@ -4,20 +4,21 @@ const BorshUser = artifacts.require("BorshUser");
 require("chai").should();
 
 contract("Borsh", () => {
+
   context("Borsh decoder should work correctly", () => {
     beforeEach(async () => {
       this.forTest = await BorshUser.new();
     });
 
     it("should decode correctly", async () => {
-      result = await this.forTest.decode("0x03000000425443320000000000000064");
+      result = await this.forTest.decode("0x00000003425443000000000000003264");
       result[0].toString().should.eq("BTC");
       result[1].toString().should.eq("50");
       result[2].toString().should.eq("100");
     });
 
     it("should decode correctly 2", async () => {
-      result = await this.forTest.decode("0x0400000062616e64900100000000000064");
+      result = await this.forTest.decode("0x0000000462616e64000000000000019064");
       result[0].toString().should.eq("band");
       result[1].toString().should.eq("400");
       result[2].toString().should.eq("100");
@@ -25,7 +26,7 @@ contract("Borsh", () => {
 
     it("should revert if invalid bytes", async () => {
       await expectRevert(
-        this.forTest.decode("0x030000004254433200000000000064"),
+        this.forTest.decode("0x000000034254433200000000000064"),
         "Borsh: Out of range"
       );
     });
