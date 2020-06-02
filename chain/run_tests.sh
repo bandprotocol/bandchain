fc67d67a1e69fb71e14db5c29eadb437f8d620f8
tansawit <sawit.tr@gmail.com>
2020-06-02 15:11:25 +0700

    commented out run_tests

--- chain/run_tests.sh
+++ chain/run_tests.sh
@@ -2,25 +2,25 @@
 
 mkdir -p pkg/owasm/res
 
-# release mode
-cd ../owasm/chaintests
+# # release mode
+# cd ../owasm/chaintests
 
-for f in *; do
-    if [ -d "$f" ]; then
-        RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release --package $f
-        cp ../target/wasm32-unknown-unknown/release/$f.wasm ../../chain/pkg/owasm/res
-    fi
-done
-cd ../../chain
+# for f in *; do
+#     if [ -d "$f" ]; then
+#         RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release --package $f
+#         cp ../target/wasm32-unknown-unknown/release/$f.wasm ../../chain/pkg/owasm/res
+#     fi
+# done
+# cd ../../chain
 
-# debug mode
-cd ../owasm/chaindebugtests
-for f in *; do
-    if [ -d "$f" ]; then
-        RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --package $f
-        cp ../target/wasm32-unknown-unknown/debug/$f.wasm ../../chain/pkg/owasm/res
-    fi
-done
+# # debug mode
+# cd ../owasm/chaindebugtests
+# for f in *; do
+#     if [ -d "$f" ]; then
+#         RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --package $f
+#         cp ../target/wasm32-unknown-unknown/debug/$f.wasm ../../chain/pkg/owasm/res
+#     fi
+# done
 
-cd ../../chain
+# cd ../../chain
 go test ./...
