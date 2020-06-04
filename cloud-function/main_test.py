085416a66c777b81b5643693246a5e1c3ffb0122
Peri <perii.peko@gmail.com>
2020-06-04 14:52:56 +0700

    Get constants from env

--- cloud-function/main_test.py
+++ cloud-function/main_test.py
@@ -1,7 +1,7 @@
 from main import app
 from flask import json
 
-def test_error_calldata_empty():
+def test_error_executable_empty():
   response = app.test_client().post(
       '/execute',
       data=json.dumps({'calldata': 'bitcoin', 'timeout': 123456}),
@@ -12,3 +12,15 @@ def test_error_calldata_empty():
 
   assert response.status_code == 400
   assert data['error'] == "executable field is missing from JSON request"
+
+def test_error_calldata_empty():
+  response = app.test_client().post(
+      '/execute',
+      data=json.dumps({'executable': '123', 'timeout': 123456}),
+      content_type='application/json',
+  )
+
+  data = json.loads(response.get_data(as_text=True))
+
+  assert response.status_code == 400
+  # assert data['error'] == "calldata field is missing from JSON request"
