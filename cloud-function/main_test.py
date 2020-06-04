048a4ada2fa3a7582a056a7bfa70648bd9aa61c3
Peri <perii.peko@gmail.com>
2020-06-04 14:28:01 +0700

    Add test

---
+++ cloud-function/main_test.py
@@ -0,0 +1,14 @@
+from main import app
+from flask import json
+
+def test_error_calldata_empty():
+  response = app.test_client().post(
+      '/execute',
+      data=json.dumps({'calldata': 'bitcoin', 'timeout': 123456}),
+      content_type='application/json',
+  )
+
+  data = json.loads(response.get_data(as_text=True))
+
+  assert response.status_code == 400
+  assert data['error'] == "executable field is missing from JSON request"
