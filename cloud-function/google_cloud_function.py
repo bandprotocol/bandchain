3d7fd204ab44c69ae0fbb09889a63a2ff2ba2d6c
Peri <perii.meko@gmail.com>
2020-06-05 14:35:51 +0700

    Format by Black

--- cloud-function/google_cloud_function.py
+++ cloud-function/google_cloud_function.py
@@ -7,7 +7,7 @@ import base64
 # Copy and paste this function on Google Cloud function
 # Set environment flag of MAX_EXECUTABLE, MAX_CALLDATA, MAX_TIMEOUT, MAX_STDOUT, MAX_STDERR
 def execute(request):
-  """Responds to any HTTP request.
+    """Responds to any HTTP request.
   Args:
       request (flask.Request): HTTP request object.
   Returns:
@@ -15,132 +15,141 @@ def execute(request):
       Response object using
       `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
   """
-  env = os.environ.copy()
+    env = os.environ.copy()
 
-  MAX_EXECUTABLE = env['MAX_EXECUTABLE']
-  MAX_CALLDATA = env['MAX_CALLDATA']
-  MAX_TIMEOUT = env['MAX_TIMEOUT']
-  MAX_STDOUT = env['MAX_STDOUT']
-  MAX_STDERR = env['MAX_STDERR']
+    MAX_EXECUTABLE = env["MAX_EXECUTABLE"]
+    MAX_CALLDATA = env["MAX_CALLDATA"]
+    MAX_TIMEOUT = env["MAX_TIMEOUT"]
+    MAX_STDOUT = env["MAX_STDOUT"]
+    MAX_STDERR = env["MAX_STDERR"]
 
-  if not MAX_EXECUTABLE:
-      raise Exception("MAX_EXECUTABLE is missing")
-  if not MAX_CALLDATA:
-      raise Exception("MAX_CALLDATA is missing")
-  if not MAX_TIMEOUT:
-      raise Exception("MAX_TIMEOUT is missing")
-  if not MAX_STDOUT:
-      raise Exception("MAX_STDOUT is missing")
-  if not MAX_STDERR:
-      raise Exception("MAX_STDERR is missing")
+    if not MAX_EXECUTABLE:
+        raise Exception("MAX_EXECUTABLE is missing")
+    if not MAX_CALLDATA:
+        raise Exception("MAX_CALLDATA is missing")
+    if not MAX_TIMEOUT:
+        raise Exception("MAX_TIMEOUT is missing")
+    if not MAX_STDOUT:
+        raise Exception("MAX_STDOUT is missing")
+    if not MAX_STDERR:
+        raise Exception("MAX_STDERR is missing")
 
-  try:
-      request_json = request.get_json()
-  except:
-      return jsonify({
-          "error": "invalid JSON request format",
-      }), 400
+    try:
+        request_json = request.get_json()
+    except:
+        return jsonify({"error": "invalid JSON request format",}), 400
 
-  request_json = request.get_json()
-  if request_json:
-      if not 'executable' in request_json:
-          return jsonify({
-              "error": "executable field is missing from JSON request",
-          }), 400
-      elif not 'calldata' in request_json:
-          return jsonify({
-              "error": "calldata field is missing from JSON request",
-          }), 400
-      elif not 'timeout' in request_json:
-          return jsonify({
-              "error": "timeout field is missing from JSON request",
-          }), 400
-      elif not request_json['timeout']:
-          return jsonify({
-              "error": "timeout field is empty",
-          }), 400
-      elif len(request_json['executable']) > int(MAX_EXECUTABLE):
-          return jsonify({
-              "error": "executable value exceed max size",
-          }), 400
-      elif len(request_json['calldata']) > int(MAX_CALLDATA):
-          return jsonify({
-              "error": "calldata value exceed max size",
-          }), 400
-      elif not isinstance(request_json['timeout'], int):
-          return jsonify({
-              "error": "timeout type is invalid",
-          }), 400
-      elif request_json['timeout'] > int(MAX_TIMEOUT):
-          return jsonify({
-              "error": "Runtime exceeded max size",
-          }), 400
-      elif request_json['timeout'] <= 0:
-          return jsonify({
-              "error": "Runtime must more than 0",
-          }), 400
+    request_json = request.get_json()
+    if request_json:
+        if not "executable" in request_json:
+            return (
+                jsonify({"error": "executable field is missing from JSON request",}),
+                400,
+            )
+        elif not "calldata" in request_json:
+            return (
+                jsonify({"error": "calldata field is missing from JSON request",}),
+                400,
+            )
+        elif not "timeout" in request_json:
+            return (
+                jsonify({"error": "timeout field is missing from JSON request",}),
+                400,
+            )
+        elif not request_json["timeout"]:
+            return jsonify({"error": "timeout field is empty",}), 400
+        elif len(request_json["executable"]) > int(MAX_EXECUTABLE):
+            return jsonify({"error": "executable value exceed max size",}), 400
+        elif len(request_json["calldata"]) > int(MAX_CALLDATA):
+            return jsonify({"error": "calldata value exceed max size",}), 400
+        elif not isinstance(request_json["timeout"], int):
+            return jsonify({"error": "timeout type is invalid",}), 400
+        elif request_json["timeout"] > int(MAX_TIMEOUT):
+            return jsonify({"error": "Runtime exceeded max size",}), 400
+        elif request_json["timeout"] <= 0:
+            return jsonify({"error": "Runtime must more than 0",}), 400
 
-  executable = ""
-  try:
-      executable = base64.b64decode(request_json["executable"])
-  except:
-      return jsonify({
-          "error": "Can't decoded executable",
-      }), 400
-  
-  path = "/tmp/execute.sh"
-  with open(path, "w") as f:
-      f.write(executable.decode())
-  os.chmod(path, 0o775)
-  try:
-      
-      timeout_millisec = request_json['timeout']
-      timeout_sec = timeout_millisec/1000
+    executable = ""
+    try:
+        executable = base64.b64decode(request_json["executable"])
+    except:
+        return jsonify({"error": "Can't decoded executable",}), 400
 
-      result = subprocess.run(
-          [path] + shlex.split(request_json["calldata"]), env=env, timeout=timeout_sec, capture_output=True
-      )
+    path = "/tmp/execute.sh"
+    with open(path, "w") as f:
+        f.write(executable.decode())
+    os.chmod(path, 0o775)
+    try:
 
-      returncode = result.returncode
-      stdout = result.stdout.decode()
-      stderr = result.stderr.decode()
+        timeout_millisec = request_json["timeout"]
+        timeout_sec = timeout_millisec / 1000
 
-      if len(stdout) > int(MAX_STDOUT):
-          return jsonify({
-              "returncode": 112,
-              "stdout": "",
-              "stderr": "",
-              "err": "Stdout exceeded max size"
-          }), 200
-      
-      if len(stderr) > int(MAX_STDERR):
-          return jsonify({
-              "returncode": 113,
-              "stdout": "",
-              "stderr": "",
-              "err": "Stderr exceeded max size"
-          }), 200
-      
+        result = subprocess.run(
+            [path] + shlex.split(request_json["calldata"]),
+            env=env,
+            timeout=timeout_sec,
+            capture_output=True,
+        )
 
-      return jsonify({
-          "returncode": returncode,
-          "stdout": stdout,
-          "stderr": stderr,
-          "err": ""
-      }), 200
-  
-  except OSError:
-      return jsonify({
-          "returncode": 126,
-          "stdout": "",
-          "stderr": "",
-          "err": "Execution fail"
-      }), 200
+        returncode = result.returncode
+        stdout = result.stdout.decode()
+        stderr = result.stderr.decode()
 
-  except subprocess.TimeoutExpired:
-      return jsonify({
-          "returncode": 111,
-          "stdout": "",
-          "stderr": "",
-          "err": "Execution time limit exceeded"
-      }), 200
+        if len(stdout) > int(MAX_STDOUT):
+            return (
+                jsonify(
+                    {
+                        "returncode": 112,
+                        "stdout": "",
+                        "stderr": "",
+                        "err": "Stdout exceeded max size",
+                    }
+                ),
+                200,
+            )
+
+        if len(stderr) > int(MAX_STDERR):
+            return (
+                jsonify(
+                    {
+                        "returncode": 113,
+                        "stdout": "",
+                        "stderr": "",
+                        "err": "Stderr exceeded max size",
+                    }
+                ),
+                200,
+            )
+
+        return (
+            jsonify(
+                {
+                    "returncode": returncode,
+                    "stdout": stdout,
+                    "stderr": stderr,
+                    "err": "",
+                }
+            ),
+            200,
+        )
+
+    except OSError:
+        return (
+            jsonify(
+                {"returncode": 126, "stdout": "", "stderr": "", "err": "Execution fail"}
+            ),
+            200,
+        )
+
+    except subprocess.TimeoutExpired:
+        return (
+            jsonify(
+                {
+                    "returncode": 111,
+                    "stdout": "",
+                    "stderr": "",
+                    "err": "Execution time limit exceeded",
+                }
+            ),
+            200,
+        )
--- cloud-function/main_test.py
+++ cloud-function/main_test.py
@@ -4,292 +4,332 @@ from test.support import EnvironmentVarGuard
 import os
 import pytest
 
-def test_error_invalid_json_request():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data="{'executable': '123', 'calldata':}",
-      content_type='application/json',
-  )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "invalid JSON request format"
+def test_error_invalid_json_request():
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data="{'executable': '123', 'calldata':}",
+        content_type="application/json",
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "invalid JSON request format"
+
 
 def test_error_missing_executable():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'calldata': 'bitcoin', 'timeout': 123456}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps({"calldata": "bitcoin", "timeout": 123456}),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "executable field is missing from JSON request"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "executable field is missing from JSON request"
+
 
 def test_error_executable_empty():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': '', 'calldata': 'bitcoin', 'timeout': 123456}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps({"executable": "", "calldata": "bitcoin", "timeout": 123456}),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 126
-  assert data['stdout'] == ""
-  assert data['stderr'] == ""
-  assert data['err'] == "Execution fail"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 126
+    assert data["stdout"] == ""
+    assert data["stderr"] == ""
+    assert data["err"] == "Execution fail"
+
 
 def test_error_missing_calldata():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': '123', 'timeout': 123456}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps({"executable": "123", "timeout": 123456}),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "calldata field is missing from JSON request"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "calldata field is missing from JSON request"
 
 
 def test_error_calldata_empty():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': 'IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwpwcmludCgnaGVsbG8nKQ==', 'calldata': '123', 'timeout': 123456}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwpwcmludCgnaGVsbG8nKQ==",
+                "calldata": "123",
+                "timeout": 123456,
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 0
-  assert data['stdout'] == "hello\n"
-  assert data['stderr'] == ""
-  assert data['err'] == ""
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 0
+    assert data["stdout"] == "hello\n"
+    assert data["stderr"] == ""
+    assert data["err"] == ""
+
 
 def test_error_missing_timeout():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': '123', 'calldata': 'bitcoin'}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps({"executable": "123", "calldata": "bitcoin"}),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "timeout field is missing from JSON request"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "timeout field is missing from JSON request"
+
 
 def test_error_timeout_empty():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': '123', 'calldata': 'bitcoin', 'timeout': ''}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps({"executable": "123", "calldata": "bitcoin", "timeout": ""}),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "timeout field is empty"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "timeout field is empty"
+
 
 def test_error_timeout_less_than_0():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': '123', 'calldata': 'bitcoin', 'timeout': -5}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps({"executable": "123", "calldata": "bitcoin", "timeout": -5}),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "Runtime must more than 0"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "Runtime must more than 0"
+
 
 def test_error_timeout_more_than_max_timeout():
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({'executable': '123', 'calldata': 'bitcoin', 'timeout': 1111111111111111111111111111111111111111}),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "executable": "123",
+                "calldata": "bitcoin",
+                "timeout": 1111111111111111111111111111111111111111,
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 400
-  assert data['error'] == "Runtime exceeded max size"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 400
+    assert data["error"] == "Runtime exceeded max size"
+
 
 def test_success_execution():
-  '''#!/usr/bin/env python3
+    """#!/usr/bin/env python3
       print('hello')
-  '''
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "123",
-        "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwpwcmludCgnaGVsbG8nKQ==",
-        "timeout": 123456
-      }),
-      content_type='application/json',
-  )
+  """
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "123",
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwpwcmludCgnaGVsbG8nKQ==",
+                "timeout": 123456,
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 0
-  assert data['stdout'] == "hello\n"
-  assert data['stderr'] == ""
-  assert data['err'] == ""
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 0
+    assert data["stdout"] == "hello\n"
+    assert data["stderr"] == ""
+    assert data["err"] == ""
+
 
 def test_error_execution_fail():
-  '''#!/usr/bin/enveeeeeeeee python3
+    """#!/usr/bin/enveeeeeeeee python3
       print('hello')
-  '''
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "",
-        "executable": "IyEvdXNyL2Jpbi9lbnZlZWVlZWVlZWUKcHl0aG9uM1xucHJpbnQoJ2hlbGxvJyk=",
-        "timeout": 123456
-      }),
-      content_type='application/json',
-  )
+  """
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "",
+                "executable": "IyEvdXNyL2Jpbi9lbnZlZWVlZWVlZWUKcHl0aG9uM1xucHJpbnQoJ2hlbGxvJyk=",
+                "timeout": 123456,
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 126
-  assert data['stdout'] == ""
-  assert data['stderr'] == ""
-  assert data['err'] == "Execution fail"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 126
+    assert data["stdout"] == ""
+    assert data["stderr"] == ""
+    assert data["err"] == "Execution fail"
+
 
 def test_error_execution_timeout():
-  '''#!/usr/bin/env python3
+    """#!/usr/bin/env python3
       import time
 
       time.sleep(1)
-  '''
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "",
-        "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKdGltZS5zbGVlcCgxKQ==",
-        "timeout": 100 #100 millisec
-      }),
-      content_type='application/json',
-  )
+  """
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "",
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKdGltZS5zbGVlcCgxKQ==",
+                "timeout": 100,  # 100 millisec
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 111
-  assert data['stdout'] == ""
-  assert data['stderr'] == ""
-  assert data['err'] == "Execution time limit exceeded"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 111
+    assert data["stdout"] == ""
+    assert data["stderr"] == ""
+    assert data["err"] == "Execution time limit exceeded"
+
 
 def test_success_execution_timeout():
-  '''#!/usr/bin/env python3
+    """#!/usr/bin/env python3
       import time
 
       time.sleep(1) # 1000 millisec
-  '''
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "",
-        "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKdGltZS5zbGVlcCgxKQpwcmludCgiaGVsbG8iKQ==",
-        "timeout": 2000  # 2000 millisec
-      }),
-      content_type='application/json',
-  )
+  """
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "",
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKdGltZS5zbGVlcCgxKQpwcmludCgiaGVsbG8iKQ==",
+                "timeout": 2000,  # 2000 millisec
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 0
-  assert data['stdout'] == "hello\n"
-  assert data['stderr'] == ""
-  assert data['err'] == ""
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 0
+    assert data["stdout"] == "hello\n"
+    assert data["stderr"] == ""
+    assert data["err"] == ""
+
 
 def test_error_infinite_loop_execution():
-  '''#!/usr/bin/env python3
+    """#!/usr/bin/env python3
       import time
 
       while True:
           print("hello")
-  '''
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "",
-        "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKd2hpbGUgVHJ1ZToKICAgIHByaW50KCJoZWxsbyIp",
-        "timeout": 1000  # 1000 millisec
-      }),
-      content_type='application/json',
-  )
+  """
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "",
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQp3aGlsZSBUcnVlOgogICAgcHJpbnQoImhlbGxvIik=",
+                "timeout": 1000,  # 1000 millisec
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 111
-  assert data['stdout'] == ""
-  assert data['stderr'] == ""
-  assert data['err'] == "Execution time limit exceeded"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 111
+    assert data["stdout"] == ""
+    assert data["stderr"] == ""
+    assert data["err"] == "Execution time limit exceeded"
+
 
 @pytest.fixture
 def mock_env(monkeypatch):
-  monkeypatch.setenv("MAX_STDOUT", "1")
-  monkeypatch.setenv("MAX_STDERR", "1")
+    monkeypatch.setenv("MAX_STDOUT", "1")
+    monkeypatch.setenv("MAX_STDERR", "1")
+
 
 def test_error_stdout_exceed(mock_env):
-  '''#!/usr/bin/env python3
+    """#!/usr/bin/env python3
       import time
 
       for i in range(10):
           print (i)
-  '''
+  """
 
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "",
-        "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKZm9yIGkgaW4gcmFuZ2UoMTApOgogICAgcHJpbnQgKGkp",
-        "timeout": 1000  # 1000 millisec
-      }),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "",
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKZm9yIGkgaW4gcmFuZ2UoMTApOgogICAgcHJpbnQgKGkp",
+                "timeout": 1000,  # 1000 millisec
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 112
-  assert data['stdout'] == ""
-  assert data['stderr'] == ""
-  assert data['err'] == "Stdout exceeded max size"
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 112
+    assert data["stdout"] == ""
+    assert data["stderr"] == ""
+    assert data["err"] == "Stdout exceeded max size"
+
 
 def test_error_stderr_exceed(mock_env):
-  '''#!/usr/bin/env python3
+    """#!/usr/bin/env python3
       import time
 
       print (1/0)
-  '''
+  """
 
-  app = create_app()
-  response = app.test_client().post(
-      '/execute',
-      data=json.dumps({
-        "calldata": "",
-        "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKcHJpbnQgKDEvMCk=",
-        "timeout": 1000  # 1000 millisec
-      }),
-      content_type='application/json',
-  )
+    app = create_app()
+    response = app.test_client().post(
+        "/execute",
+        data=json.dumps(
+            {
+                "calldata": "",
+                "executable": "IyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgdGltZQoKcHJpbnQgKDEvMCk=",
+                "timeout": 1000,  # 1000 millisec
+            }
+        ),
+        content_type="application/json",
+    )
 
-  data = json.loads(response.get_data(as_text=True))
-  assert response.status_code == 200
-  assert data['returncode'] == 113
-  assert data['stdout'] == ""
-  assert data['stderr'] == ""
-  assert data['err'] == "Stderr exceeded max size"
-
-
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["returncode"] == 113
+    assert data["stdout"] == ""
+    assert data["stderr"] == ""
+    assert data["err"] == "Stderr exceeded max size"
