81c0ce654a6aa4e35cb14ebc49828c3d640fa4d8
Peri <perii.meko@gmail.com>
2020-06-05 20:09:50 +0700

    Refactor

--- cloud-function/google_cloud_function.py
+++ cloud-function/google_cloud_function.py
@@ -4,8 +4,27 @@ import shlex
 import subprocess
 import base64
 
-# Copy and paste this function on Google Cloud function
+# Copy and paste this file on Google Cloud function
 # Set environment flag of MAX_EXECUTABLE, MAX_CALLDATA, MAX_TIMEOUT, MAX_STDOUT, MAX_STDERR
+
+def get_env(env, flag):
+  if not env[flag]:
+    raise Exception(flag + " is missing")
+  return int(env[flag])
+
+def check_field_in_json(request_json, field):
+  if not field in request_json:
+    return (
+        jsonify({"error": field + " field is missing from JSON request",}),
+        400,
+    )
+  return ()
+
+def check_max_exceed(request_json, field, max):
+  if len(request_json[field]) > max:
+    return jsonify({"error": field + " value exceeded max size",}), 400
+  return ()
+
 def execute(request):
     """Responds to any HTTP request.
   Args:
@@ -17,22 +36,11 @@ def execute(request):
   """
     env = os.environ.copy()
 
-    MAX_EXECUTABLE = env["MAX_EXECUTABLE"]
-    MAX_CALLDATA = env["MAX_CALLDATA"]
-    MAX_TIMEOUT = env["MAX_TIMEOUT"]
-    MAX_STDOUT = env["MAX_STDOUT"]
-    MAX_STDERR = env["MAX_STDERR"]
-
-    if not MAX_EXECUTABLE:
-        raise Exception("MAX_EXECUTABLE is missing")
-    if not MAX_CALLDATA:
-        raise Exception("MAX_CALLDATA is missing")
-    if not MAX_TIMEOUT:
-        raise Exception("MAX_TIMEOUT is missing")
-    if not MAX_STDOUT:
-        raise Exception("MAX_STDOUT is missing")
-    if not MAX_STDERR:
-        raise Exception("MAX_STDERR is missing")
+    MAX_EXECUTABLE = get_env(env, "MAX_EXECUTABLE")
+    MAX_CALLDATA = get_env(env, "MAX_CALLDATA")
+    MAX_TIMEOUT = get_env(env, "MAX_TIMEOUT")
+    MAX_STDOUT = get_env(env, "MAX_STDOUT")
+    MAX_STDERR = get_env(env, "MAX_STDERR")
 
     try:
         request_json = request.get_json()
@@ -41,33 +49,28 @@ def execute(request):
 
     request_json = request.get_json()
     if request_json:
-        if not "executable" in request_json:
-            return (
-                jsonify({"error": "executable field is missing from JSON request",}),
-                400,
-            )
-        elif not "calldata" in request_json:
-            return (
-                jsonify({"error": "calldata field is missing from JSON request",}),
-                400,
-            )
-        elif not "timeout" in request_json:
-            return (
-                jsonify({"error": "timeout field is missing from JSON request",}),
-                400,
-            )
-        elif not request_json["timeout"]:
-            return jsonify({"error": "timeout field is empty",}), 400
-        elif len(request_json["executable"]) > int(MAX_EXECUTABLE):
-            return jsonify({"error": "executable value exceed max size",}), 400
-        elif len(request_json["calldata"]) > int(MAX_CALLDATA):
-            return jsonify({"error": "calldata value exceed max size",}), 400
-        elif not isinstance(request_json["timeout"], int):
+
+        validate_executable = check_field_in_json(request_json, "executable")
+        if validate_executable: return validate_executable
+
+        validate_calldata = check_field_in_json(request_json, "calldata")
+        if validate_calldata: return validate_calldata
+
+        validate_timeout = check_field_in_json(request_json, "timeout")
+        if validate_timeout: return validate_timeout
+
+        valadate_max_executable = check_max_exceed(request_json, "executable", MAX_EXECUTABLE)
+        if valadate_max_executable: return valadate_max_executable
+
+        valadate_max_calldata = check_max_exceed(request_json, "calldata", MAX_CALLDATA)
+        if valadate_max_calldata: return valadate_max_calldata
+        
+        if not isinstance(request_json["timeout"], int):
             return jsonify({"error": "timeout type is invalid",}), 400
-        elif request_json["timeout"] > int(MAX_TIMEOUT):
-            return jsonify({"error": "Runtime exceeded max size",}), 400
         elif request_json["timeout"] <= 0:
             return jsonify({"error": "Runtime must more than 0",}), 400
+        elif request_json["timeout"] > MAX_TIMEOUT:
+            return jsonify({"error": "Runtime exceeded max size",}), 400
 
     executable = ""
     try:
@@ -96,8 +99,8 @@ def execute(request):
         proc.wait(timeout=timeout_sec)
 
         returncode = proc.returncode
-        stdout = proc.stdout.read(int(MAX_STDOUT)).decode()
-        stderr = proc.stderr.read(int(MAX_STDERR)).decode()
+        stdout = proc.stdout.read(MAX_STDOUT).decode()
+        stderr = proc.stderr.read(MAX_STDERR).decode()
 
         return (
             jsonify(
