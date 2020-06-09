faa4265c8d5c463918f5200bc562a925081f14f7
Peri <perii.meko@gmail.com>
2020-06-09 19:32:08 +0700

    Added test for wat to wasm

---
+++ go-owasm/api/lib_test.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func readWatFile(fileName string) []byte {
+	code, err := ioutil.ReadFile(fmt.Sprintf("./../wasm/%s.wat", fileName))
+	if err != nil {
+		panic(err)
+	}
+	return code
+}
+
+func readWasmFile(fileName string) []byte {
+	code, err := ioutil.ReadFile(fmt.Sprintf("./../wasm/%s.wasm", fileName))
+	if err != nil {
+		panic(err)
+	}
+	return code
+}
+
+func TestSuccessWatToOwasm(t *testing.T) {
+	code := readWatFile("test")
+	wasm, err := WatToWasm(code)
+	require.NoError(t, err)
+
+	expectedWasm := readWasmFile("test")
+	require.Equal(t, expectedWasm, wasm)
+}
+
+func TestFailInvalidWatContent(t *testing.T) {
+	code := []byte("invalid wat content")
+	wasm, err := WatToWasm(code)
+	require.Equal(t, ErrParseFail, err)
+	require.Equal(t, []byte(""), wasm)
+}
