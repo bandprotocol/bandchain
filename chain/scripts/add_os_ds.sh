762a01cfcf80fdaf116324e1ef72fc2a38d876b5
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-06-11 10:39:11 +0700

    fix gen_os_ds

--- chain/scripts/add_os_ds.sh
+++ chain/scripts/add_os_ds.sh
@@ -1,161 +1,188 @@
-bandd add-data-source "001_coingecko_price.py" \ 
-	"001_coingecko_price.py" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"CoinGecko Cryptocurrency Price" \
+	"Retrieves current price of a cryptocurrency from https://www.coingecko.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/001_coingecko_price.py
 
-bandd add-data-source "002_crypto_compare_price.sh" \ 
-	"002_crypto_compare_price.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"CryptoCompare Cryptocurrency Price" \
+	"Retrieves current price of a cryptocurrency from https://www.cryptocompare.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/002_crypto_compare_price.sh
 
-bandd add-data-source "003_binance_price.sh" \ 
-	"003_binance_price.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Binance Cryptocurrency Price" \
+	"Retrieves current price of a cryptocurrency from https://www.binance.com/en" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/003_binance_price.sh
 
-bandd add-data-source "004_open_weather_map.sh" \ 
-	"004_open_weather_map.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Open Weather Map Weather Data" \
+	"Retrieves current weather information from https://www.openweathermap.org" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/004_open_weather_map.sh
 
-bandd add-data-source "005_gold_price.sh" \ 
-	"005_gold_price.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"FreeForexAPI Gold Price" \
+	"Retrives current gold price from https://www.freeforexapi.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/005_gold_price.sh
 
-bandd add-data-source "006_alphavantage.sh" \ 
-	"006_alphavantage.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Alpha Vantage Stock Price" \
+	"Retrives current price of a stock from https://www.alphavaage.co" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/006_alphavantage.sh
 
-bandd add-data-source "007_bitcoin_count.sh" \ 
-	"007_bitcoin_count.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Blockchain.info Bitcoin Block Count" \
+	"Retrives latest Bitcoin block height from https://www.blockchain.info" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/007_bitcoin_count.sh
 
-bandd add-data-source "008_bitcoin_hash.sh" \ 
-	"008_bitcoin_hash.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"BlockCypher Bitcoin Block Hash" \
+	"Retrives Bitcoin block hash at a given block height from https://blockcypher.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/008_bitcoin_hash.sh
 
-bandd add-data-source "009_coingecko_volume.sh" \ 
-	"009_coingecko_volume.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"CoinGecko Cryptocurrency Trading Volume" \
+	"Retrieves current trading volume of a cryptocurrency from https://www.coingecko.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/009_coingecko_volume.sh
 
-bandd add-data-source "010_crypto_compare_volume.sh" \ 
-	"010_crypto_compare_volume.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"CryptoCompare Cryptocurrency Trading Volume" \
+	"Retrieves current trading volume of a cryptocurrency from https://www.cryptocompare.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/010_crypto_compare_volume.sh
 
-bandd add-data-source "011_ethgasstation.sh" \ 
-	"011_ethgasstation.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"ETH Gas Station Current Ethereum Gas Price" \
+	"Retrieves current Ethereum gas price from https://ethgasstation.info" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/011_ethgasstation.sh
 
-bandd add-data-source "012_open_sky_network.sh" \ 
-	"012_open_sky_network.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Open Sky Network Flight Data" \
+	"Retrieves flight information from https://opensky-network.org" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/012_open_sky_network.sh
 
-bandd add-data-source "013_qrng_anu.sh" \ 
-	"013_qrng_anu.sh" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Quantum Random Number Generator" \
+	"Retrieves array of random number from https://qrng.anu.edu.au" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/013_qrng_anu.sh
 
-bandd add-data-source "014_yahoo_finance.py" \ 
-	"014_yahoo_finance.py" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-data-source \
+	"Yahoo Finance Stock Price" \
+	"Retrieves current price of a stock from https://finance.yahoo.com" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/datasources/014_yahoo_finance.py
 
-bandd add-oracle-script "001_crypto_price" \ 
-	"001_crypto_price" \ 
-	"{symbol:string,multiplier:u64}/{px:u64}" \ 
-	"https://ipfs.io/ipfs/QmdMKT62HYaaYH44DrW1UkQNhsd76nZXej6KXWjYtR9c5m" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Cryptocurrency Price in USD" \
+	"Oracle script that queries the average cryptocurrency price using current price data from CoinGecko, CryptoCompare, and Binance" \
+	"{symbol:string,multiplier:u64}/{px:u64}" \
+	"https://ipfs.io/ipfs/QmdMKT62HYaaYH44DrW1UkQNhsd76nZXej6KXWjYtR9c5m" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/001_crypto_price.wasm
 
-bandd add-oracle-script "002_gold_price" \ 
-	"002_gold_price" \ 
-	"{multiplier:u64}/{px:u64}" \ 
-	"https://ipfs.io/ipfs/QmZ4oq69uvxBgcm5Lby415Fycc6upRpEe9cjnZzadFAYEq" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Gold Price in ATOMs" \
+	"Oracle script that queries current average gold price in ATOMs using gold price data from FreeForexAPI and ATOM price from Binance" \
+	"{multiplier:u64}/{px:u64}" \
+	"https://ipfs.io/ipfs/QmZ4oq69uvxBgcm5Lby415Fycc6upRpEe9cjnZzadFAYEq" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/002_gold_price.wasm
 
-bandd add-oracle-script "003_alphavantage" \ 
-	"003_alphavantage" \ 
-	"{symbol:string,api_key:string,multiplier:u64}/{px:u64}" \ 
-	"https://ipfs.io/ipfs/QmWohk9LcGSLTGdmUddCXMCa4Y96KAYkXQwba2Gfr2MTGw" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Stock Price (Alpha Vantage)" \
+	"Oracle script that queries the current price of a stock from Alpha Vantage" \
+	"{symbol:string,api_key:string,multiplier:u64}/{px:u64}" \
+	"https://ipfs.io/ipfs/QmWohk9LcGSLTGdmUddCXMCa4Y96KAYkXQwba2Gfr2MTGw" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/003_alphavantage.wasm
 
-bandd add-oracle-script "004_bitcoin_block_count" \ 
-	"004_bitcoin_block_count" \ 
-	"{_unused:u8}/{block_count:u64}" \ 
-	"https://ipfs.io/ipfs/QmdjEMcoUJfVSLp5nYNjuozGqr3p8X2Td2t1XgyBEspcFm" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Latest Bitcoin Block Count" \
+	"Oracle script that queries the latest Bitcoin block height from Blockchain.info" \
+	"{_unused:u8}/{block_count:u64}" \
+	"https://ipfs.io/ipfs/QmdjEMcoUJfVSLp5nYNjuozGqr3p8X2Td2t1XgyBEspcFm" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/004_bitcoin_block_count.wasm
 
-bandd add-oracle-script "005_bitcoin_block_hash" \ 
-	"005_bitcoin_block_hash" \ 
-	"{block_height:u64}/{block_hash:string}" \ 
-	"https://ipfs.io/ipfs/QmVYbGi5r7o5Aup97jZ6XMAp45tmiTjfwuSppPTZxcNzWQ" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Bitcoin Block Hash" \
+	"Oracle script for getting the Bitcoin block hash at the given block heightOracle script that queries the Bitcoin block hash at the given block height from BlockCypher" \
+	"{block_height:u64}/{block_hash:string}" \
+	"https://ipfs.io/ipfs/QmVYbGi5r7o5Aup97jZ6XMAp45tmiTjfwuSppPTZxcNzWQ" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/005_bitcoin_block_hash.wasm
 
-bandd add-oracle-script "006_coingecko_volume" \ 
-	"006_coingecko_volume" \ 
-	"{symbol:string,multiplier:u64}/{volume:u64}" \ 
-	"https://ipfs.io/ipfs/QmWWV2uS2nj9wzz29YhAezXqpy5sm48kwQnifFcNHNCM4V" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"CoinGecko Cryptocurrency Volume" \
+	"Oracle script that queries a cryptocurrency's average trading volume for the past day from Coingecko" \
+	"{symbol:string,multiplier:u64}/{volume:u64}" \
+	"https://ipfs.io/ipfs/QmWWV2uS2nj9wzz29YhAezXqpy5sm48kwQnifFcNHNCM4V" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/006_coingecko_volume.wasm
 
-bandd add-oracle-script "007_crypto_compare_volume" \ 
-	"007_crypto_compare_volume" \ 
-	"{symbol:string,multiplier:u64}/{volume:u64}" \ 
-	"https://ipfs.io/ipfs/Qmc4NxRirdYZyyou8JuVWLMWgor26WaRLXhoVFDEvv5MN5" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"CryptoCompare Cryptocurrency Volume" \
+	"Oracle script that queries a cryptocurrency's average trading volume for the past day from CryptoCompare" \
+	"{symbol:string,multiplier:u64}/{volume:u64}" \
+	"https://ipfs.io/ipfs/Qmc4NxRirdYZyyou8JuVWLMWgor26WaRLXhoVFDEvv5MN5" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/007_crypto_compare_volume.wasm
 
-bandd add-oracle-script "008_eth_gas_station" \ 
-	"008_eth_gas_station" \ 
-	"{gas_option:string}/{gweix10:u64}" \ 
-	"https://ipfs.io/ipfs/QmX3FE4QnGNJw9a5M4cgfXb9bBiTHdKzy2bN3Q3QUPz5KE" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Ethereum Gas Price" \
+	"Oracle script that queries the current Ethereum gas price from ETH gas station" \
+	"{gas_option:string}/{gweix10:u64}" \
+	"https://ipfs.io/ipfs/QmX3FE4QnGNJw9a5M4cgfXb9bBiTHdKzy2bN3Q3QUPz5KE" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/008_eth_gas_station.wasm
 
-bandd add-oracle-script "009_open_sky_network" \ 
-	"009_open_sky_network" \ 
-	"{flight_op:string,airport:string,icao24:string,begin:string,end:string}/{flight_existence:bool}" \ 
-	"https://ipfs.io/ipfs/QmcHVJbQUPzKLYpBgMU8Gm99QvGo3fJabPMeCd4KFbEuQj" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Open Sky Network" \
+	"Oracle script for checking whether a given flight number exists during the given time period from OpenSky Network" \
+	"{flight_op:string,airport:string,icao24:string,begin:string,end:string}/{flight_existence:bool}" \
+	"https://ipfs.io/ipfs/QmcHVJbQUPzKLYpBgMU8Gm99QvGo3fJabPMeCd4KFbEuQj" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/009_open_sky_network.wasm
 
-bandd add-oracle-script "010_open_weather_map" \ 
-	"010_open_weather_map" \ 
-	"{country:string,main_field:string,sub_field:string,multiplier:u64}/{value:u64}" \ 
-	"https://ipfs.io/ipfs/QmdPcvkXZk47AzkV5NxxYm3fnToYLewfsvP8BRSMpySQGK" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Open Weather Map" \
+	"Oracle script that queries the current weather data of a location from OpenWeatherMap" \
+	"{country:string,main_field:string,sub_field:string,multiplier:u64}/{value:u64}" \
+	"https://ipfs.io/ipfs/QmdPcvkXZk47AzkV5NxxYm3fnToYLewfsvP8BRSMpySQGK" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/010_open_weather_map.wasm
 
-bandd add-oracle-script "011_qrng" \ 
-	"011_qrng" \ 
-	"{size:u64}/{random_bytes:string}" \ 
-	"https://ipfs.io/ipfs/QmSoZQXsW5GqJivazw4LYB93pj5zW8qyNaWeBM2NfXkeHc" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Quantum Random Number Generator" \
+	"Oracle script that queries a large random number from  Australia's National University Quantum Random API." \
+	"{size:u64}/{random_bytes:string}" \
+	"https://ipfs.io/ipfs/QmSoZQXsW5GqJivazw4LYB93pj5zW8qyNaWeBM2NfXkeHc" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/011_qrng.wasm
 
-bandd add-oracle-script "012_yahoo_price" \ 
-	"012_yahoo_price" \ 
-	"{symbol:string,multiplier:u64}/{px:u64}" \ 
-	"https://ipfs.io/ipfs/QmQZ6UZwFYQLxSBvBS2sNm6p4D1hUkXxxEp5kiDCjwMFzH" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Yahoo Stock Price" \
+	"Oracle script that queries the current price of a stock from Yahoo Finance" \
+	"{symbol:string,multiplier:u64}/{px:u64}" \
+	"https://ipfs.io/ipfs/QmQZ6UZwFYQLxSBvBS2sNm6p4D1hUkXxxEp5kiDCjwMFzH" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/012_yahoo_price.wasm
 
-bandd add-oracle-script "013_fair_crypto_market_price" \ 
-	"013_fair_crypto_market_price" \ 
-	"{base_symbol:string,quote_symbol:string,aggregation_method:string,multiplier:u64}/{px:u64}" \ 
-	"https://ipfs.io/ipfs/QmSNvRVjTsSaGSyFgXnbbDyoS7q4YxJRdysrnC7MhUHTNi" \ 
-	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \ 
+bandd add-oracle-script \
+	"Fair Cryptocurrency Market Price" \
+	"Oracle script that queries the current price of a cryptocurrency from CoinGecko, CryptoCompare, and Binance and aggregates them using the user-selected method in the selected  base currency" \
+	"{base_symbol:string,quote_symbol:string,aggregation_method:string,multiplier:u64}/{px:u64}" \
+	"https://ipfs.io/ipfs/QmSNvRVjTsSaGSyFgXnbbDyoS7q4YxJRdysrnC7MhUHTNi" \
+	band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs \
 	/Users/beeb/bandchain/chain/pkg/owasm/res/013_fair_crypto_market_price.wasm
 
--- chain/scripts/gen_ds_os_script.py
+++ chain/scripts/gen_ds_os_script.py
@@ -3,76 +3,110 @@ import json
 import sys
 import requests
 import subprocess
-
-url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-dir = os.path.abspath(__file__)
-data_source_dir = os.path.abspath(os.path.join(dir, '../../datasources'))
-data_sources = []
-owner = 'band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs'
-
-f = open("add_os_ds.sh", "w")
-
-for filename in sorted(os.listdir(data_source_dir)):
-    file_path = os.path.join(data_source_dir, filename)
-    data_sources.append({'name': filename, 'description': filename,
-                         'path': os.path.join(data_source_dir, filename)})
-
-for value in data_sources:
-    name = value['name']
-    description = value['description']
-    path = value['path']
-    command_line = f'bandd add-data-source "{name}" \ \n\t"{description}" \ \n\t{owner} \ \n\t{path}\n\n'
-    # print(command_line)
-    f.write(command_line)
+from data_sources import data_source_info
+from oracle_scripts import oracle_scripts_info
 
 
-os_source_dir = os.path.abspath(
-    os.path.join(dir, '../../../owasm/chaintests'))
+def gen_data_source_oracle_script(ds_info, os_info, owner):
+    dir = os.path.abspath(__file__)
+    data_source_dir = os.path.abspath(os.path.join(dir, '../../datasources'))
+    os_source_dir = os.path.abspath(
+        os.path.join(dir, '../../../owasm/chaintests'))
 
-oracle_scripts = []
-for filename in sorted(os.listdir(os_source_dir)):
-    file_path = os.path.join(os_source_dir, filename, 'src', 'lib.rs')
+    file = open("add_os_ds.sh", "w")
+    if len(ds_info) != len(os.listdir(data_source_dir)):
+        raise Exception(
+            "length data source info does not match with amount of data sources in directory")
+    if len(os_info) != len(os.listdir(os_source_dir)):
+        raise Exception(
+            "length oracle script info does not match with amount of oracle scripts in directory")
+
+    gen_data_source(file, data_source_dir, ds_info, owner)
+    gen_oracle_script(file, os_source_dir, os_info, owner)
+
+    file.close()
+
+
+def gen_data_source(file, data_source_dir, ds_info, owner):
+    data_sources = []
+    for (name, des), filename in zip(ds_info, sorted(os.listdir(data_source_dir))):
+        file_path = os.path.join(data_source_dir, filename)
+        data_sources.append({'name': name, 'description': des,
+                             'path': file_path})
+
+    write_add_data_source(file, data_sources)
+
+
+def write_add_data_source(file, data_sources):
+    for value in data_sources:
+        name = value['name']
+        description = value['description']
+        path = value['path']
+        command_line = f'bandd add-data-source \\ \n\t"{name}" \\ \n\t"{description}" \\ \n\t{owner} \\ \n\t{path}\n\n'
+        file.write(command_line)
+
+
+def gen_oracle_script(file, os_source_dir, os_info, owner):
+    oracle_scripts = []
+    for (name, des), filename in zip(os_info, sorted(os.listdir(os_source_dir))):
+        file_path = os.path.join(os_source_dir, filename, 'src', 'lib.rs')
+        oracle_scripts.append({
+            'name': name, 'description': des, 'schema': get_schema(os.path.join(os_source_dir, filename)),
+            'url': get_url(file_path, filename),
+            'path': get_oracle_script_path(filename)})
+
+    write_add_oracle_script(file, oracle_scripts)
+
+
+def get_schema(path):
+    pwd = os.getcwd()
+    os.chdir(path)
+    subprocess.check_output(
+        'cargo test -- --nocapture | grep -v "^test" | grep -v "^running" | grep -v "^$" > schema.txt',
+        stderr=subprocess.STDOUT,
+        shell=True)
+
+    with open("schema.txt", 'r') as file:
+        schema = file.read().strip()
+        os.remove("schema.txt")
+        os.chdir(pwd)
+
+        return schema
+
+
+def get_url(file_path, filename):
+    url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+    headers = {
+        'pinata_api_key': os.getenv('PINATA_API_KEY'),
+        'pinata_secret_api_key': os.getenv('PINATA_API_SECRET')
+    }
     payload = {'pinataMetadata': f'{{"name": "{filename}"}}',
                'pinataOptions': '{"cidVersion":0}'}
     files = [
         ('file', open(file_path, 'rb')),
     ]
-    headers = {
-        'pinata_api_key': '5f7169a396725c53075b',
-        'pinata_secret_api_key': '2fdca43a889df5602fd79dcc65a310de8a6ec85ec0aca6ab55a78aa1d45e7ce7'
-    }
 
     r = requests.request(
         "POST", url, headers=headers, data=payload, files=files)
+    r.raise_for_status()
+    return 'https://ipfs.io/ipfs/' + json.loads(r.content)['IpfsHash']
 
-    # os.system(f"cd {os.path.join(os_source_dir, filename)}")
-    os.chdir(os.path.join(os_source_dir, filename))
-    print (os.getcwd())
 
-    x = subprocess.check_output(
-        'cargo test -- --nocapture | grep -v "^test" | grep -v "^running" | grep -v "^$" > out.txt',
-        stderr=subprocess.STDOUT,
-        shell=True)
+def get_oracle_script_path(filename):
+    return os.path.join(os.path.abspath(os.path.join(os.path.abspath(__file__), '../../')), 'pkg/owasm/res', filename + '.wasm')
 
-    schema = ""
-    ff = open("out.txt", "r")
-    schema = ff.read().strip()
-    ff.close()
-    print(schema.strip())
 
-    oracle_scripts.append({
-        'name': filename, 'description': filename, 'schema': schema,
-        'url': 'https://ipfs.io/ipfs/' + json.loads(r.content)['IpfsHash'],
-        'path': os.path.join(os.path.abspath(os.path.join(dir, '../../')), 'pkg/owasm/res', filename + '.wasm')})
+def write_add_oracle_script(file, oracle_scripts):
+    for value in oracle_scripts:
+        name = value['name']
+        description = value['description']
+        schema = value['schema']
+        url = value['url']
+        path = value['path']
+        command_line = f'bandd add-oracle-script \\ \n\t"{name}" \\ \n\t"{description}" \\ \n\t"{schema}" \\ \n\t"{url}" \\ \n\t{owner} \\ \n\t{path}\n\n'
+        file.write(command_line)
 
-for value in oracle_scripts:
-    name = value['name']
-    description = value['description']
-    schema = value['schema']
-    url = value['url']
-    path = value['path']
-    command_line = f'bandd add-oracle-script "{name}" \ \n\t"{description}" \ \n\t"{schema}" \ \n\t"{url}" \ \n\t{owner} \ \n\t{path}\n\n'
-    # print(command_line)
-    f.write(command_line)
 
-f.close()
+if __name__ == "__main__":
+    owner = 'band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs'
+    gen_data_source_oracle_script(data_source_info, oracle_scripts_info, owner)
