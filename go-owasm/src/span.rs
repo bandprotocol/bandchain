f320e995b3241c6d2d31575dfbf4eba0bb371d4d
Peri <perii.meko@gmail.com>
2020-06-11 21:34:20 +0700

    Remove unused

--- go-owasm/src/span.rs
+++ go-owasm/src/span.rs
@@ -34,18 +34,9 @@ impl Span {
     }
 }
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-    
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
 
     #[test]
     fn test_success_write_span_ok() {
