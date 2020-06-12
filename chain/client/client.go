fca8f46db304dd660ae1c7bb23ed32ba2c55940c
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-06-12 16:52:31 +0700

    chain: Add endpoint to get genesis file

--- chain/client/client.go
+++ chain/client/client.go
@@ -10,6 +10,7 @@ import (
 )
 
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc("/bandchain/genesis", GetGenesisHandlerFn(cliCtx)).Methods("GET")
 	r.HandleFunc("/bandchain/evm-validators", GetEVMValidators(cliCtx)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/bandchain/proof/{%s}", proof.RequestIDTag), proof.GetProofHandlerFn(cliCtx)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/bandchain/getfile/{%s}", Filename), GetFile()).Methods("GET")
