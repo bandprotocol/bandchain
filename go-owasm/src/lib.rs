8ff3a9e83b6f380cf2be82452bb43097804c29c9
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-06-18 18:39:52 +0700

    refactor code

--- go-owasm/src/lib.rs
+++ go-owasm/src/lib.rs
@@ -119,8 +119,12 @@ fn check_wasm_exports(module: &Module) -> Result<(), Error> {
             .collect()
     });
 
-    for required_export in REQUIRED_EXPORTS {
-        if !available_exports.iter().any(|x| x == required_export) {
+    if available_exports.len() != REQUIRED_EXPORTS.len() {
+        return Err(Error::CheckWasmExportsError);
+    }
+
+    for available_export in available_exports {
+        if !REQUIRED_EXPORTS.contains(&available_export.as_str()) {
             return Err(Error::CheckWasmExportsError);
         }
     }
