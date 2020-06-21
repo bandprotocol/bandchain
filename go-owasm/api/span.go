d75d64175fad5b1cbd4291330c04bb5c68ace869
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-06-21 20:31:56 +0700

    Add comments to go span functions

--- go-owasm/api/span.go
+++ go-owasm/api/span.go
@@ -5,6 +5,7 @@ package api
 import "C"
 import "unsafe"
 
+// newSpan creates a span with the given capacity and zero length, ready to be written.
 func newSpan(size int) C.Span {
 	return C.Span{
 		ptr: (*C.uint8_t)(C.malloc(C.uintptr_t(size))),
@@ -13,6 +14,7 @@ func newSpan(size int) C.Span {
 	}
 }
 
+// copySpan creates a full span populated with the given data.
 func copySpan(data []byte) C.Span {
 	return C.Span{
 		ptr: (*C.uint8_t)(C.CBytes(data)),
@@ -21,14 +23,17 @@ func copySpan(data []byte) C.Span {
 	}
 }
 
+// readSpan returns a copy of data in the span.
 func readSpan(span C.Span) []byte {
 	return C.GoBytes(unsafe.Pointer(span.ptr), C.int(span.len))
 }
 
+// freeSpan deallocates the memory data of the span. Must be called for every newSpan/copySpan.
 func freeSpan(span C.Span) {
 	C.free(unsafe.Pointer(span.ptr))
 }
 
+// writeSpan writes the given data into the span. Returns error if capacity is not enough.
 func writeSpan(span *C.Span, data []byte) C.Error {
 	if int(span.cap) < len(data) {
 		return C.Error_SpanTooSmallError
