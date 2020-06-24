66528fa3f6fa0db987976854ad8b6c9a06f190e0
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-06-24 17:26:34 +0700

    chain/bandcli: Remove unused endpoints and swagger

--- chain/client/client.go
+++ chain/client/client.go
@@ -1,8 +1,6 @@
 package rpc
 
 import (
-	"fmt"
-
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/gorilla/mux"
 )
@@ -10,5 +8,4 @@ import (
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/bandchain/genesis", GetGenesisHandlerFn(cliCtx)).Methods("GET")
 	r.HandleFunc("/bandchain/evm-validators", GetEVMValidators(cliCtx)).Methods("GET")
-	r.HandleFunc(fmt.Sprintf("/bandchain/getfile/{%s}", Filename), GetFile()).Methods("GET")
 }
