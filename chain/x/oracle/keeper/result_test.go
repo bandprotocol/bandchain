d5110284f958d4c5b0561cde966e0526fca60941
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-06-28 12:02:31 +0700

    chain: Update AddFile comments and remove unused GetAllResults

--- chain/x/oracle/keeper/result_test.go
+++ chain/x/oracle/keeper/result_test.go
@@ -36,27 +36,3 @@ func TestResultBasicFunctions(t *testing.T) {
 	require.True(t, k.HasResult(ctx, types.RequestID(1)))
 	require.False(t, k.HasResult(ctx, types.RequestID(2)))
 }
-
-func TestGetAllResults(t *testing.T) {
-	_, ctx, k := createTestInput()
-
-	req1 := types.NewOracleRequestPacketData("alice", 1, BasicCalldata, 1, 1)
-	res1 := types.NewOracleResponsePacketData("alice", 1, 1, 1589535020, 1589535022, 1, BasicCalldata)
-	encodedResult1 := obi.MustEncode(req1, res1)
-	k.SetResult(ctx, types.RequestID(1), encodedResult1)
-	req4 := types.NewOracleRequestPacketData("bob", 1, BasicCalldata, 1, 1)
-	res4 := types.NewOracleResponsePacketData("bob", 4, 1, 1589535020, 1589535022, 1, BasicCalldata)
-	encodedResult4 := obi.MustEncode(req4, res4)
-	k.SetResult(ctx, types.RequestID(4), encodedResult4)
-
-	results := k.GetAllResults(ctx)
-
-	require.Equal(t, 4, len(results))
-	require.Equal(t, encodedResult1, results[0])
-
-	// result of reqID 2 and 3 should be nil
-	require.Empty(t, results[1])
-	require.Empty(t, results[2])
-
-	require.Equal(t, encodedResult4, results[3])
-}
