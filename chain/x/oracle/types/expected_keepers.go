751de06de15a862b115539af2d8c8b4eab827e28
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-06-30 16:03:34 +0700

    Remove unused interface functions in expected_keepers

--- chain/x/oracle/types/expected_keepers.go
+++ chain/x/oracle/types/expected_keepers.go
@@ -15,15 +15,8 @@ type AccountKeeper interface {
 
 // SupplyKeeper defines the expected supply Keeper.
 type SupplyKeeper interface {
-	GetModuleAddress(name string) sdk.AccAddress
 	GetModuleAccount(ctx sdk.Context, name string) supplyexported.ModuleAccountI
-
-	// TODO remove with genesis 2-phases refactor https://github.com/cosmos/cosmos-sdk/issues/2862
-	SetModuleAccount(sdk.Context, supplyexported.ModuleAccountI)
-
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule string, recipientModule string, amt sdk.Coins) error
-	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
 
 // StakingKeeper defines the expected staking keeper.
@@ -31,8 +24,6 @@ type StakingKeeper interface {
 	ValidatorByConsAddr(sdk.Context, sdk.ConsAddress) stakingexported.ValidatorI
 	IterateBondedValidatorsByPower(ctx sdk.Context, fn func(index int64, validator stakingexported.ValidatorI) (stop bool))
 	Validator(ctx sdk.Context, address sdk.ValAddress) stakingexported.ValidatorI
-	Slash(ctx sdk.Context, consAddr sdk.ConsAddress, infractionHeight int64, power int64, slashFactor sdk.Dec)
-	Jail(ctx sdk.Context, consAddr sdk.ConsAddress)
 }
 
 // DistrKeeper defines the expected distribution keeper.
