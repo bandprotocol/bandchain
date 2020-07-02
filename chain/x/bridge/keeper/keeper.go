6e2dbd176be868477bcd7eb0f1f4a5d90b206a3e
Peri <perii.meko@gmail.com>
2020-07-02 10:40:55 +0700

    Add keeper

--- chain/x/bridge/keeper/keeper.go
+++ chain/x/bridge/keeper/keeper.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"github.com/bandprotocol/bandchain/chain/x/bridge/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -17,4 +18,60 @@ func NewKeeper(cdc *codec.Codec, key sdk.StoreKey) Keeper {
 		cdc:      cdc,
 		storeKey: key,
 	}
+}
+
+// SetChainID sets the chainID for relay and verify proof.
+func (k Keeper) SetChainID(ctx sdk.Context, chainID string) {
+	bz := k.cdc.MustMarshalBinaryLengthPrefixed(chainID)
+	ctx.KVStore(k.storeKey).Set(types.ChainIDStoreKey, bz)
+}
+
+// GetChainID returns the chain ID for relay and verify proof.
+func (k Keeper) GetChainID(ctx sdk.Context) string {
+	var chainID string
+	bz := ctx.KVStore(k.storeKey).Get(types.ChainIDStoreKey)
+	k.cdc.MustUnmarshalBinaryLengthPrefixed(bz, &chainID)
+	return chainID
+}
+
+// SetLatestRelayBlockHeight sets the latest block height that relay block.
+func (k Keeper) SetLatestRelayBlockHeight(ctx sdk.Context, height int64) {
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.LatestRelayBlockHeightStoreKey, k.cdc.MustMarshalBinaryLengthPrefixed(height))
+}
+
+// GetLatestRelayBlockHeight returns the latest block height that relay block.
+func (k Keeper) GetLatestRelayBlockHeight(ctx sdk.Context) int64 {
+	var height int64
+	bz := ctx.KVStore(k.storeKey).Get(types.LatestRelayBlockHeightStoreKey)
+	k.cdc.MustUnmarshalBinaryLengthPrefixed(bz, &height)
+	return height
+}
+
+// SetLatestValidatorsUpdateBlockHeight sets the lastest block height that validator set is updated.
+func (k Keeper) SetLatestValidatorsUpdateBlockHeight(ctx sdk.Context, height int64) {
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.LatestValidatorsUpdateBlockHeightStoreKey, k.cdc.MustMarshalBinaryLengthPrefixed(height))
+}
+
+// GetLatestValidatorsUpdateBlockHeight returns the latest block height that validator set is updated.
+func (k Keeper) GetLatestValidatorsUpdateBlockHeight(ctx sdk.Context) int64 {
+	var height int64
+	bz := ctx.KVStore(k.storeKey).Get(types.LatestValidatorsUpdateBlockHeightStoreKey)
+	k.cdc.MustUnmarshalBinaryLengthPrefixed(bz, &height)
+	return height
+}
+
+// SetAppHash sets the app hash to the given height
+func (k Keeper) SetAppHash(ctx sdk.Context, height int64, appHash []byte) {
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.AppHashStoreKey(height), k.cdc.MustMarshalBinaryBare(appHash))
+}
+
+// GetAppHash returns the app hash of the given height
+func (k Keeper) GetAppHash(ctx sdk.Context, height int64) []byte {
+	var hash []byte
+	bz := ctx.KVStore(k.storeKey).Get(types.AppHashStoreKey(height))
+	k.cdc.MustUnmarshalBinaryLengthPrefixed(bz, &hash)
+	return hash
 }
--- chain/x/bridge/types/key.go
+++ chain/x/bridge/types/key.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 const (
 	// ModuleName is the name of the module.
 	ModuleName = "bridge"
@@ -10,4 +14,22 @@ const (
 var (
 	// GlobalStoreKeyPrefix is the prefix for global primitive state variables.
 	GlobalStoreKeyPrefix = []byte{0x00}
+	// ChainIDStoreKey is the key that keeps the chain ID.
+	ChainIDStoreKey = append(GlobalStoreKeyPrefix, []byte("ChainID")...)
+	// LatestValidatorsUpdateBlockHeightStoreKey is the key that keeps the latest block height that validators are updated.
+	LatestValidatorsUpdateBlockHeightStoreKey = append(GlobalStoreKeyPrefix, []byte("LatestUpdatedValidatorsBlockHeight")...)
+	//LatestRelayBlockHeightStoreKey is the key that keeps the latest block height that relay.
+	LatestRelayBlockHeightStoreKey = append(GlobalStoreKeyPrefix, []byte("LatestRelayBlockHeight")...)
+
+	// ValidatorStoreKeyPrefix is the prefix for validator store.
+	ValidatorStoreKeyPrefix = []byte{0x01}
+	// AppHashStoreKeyPrefix is the prefix for app hash store.
+	AppHashStoreKeyPrefix = []byte{0x02}
+	// LatestResponseStoreKeyPrefix is the prefix for latest response store.
+	LatestResponseStoreKeyPrefix = []byte{0x03}
 )
+
+//AppHashStoreKey returns the key to retrieve the app hash from block height.
+func AppHashStoreKey(height int64) []byte {
+	return append(AppHashStoreKeyPrefix, sdk.Uint64ToBigEndian(uint64(height))...)
+}
