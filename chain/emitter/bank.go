8727025b69751f4c82893bf8cf7e323e582ab669
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-07-03 12:49:46 +0700

    fix bug

--- chain/emitter/bank.go
+++ chain/emitter/bank.go
@@ -5,12 +5,12 @@ import (
 )
 
 // handleMsgSend implements emitter handler for MsgSend.
-func (app *App) handleMsgSend(txHash []byte, msg bank.MsgSend, evMap EvMap, extra JsDict) {
+func (app *App) handleMsgSend(msg bank.MsgSend) {
 	app.accs = append(app.accs, msg.FromAddress, msg.ToAddress)
 }
 
 // handleMsgMultiSend implements emitter handler for MsgMultiSend.
-func (app *App) handleMsgMultiSend(txHash []byte, msg bank.MsgMultiSend, evMap EvMap, extra JsDict) {
+func (app *App) handleMsgMultiSend(msg bank.MsgMultiSend) {
 	for _, input := range msg.Inputs {
 		app.accs = append(app.accs, input.Address)
 	}
