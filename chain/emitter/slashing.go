45b5321b35ba8b38659ca95141d7262e02232019
taobun <taobunoi@gmail.com>
2020-07-03 23:49:10 +0700

    Fix bug get event

--- chain/emitter/slashing.go
+++ chain/emitter/slashing.go
@@ -7,17 +7,11 @@ import (
 
 // handleEventSlash implements emitter handler for Slashing event.
 func (app *App) handleEventSlash(event EvMap) {
-	if raw, ok := event[slashing.AttributeKeyJailed]; ok && len(raw) == 1 {
-		consAddress, err := sdk.ConsAddressFromBech32(raw[0])
-		if err != nil {
-			panic(err)
-		}
-		validator, found := app.StakingKeeper.GetValidatorByConsAddr(app.DeliverContext, consAddress)
-		if !found {
-			panic("handleEventSlash: validator not found")
-		}
+	if raw, ok := event[slashing.EventTypeSlash+"."+slashing.AttributeKeyJailed]; ok && len(raw) == 1 {
+		consAddress, _ := sdk.ConsAddressFromBech32(raw[0])
+		validator, _ := app.StakingKeeper.GetValidatorByConsAddr(app.DeliverContext, consAddress)
 		app.Write("UPDATE_VALIDATOR", JsDict{
-			"operator_address": validator.OperatorAddress,
+			"operator_address": validator.OperatorAddress.String(),
 			"tokens":           validator.Tokens.Uint64(),
 			"jailed":           validator.Jailed,
 		})
@@ -26,13 +20,9 @@ func (app *App) handleEventSlash(event EvMap) {
 
 // handleMsgUnjail implements emitter handler for MsgUnjail.
 func (app *App) handleMsgUnjail(msg slashing.MsgUnjail) {
-	validator, found := app.StakingKeeper.GetValidator(app.DeliverContext, msg.ValidatorAddr)
-	if !found {
-		panic("handleMsgUnjail: validator not found")
-	}
+	validator, _ := app.StakingKeeper.GetValidator(app.DeliverContext, msg.ValidatorAddr)
 	app.Write("UPDATE_VALIDATOR", JsDict{
-		"operator_address": msg.ValidatorAddr,
+		"operator_address": msg.ValidatorAddr.String(),
 		"jailed":           validator.Jailed,
 	})
-
 }
