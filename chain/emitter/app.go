44241c20997fe775d636cd28c683dafdfecd5e5b
taobun <taobunoi@gmail.com>
2020-07-03 23:51:36 +0700

    Rename variable

--- chain/emitter/app.go
+++ chain/emitter/app.go
@@ -38,7 +38,7 @@ type App struct {
 
 // NewBandAppWithEmitter creates a new App instance.
 func NewBandAppWithEmitter(
-	topicPath string, logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest bool,
+	kafkaURI string, logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest bool,
 	invCheckPeriod uint, skipUpgradeHeights map[int64]bool, home string,
 	baseAppOptions ...func(*bam.BaseApp),
 ) *App {
@@ -46,13 +46,13 @@ func NewBandAppWithEmitter(
 		logger, db, traceStore, loadLatest, invCheckPeriod, skipUpgradeHeights,
 		home, baseAppOptions...,
 	)
-	topics := strings.SplitN(topicPath, "@", 2)
+	paths := strings.SplitN(kafkaURI, "@", 2)
 	return &App{
 		BandApp:   app,
 		txDecoder: auth.DefaultTxDecoder(app.Codec()),
 		writer: kafka.NewWriter(kafka.WriterConfig{
-			Brokers:      topics[1:],
-			Topic:        topics[0],
+			Brokers:      paths[1:],
+			Topic:        paths[0],
 			Balancer:     &kafka.LeastBytes{},
 			BatchTimeout: 1 * time.Millisecond,
 			// Async:    true, // TODO: We may be able to enable async mode on replay
