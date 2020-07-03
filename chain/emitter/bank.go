4fd4298d26807abeb2e3c60e551cd55b12e83f33
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-07-03 15:17:42 +0700

    Use AddAccounts

--- chain/emitter/bank.go
+++ chain/emitter/bank.go
@@ -6,15 +6,15 @@ import (
 
 // handleMsgSend implements emitter handler for MsgSend.
 func (app *App) handleMsgSend(msg bank.MsgSend) {
-	app.accs = append(app.accs, msg.FromAddress, msg.ToAddress)
+	app.AddAccounts(msg.FromAddress, msg.ToAddress)
 }
 
 // handleMsgMultiSend implements emitter handler for MsgMultiSend.
 func (app *App) handleMsgMultiSend(msg bank.MsgMultiSend) {
 	for _, input := range msg.Inputs {
-		app.accs = append(app.accs, input.Address)
+		app.AddAccounts(input.Address)
 	}
 	for _, output := range msg.Outputs {
-		app.accs = append(app.accs, output.Address)
+		app.AddAccounts(output.Address)
 	}
 }
