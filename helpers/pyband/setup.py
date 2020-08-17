797ed91a8edeb91cebf455e8621f1b96ea7f718f
Sorawit Suriyakarn <swit@bandprotocol.com>
2020-08-17 17:08:34 +0700

    :building_construction: pyband: Patch packet field name

--- helpers/pyband/setup.py
+++ helpers/pyband/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="pyband",
     packages=["pyband"],
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     description="Python library for BandChain",
     author="Band Protocol",
