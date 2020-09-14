f3294900daf756f07772c6adde43992b863ec131
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-09-14 19:31:11 +0700

    fix typos

--- runtime/lambda-authorizer/lambda_function.py
+++ runtime/lambda-authorizer/lambda_function.py
@@ -3,7 +3,7 @@ import jwt
 import json
 
 
-def reponse(is_authorized):
+def response(is_authorized):
     return {
         "isAuthorized": is_authorized,
         "context": {},
@@ -12,21 +12,21 @@ def reponse(is_authorized):
 
 def lambda_handler(event, context):
     if "headers" not in event:
-        return reponse(False)
+        return response(False)
     headers = event["headers"]
 
     if "authorization" not in headers:
-        return reponse(False)
+        return response(False)
     token = headers["authorization"]
 
     env = os.environ.copy()
     if "JWT_SECRET_KEY" not in env:
-        return reponse(False)
+        return response(False)
 
     SINGING_KEY = env["JWT_SECRET_KEY"]
     try:
         jwt.decode(token, SINGING_KEY, algorithms=['HS256'])
     except:
-        return reponse(False)
+        return response(False)
 
-    return reponse(True)
+    return response(True)
