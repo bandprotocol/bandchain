122551d7d813aa87af04a4bf2e9f960326cca6ba
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-09-15 22:06:33 +0700

    fix conflict

--- chain/emitter/sync/staking.go
+++ chain/emitter/sync/staking.go
@@ -27,7 +27,7 @@ func (app *App) emitStakingModule() {
 	})
 	app.StakingKeeper.IterateRedelegations(app.DeliverContext, func(_ int64, red types.Redelegation) (stop bool) {
 		for _, entry := range red.Entries {
-			app.Write("NEW_REDELEGATION", JsDict{
+			app.Write("NEW_REDELEGATION", common.JsDict{
 				"delegator_address":    red.DelegatorAddress,
 				"operator_src_address": red.ValidatorSrcAddress,
 				"operator_dst_address": red.ValidatorDstAddress,
@@ -39,7 +39,7 @@ func (app *App) emitStakingModule() {
 	})
 	app.StakingKeeper.IterateUnbondingDelegations(app.DeliverContext, func(_ int64, ubd types.UnbondingDelegation) (stop bool) {
 		for _, entry := range ubd.Entries {
-			app.Write("NEW_UNBONDING_DELEGATION", JsDict{
+			app.Write("NEW_UNBONDING_DELEGATION", common.JsDict{
 				"delegator_address": ubd.DelegatorAddress,
 				"operator_address":  ubd.ValidatorAddress,
 				"completion_time":   entry.CompletionTime.UnixNano(),
@@ -196,10 +196,10 @@ func (app *App) emitUpdateRedelation(operatorSrcAddress sdk.ValAddress, operator
 
 func (app *App) handleEventTypeCompleteUnbonding(evMap common.EvMap) {
 	acc, _ := sdk.AccAddressFromBech32(evMap[types.EventTypeCompleteUnbonding+"."+types.AttributeKeyDelegator][0])
-	app.Write("REMOVE_UNBONDING", JsDict{"timestamp": app.DeliverContext.BlockTime().UnixNano()})
+	app.Write("REMOVE_UNBONDING", common.JsDict{"timestamp": app.DeliverContext.BlockTime().UnixNano()})
 	app.AddAccountsInBlock(acc)
 }
 
-func (app *App) handEventTypeCompleteRedelegation(evMap EvMap) {
-	app.Write("REMOVE_REDELEGATION", JsDict{"timestamp": app.DeliverContext.BlockTime().UnixNano()})
+func (app *App) handEventTypeCompleteRedelegation(evMap common.EvMap) {
+	app.Write("REMOVE_REDELEGATION", common.JsDict{"timestamp": app.DeliverContext.BlockTime().UnixNano()})
 }
