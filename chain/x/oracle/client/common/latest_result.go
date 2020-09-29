f28a0876b6cc32bc2ec8751d7a42674506024685
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-09-29 23:27:17 +0700

    remove pointer

--- chain/x/oracle/client/common/latest_result.go
+++ chain/x/oracle/client/common/latest_result.go
@@ -7,15 +7,15 @@ import (
 
 func QuerySearchLatestResult(
 	route string, cliCtx context.CLIContext, oid, calldata, askCount, minCount string, limit int,
-) (*[]byte, int64, error) {
+) ([]byte, int64, error) {
 	requests, h, err := queryRequestsByLatestTxs(route, cliCtx, oid, calldata, askCount, minCount, limit)
 	if err != nil {
 		return nil, 0, err
 	}
 	if len(requests) == 0 {
 		bz, err := types.QueryNotFound("request with specified specification not found")
-		return &bz, 0, err
+		return bz, 0, err
 	}
 	bz, err := types.QueryOK(requests)
-	return &bz, h, err
+	return bz, h, err
 }
