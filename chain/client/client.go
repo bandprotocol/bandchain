d1130d80cd62fdafb30e019aa58de1f926be4c2a
taobun <bun@bandprotocol.com>
2020-10-21 18:44:55 +0700

    update pyband

--- chain/client/client.go
+++ chain/client/client.go
@@ -6,7 +6,7 @@ import (
 )
 
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc("/bandchain/chain-id", GetChainIDFn(cliCtx)).Methods("GET")
+	r.HandleFunc("/bandchain/chain_id", GetChainIDFn(cliCtx)).Methods("GET")
 	r.HandleFunc("/bandchain/genesis", GetGenesisHandlerFn(cliCtx)).Methods("GET")
 	r.HandleFunc("/bandchain/evm-validators", GetEVMValidators(cliCtx)).Methods("GET")
 }
