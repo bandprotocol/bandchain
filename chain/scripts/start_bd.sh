b428f11a01eec7949d79a541682e94620b429f11
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2020-10-26 20:22:12 +0700

    remove

--- chain/scripts/start_bd.sh
+++
@@ -1,5 +0,0 @@
-cp ./docker-config/single-validator/priv_validator_key.json ~/.bd/config/priv_validator_key.json
-cp ./docker-config/single-validator/node_key.json ~/.bd/config/node_key.json
-
-# start bandchain
-bd start --rpc.laddr tcp://0.0.0.0:1237
