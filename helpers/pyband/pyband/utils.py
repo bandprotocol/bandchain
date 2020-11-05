e4a78a5719b60f9f3867909be8f08d635a31b26e
Shine Sutheeravet <supreeda@bandprotocol.com>
2020-11-05 19:35:30 +0700

    :art: refactor get_latest_block on client module

---
+++ helpers/pyband/pyband/utils.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+def parse_datetime(raw_datetime) -> int:
+    splitted_datetime = raw_datetime.split(".")
+    formatted_datetime = splitted_datetime[0] + ".0Z"
+    parsed_datetime = datetime.strptime(formatted_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return int(parsed_datetime.timestamp())
